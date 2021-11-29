78c2b2f93cdfc261f25cb70d928ce992edd33dc5
h82258652 <842053625@qq.com>
2021-11-29 23:10:40 +0800

    submit 125 solution

---
+++ src/LeetCode125/Solution.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode125
+{
+    public class Solution
+    {
+        public bool IsPalindrome(string s)
+        {
+            var left = 0;
+            var right = s.Length - 1;
+            while (left < right)
+            {
+                var leftChar = s[left];
+                if (!char.IsLetterOrDigit(leftChar))
+                {
+                    left++;
+                    continue;
+                }
+
+                var rightChar = s[right];
+                if (!char.IsLetterOrDigit(rightChar))
+                {
+                    right--;
+                    continue;
+                }
+
+                if (char.ToLower(leftChar) == char.ToLower(rightChar))
+                {
+                    left++;
+                    right--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/LeetCode125/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode125
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("A man, a plan, a canal: Panama", true)]
+        [InlineData("race a car", false)]
+        public void Test(string s, bool assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.IsPalindrome(s);
+            result.Should().Be(assertResult);
+        }
+    }
+}
