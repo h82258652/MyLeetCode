b397b69b782f8a8a13e1de53b32b1b756726aff8
h82258652 <842053625@qq.com>
2021-11-29 20:27:39 +0800

    submit 007 solution

---
+++ src/LeetCode007/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace LeetCode007
+{
+    public class Solution
+    {
+        public int Reverse(int x)
+        {
+            if (x == int.MinValue || x == 0)
+            {
+                return 0;
+            }
+
+            int flag = x > 0 ? 1 : -1;
+            x = Math.Abs(x);
+
+            int result = 0;
+            while (x > 0)
+            {
+                // 溢出检测
+                if (result > int.MaxValue / 10)
+                {
+                    return 0;
+                }
+
+                result *= 10;
+                var mod = x % 10;
+
+                // 溢出检测
+                if (result > int.MaxValue - mod)
+                {
+                    return 0;
+                }
+
+                result += mod;
+                x /= 10;
+            }
+
+            return result * flag;
+        }
+    }
+}
---
+++ src/LeetCode007/TestCases.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode007
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(123, 321)]
+        [InlineData(-123, -321)]
+        [InlineData(120, 21)]
+        [InlineData(0, 0)]
+        [InlineData(1534236469, 0)]
+        public void Test(int x, int assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.Reverse(x);
+            result.Should().Be(assertResult);
+        }
+    }
+}
