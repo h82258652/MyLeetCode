8e56470119df34ba500aab1d8366d15ba6438bec
h82258652 <842053625@qq.com>
2021-11-29 21:12:09 +0800

    submit 136 solution

---
+++ src/LeetCode136/Solution.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode136
+{
+    public class Solution
+    {
+        public int SingleNumber(int[] nums)
+        {
+            int result = 0;
+            foreach (var num in nums)
+            {
+                result ^= num;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode136/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode136
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 2, 2, 1 }, 1)]
+        [InlineData(new[] { 4, 1, 2, 1, 2 }, 4)]
+        public void Test(int[] nums, int assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.SingleNumber(nums);
+            result.Should().Be(assertResult);
+        }
+    }
+}
