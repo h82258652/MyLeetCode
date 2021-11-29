9b243bbab07f5e29619c6709b1515fcf07004ec7
h82258652 <842053625@qq.com>
2021-11-29 23:03:38 +0800

    submit 058 solution

---
+++ src/LeetCode058/Solution.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode058
+{
+    public class Solution
+    {
+        public int LengthOfLastWord(string s)
+        {
+            var sLength = s.Length;
+            var start = sLength - 1;
+            while (true)
+            {
+                if (s[start] == ' ')
+                {
+                    start--;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            var end = start - 1;
+            while (end >= 0)
+            {
+                if (s[end] == ' ')
+                {
+                    break;
+                }
+
+                end--;
+            }
+
+            return start - end;
+        }
+    }
+}
---
+++ src/LeetCode058/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode058
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("Hello World", 5)]
+        [InlineData("   fly me   to   the moon  ", 4)]
+        [InlineData("luffy is still joyboy", 6)]
+        [InlineData("day", 3)]
+        public void Test(string s, int assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.LengthOfLastWord(s);
+            result.Should().Be(assertResult);
+        }
+    }
+}
