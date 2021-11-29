9ccf94546db3cce8e503d4d54677a684a7655a49
h82258652 <842053625@qq.com>
2021-11-29 20:59:08 +0800

    submit 035 solution

---
+++ src/LeetCode035/Solution.cs
@@ -0,0 +1,51 @@
+﻿namespace LeetCode035
+{
+    public class Solution
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            return SearchInsert(nums, target, 0, nums.Length - 1);
+        }
+
+        private int SearchInsert(int[] nums, int target, int low, int high)
+        {
+            if (high - low <= 1)
+            {
+                var highValue = nums[high];
+                if (highValue == target)
+                {
+                    return high;
+                }
+
+                var lowValue = nums[low];
+                if (lowValue == target)
+                {
+                    return low;
+                }
+
+                if (target > highValue)
+                {
+                    return high + 1;
+                }
+
+                return target > lowValue ? high : low;
+            }
+
+            var midIndex = (low + high) / 2;
+            var midValue = nums[midIndex];
+            if (midValue == target)
+            {
+                return midIndex;
+            }
+
+            if (midValue < target)
+            {
+                return SearchInsert(nums, target, midIndex, high);
+            }
+            else
+            {
+                return SearchInsert(nums, target, low, midIndex);
+            }
+        }
+    }
+}
---
+++ src/LeetCode035/TestCases.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode035
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1, 3, 5, 6 }, 5, 2)]
+        [InlineData(new[] { 1, 3, 5, 6 }, 2, 1)]
+        [InlineData(new[] { 1, 3, 5, 6 }, 7, 4)]
+        [InlineData(new[] { 1, 3, 5, 6 }, 0, 0)]
+        [InlineData(new[] { 1 }, 0, 0)]
+        public void Test(int[] nums, int target, int assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.SearchInsert(nums, target);
+            result.Should().Be(assertResult);
+        }
+    }
+}
