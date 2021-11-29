d05044dbae091e25692edf8d725e8e15bd46d94b
h82258652 <842053625@qq.com>
2021-11-29 20:34:33 +0800

    submit 009 solution

---
+++ src/LeetCode009/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode009
+{
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0)
+            {
+                return false;
+            }
+
+            int temp = x;
+            int palindrome = 0;
+            while (temp > 0)
+            {
+                palindrome *= 10;
+                palindrome += temp % 10;
+                temp /= 10;
+            }
+
+            return x == palindrome;
+        }
+    }
+}
---
+++ src/LeetCode009/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode009
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(121, true)]
+        [InlineData(-121, false)]
+        [InlineData(10, false)]
+        [InlineData(-101, false)]
+        public void Test(int x, bool assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.IsPalindrome(x);
+            result.Should().Be(assertResult);
+        }
+    }
+}
