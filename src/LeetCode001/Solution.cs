b26c921c658cdfba734a5a318f2ff6b17881fdc2
h82258652 <842053625@qq.com>
2021-11-29 19:52:56 +0800

    submit the first solution

---
+++ src/LeetCode001/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LeetCode001
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int length = nums.Length;
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = i + 1; j < length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new[] { i, j };
+                    }
+                }
+            }
+
+            throw new InvalidOperationException();
+        }
+    }
+}
---
+++ src/LeetCode001/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode001
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 2, 7, 11, 15 }, 9, new[] { 0, 1 })]
+        [InlineData(new[] { 3, 2, 4 }, 6, new[] { 1, 2 })]
+        [InlineData(new[] { 3, 3 }, 6, new[] { 0, 1 })]
+        public void Test(int[] nums, int target, int[] assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.TwoSum(nums, target);
+            result.Should().Equal(assertResult);
+        }
+    }
+}
