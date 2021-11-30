4acc87e4d80e7a43690b2a6be9a2a110dba08240
h82258652 <842053625@qq.com>
2021-11-30 10:34:49 +0800

    submit 013 solution

---
+++ src/LeetCode013/Solution.cs
@@ -0,0 +1,101 @@
+﻿namespace LeetCode013
+{
+    public class Solution
+    {
+        public int RomanToInt(string s)
+        {
+            var result = 0;
+
+            var length = s.Length;
+            var lengthMinus1 = length - 1;
+            for (byte i = 0; i < length;)
+            {
+                var c = s[i];
+                switch (c)
+                {
+                    case 'I':
+                        if (i < lengthMinus1)
+                        {
+                            switch (s[i + 1])
+                            {
+                                case 'V':
+                                    result += 4;
+                                    i += 2;
+                                    continue;
+                                case 'X':
+                                    result += 9;
+                                    i += 2;
+                                    continue;
+                            }
+                        }
+
+                        result++;
+                        i++;
+                        break;
+
+                    case 'V':
+                        result += 5;
+                        i++;
+                        break;
+
+                    case 'X':
+                        if (i < lengthMinus1)
+                        {
+                            switch (s[i + 1])
+                            {
+                                case 'L':
+                                    result += 40;
+                                    i += 2;
+                                    continue;
+                                case 'C':
+                                    result += 90;
+                                    i += 2;
+                                    continue;
+                            }
+                        }
+
+                        result += 10;
+                        i++;
+                        break;
+
+                    case 'L':
+                        result += 50;
+                        i++;
+                        break;
+
+                    case 'C':
+                        if (i < lengthMinus1)
+                        {
+                            switch (s[i + 1])
+                            {
+                                case 'D':
+                                    result += 400;
+                                    i += 2;
+                                    continue;
+                                case 'M':
+                                    result += 900;
+                                    i += 2;
+                                    continue;
+                            }
+                        }
+
+                        result += 100;
+                        i++;
+                        break;
+
+                    case 'D':
+                        result += 500;
+                        i++;
+                        break;
+
+                    case 'M':
+                        result += 1000;
+                        i++;
+                        break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode013/TestCases.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode013
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("III", 3)]
+        [InlineData("IV", 4)]
+        [InlineData("IX", 9)]
+        [InlineData("LVIII", 58)]
+        [InlineData("MCMXCIV", 1994)]
+        public void Test(string s, int assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.RomanToInt(s);
+            result.Should().Be(assertResult);
+        }
+    }
+}
