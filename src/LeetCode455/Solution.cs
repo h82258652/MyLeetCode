6951aa87c89b4fa9ed522fc034f0b21b631382b2
h82258652 <842053625@qq.com>
2021-11-30 16:10:09 +0800

    submit 455 solution

---
+++ src/LeetCode455/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeetCode455
+{
+    public class Solution
+    {
+        public int FindContentChildren(int[] g, int[] s)
+        {
+            var result = 0;
+            Array.Sort(g);
+            Array.Sort(s);
+
+            var gLength = g.Length;
+            var sLength = s.Length;
+            var j = 0;
+            for (var i = 0; i < gLength && j < sLength;)
+            {
+                if (g[i] <= s[j])
+                {
+                    result++;
+                    i++;
+                }
+
+                j++;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode455/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode455
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 3 }, new[] { 1, 1 }, 1)]
+        [InlineData(new[] { 1, 2 }, new[] { 1, 2, 3 }, 2)]
+        public void Test(int[] g, int[] s, int assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.FindContentChildren(g, s);
+            result.Should().Be(assertResult);
+        }
+    }
+}
