eb0290cfc2291393804837e56563e1aed7d78f77
h82258652 <842053625@qq.com>
2021-11-30 12:41:58 +0800

    submit 1002 solution

---
+++ src/LeetCode1002/Solution.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode1002
+{
+    public class Solution
+    {
+        public IList<string> CommonChars(string[] words)
+        {
+            var wordsLength = words.Length;
+            if (wordsLength == 1)
+            {
+                return words[0].Select(temp => temp.ToString()).ToList();
+            }
+
+            var firstWord = words[0];
+            var charCounts = new int[26];
+            foreach (var c in firstWord)
+            {
+                charCounts[c - 'a']++;
+            }
+
+            var result = new List<string>();
+
+            for (var i = 0; i < charCounts.Length; i++)
+            {
+                var charCount = charCounts[i];
+                if (charCount <= 0)
+                {
+                    continue;
+                }
+
+                var maxCount = int.MaxValue;
+                for (var j = 1; j < wordsLength; j++)
+                {
+                    var word = words[j];
+                    var count = word.Count(temp => temp == ('a' + i));
+                    maxCount = Math.Min(maxCount, count);
+                }
+
+                maxCount = Math.Min(maxCount, charCount);
+
+                for (var j = 0; j < maxCount; j++)
+                {
+                    var temp = (char)('a' + i);
+                    result.Add(temp.ToString());
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode1002/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1002
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { "bella", "label", "roller" }, new[] { "e", "l", "l" })]
+        [InlineData(new[] { "cool", "lock", "cook" }, new[] { "c", "o" })]
+        [InlineData(new[] { "dadaabaa", "bdaaabcc", "abccddbb", "bbaacdba", "ababbbab", "ccddbbba", "bbdabbda", "bdabaacb" }, new[] { "a", "b" })]
+        public void Test(string[] words, string[] assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.CommonChars(words);
+            result.Should().BeEquivalentTo(assertResult);
+        }
+    }
+}
