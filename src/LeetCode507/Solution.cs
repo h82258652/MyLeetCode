e2d841ab0c281d16d6ad869041697f30cf1b56cb
h82258652 <842053625@qq.com>
2021-11-30 13:31:00 +0800

    submit 507 solution

---
+++ src/LeetCode507/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode507
+{
+    public class Solution
+    {
+        public bool CheckPerfectNumber(int num)
+        {
+            var total = 0;
+            var i = 1;
+            var half = num / 2;
+            while (i <= half)
+            {
+                if (num % i == 0)
+                {
+                    total += i;
+                }
+
+                i++;
+            }
+
+            return total == num;
+        }
+    }
+}
---
+++ src/LeetCode507/TestCases.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode507
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(28, true)]
+        [InlineData(6, true)]
+        [InlineData(496, true)]
+        [InlineData(8128, true)]
+        [InlineData(2, false)]
+        public void Test(int num, bool assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.CheckPerfectNumber(num);
+            result.Should().Be(assertResult);
+        }
+    }
+}
