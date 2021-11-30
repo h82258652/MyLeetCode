66b0d15dda6bcbe2d90f98c3c514e118a608e046
h82258652 <842053625@qq.com>
2021-11-30 13:38:52 +0800

    submit 461 solution

---
+++ src/LeetCode461/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode461
+{
+    public class Solution
+    {
+        public int HammingDistance(int x, int y)
+        {
+            var xor = x ^ y;
+            var result = 0;
+            while (xor > 0)
+            {
+                if (xor % 2 == 1)
+                {
+                    result++;
+                }
+
+                xor >>= 1;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode461/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode461
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(1, 4, 2)]
+        [InlineData(3, 1, 1)]
+        public void Test(int x, int y, int assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.HammingDistance(x, y);
+            result.Should().Be(assertResult);
+        }
+    }
+}
