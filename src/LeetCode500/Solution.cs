6e91b2ca9571f148826a3a4e70064574250a5d14
h82258652 <842053625@qq.com>
2021-11-30 14:57:35 +0800

    submit 500 solution

---
+++ src/LeetCode500/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode500
+{
+    public class Solution
+    {
+        public string[] FindWords(string[] words)
+        {
+            var charMap = new[] { 2, 3, 3, 2, 1, 2, 2, 2, 1, 2, 2, 2, 3, 3, 1, 1, 1, 1, 2, 1, 1, 3, 1, 3, 1, 3 };
+
+            var result = new List<string>();
+            foreach (var word in words)
+            {
+                var firstChar = char.ToLower(word[0]);
+                var line = charMap[firstChar - 'a'];
+                var wordLength = word.Length;
+                var match = true;
+                for (var i = 1; i < wordLength; i++)
+                {
+                    var c = char.ToLower(word[i]);
+                    if (charMap[c - 'a'] != line)
+                    {
+                        match = false;
+                        break;
+                    }
+                }
+
+                if (match)
+                {
+                    result.Add(word);
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ src/LeetCode500/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode500
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { "Hello", "Alaska", "Dad", "Peace" }, new[] { "Alaska", "Dad" })]
+        [InlineData(new[] { "omk" }, new string[] { })]
+        [InlineData(new[] { "adsdf", "sfd" }, new[] { "adsdf", "sfd" })]
+        public void Test(string[] words, string[] assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.FindWords(words);
+            result.Should().Equal(assertResult);
+        }
+    }
+}
