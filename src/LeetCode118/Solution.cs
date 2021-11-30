487e4001219d4adc8fda34b509953b2f7fcb57c7
h82258652 <842053625@qq.com>
2021-11-30 21:19:21 +0800

    submit 118 solution

---
+++ src/LeetCode118/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode118
+{
+    public class Solution
+    {
+        public IList<IList<int>> Generate(int numRows)
+        {
+            var result = new IList<int>[numRows];
+            for (var i = 0; i < numRows; i++)
+            {
+                result[i] = new List<int>();
+            }
+            result[0].Add(1);
+
+            for (var i = 1; i < numRows; i++)
+            {
+                var previousRow = result[i - 1];
+                var row = result[i];
+                for (var j = 0; j < previousRow.Count; j++)
+                {
+                    var temp = previousRow[j];
+                    if (j == 0)
+                    {
+                        row.Add(temp);
+                    }
+                    else
+                    {
+                        row.Add(temp + previousRow[j - 1]);
+                    }
+                }
+                row.Add(1);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode118/TestCases.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode118
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(5, "[[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]")]
+        [InlineData(1, "[[1]]")]
+        public void Test(int numRows, string assertResultJson)
+        {
+            var assertResult = JsonSerializer.Deserialize<IList<IList<int>>>(assertResultJson);
+            var solution = new Solution();
+            var result = solution.Generate(numRows);
+            result.Should().BeEquivalentTo(assertResult);
+        }
+    }
+}
