9b595b993bacf17064d0c220bb3be0d1b962c298
h82258652 <842053625@qq.com>
2021-11-30 22:00:15 +0800

    submit 021 solution

---
+++ src/LeetCode021/Solution.cs
@@ -0,0 +1,79 @@
+﻿namespace LeetCode021
+{
+    public class Solution
+    {
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            ListNode result = null;
+            ListNode node = null;
+
+            while (list1 != null && list2 != null)
+            {
+                if (list1.val <= list2.val)
+                {
+                    if (result == null)
+                    {
+                        result = new ListNode(list1.val);
+                        node = result;
+                    }
+                    else
+                    {
+                        node.next = new ListNode(list1.val);
+                        node = node.next;
+                    }
+
+                    list1 = list1.next;
+                }
+                else
+                {
+                    if (result == null)
+                    {
+                        result = new ListNode(list2.val);
+                        node = result;
+                    }
+                    else
+                    {
+                        node.next = new ListNode(list2.val);
+                        node = node.next;
+                    }
+
+                    list2 = list2.next;
+                }
+            }
+
+            while (list1 != null)
+            {
+                if (node == null)
+                {
+                    node = new ListNode(list1.val);
+                    result = node;
+                }
+                else
+                {
+                    node.next = new ListNode(list1.val);
+                    node = node.next;
+                }
+
+                list1 = list1.next;
+            }
+
+            while (list2 != null)
+            {
+                if (node == null)
+                {
+                    node = new ListNode(list2.val);
+                    result = node;
+                }
+                else
+                {
+                    node.next = new ListNode(list2.val);
+                    node = node.next;
+                }
+
+                list2 = list2.next;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode021/TestCases.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode021
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 4 }, new[] { 1, 3, 4 }, new[] { 1, 1, 2, 3, 4, 4 })]
+        [InlineData(new int[] { }, new int[] { }, new int[] { })]
+        [InlineData(new int[] { }, new[] { 0 }, new[] { 0 })]
+        public void Test(int[] list1Array, int[] list2Array, int[] assertResultArray)
+        {
+            var list1 = ListNode.Create(list1Array);
+            var list2 = ListNode.Create(list2Array);
+            var solution = new Solution();
+            var result = solution.MergeTwoLists(list1, list2);
+            (result?.ToArray() ?? Array.Empty<int>()).Should().Equal(assertResultArray);
+        }
+    }
+}
