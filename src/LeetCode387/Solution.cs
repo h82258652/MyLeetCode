941b189c7ae45e1ea535e5b9d9a622f0df52fb8d
h82258652 <842053625@qq.com>
2021-11-30 14:23:41 +0800

    submit 387 solution

---
+++ src/LeetCode387/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode387
+{
+    public class Solution
+    {
+        public int FirstUniqChar(string s)
+        {
+            var charCount = new int[26];
+            foreach (var c in s)
+            {
+                charCount[c - 'a']++;
+            }
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                var c = s[i];
+                if (charCount[c - 'a'] > 1)
+                {
+                    continue;
+                }
+
+                return i;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ src/LeetCode387/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode387
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("leetcode", 0)]
+        [InlineData("loveleetcode", 2)]
+        public void Test(string s, int assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.FirstUniqChar(s);
+            result.Should().Be(assertResult);
+        }
+    }
+}
