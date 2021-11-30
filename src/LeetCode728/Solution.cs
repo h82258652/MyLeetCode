e490cde2657298aec8b0196b9972d0cd15975266
h82258652 <842053625@qq.com>
2021-11-30 16:49:23 +0800

    submit 728 solution

---
+++ src/LeetCode728/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode728
+{
+    public class Solution
+    {
+        public IList<int> SelfDividingNumbers(int left, int right)
+        {
+            var result = new List<int>();
+            for (var i = left; i <= right; i++)
+            {
+                if (IsDividingNumber(i))
+                {
+                    result.Add(i);
+                }
+            }
+
+            return result;
+        }
+
+        private bool IsDividingNumber(int num)
+        {
+            var temp = num;
+            while (temp > 0)
+            {
+                var mod = temp % 10;
+                if (mod == 0)
+                {
+                    return false;
+                }
+
+                if (num % mod != 0)
+                {
+                    return false;
+                }
+
+                temp /= 10;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/LeetCode728/TestCases.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode728
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(1, 22, new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22 })]
+        public void Test(int left, int right, int[] assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.SelfDividingNumbers(left, right);
+            result.Should().Equal(assertResult);
+        }
+    }
+}
