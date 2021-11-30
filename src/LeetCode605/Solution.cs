6f2d8a5b66332159e111759b9d0454cccd1eefa1
h82258652 <842053625@qq.com>
2021-11-30 16:37:00 +0800

    submit 605 solution

---
+++ src/LeetCode605/Solution.cs
@@ -0,0 +1,49 @@
+﻿namespace LeetCode605
+{
+    public class Solution
+    {
+        public bool CanPlaceFlowers(int[] flowerbed, int n)
+        {
+            var length = flowerbed.Length;
+
+            int previous = 0;
+            for (var i = 0; i < length; i++)
+            {
+                var bed = flowerbed[i];
+                if (bed == 1 || previous == 1)
+                {
+                    previous = bed;
+                    continue;
+                }
+
+                if (i < length - 1)
+                {
+                    var nextBed = flowerbed[i + 1];
+                    if (nextBed == 1)
+                    {
+                        previous = nextBed;
+                        i++;
+                        continue;
+                    }
+                    else
+                    {
+                        flowerbed[i] = 1;
+                        n--;
+                        previous = 1;
+                    }
+                }
+                else
+                {
+                    n--;
+                }
+
+                if (n <= 0)
+                {
+                    break;
+                }
+            }
+
+            return n <= 0;
+        }
+    }
+}
---
+++ src/LeetCode605/TestCases.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode605
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1, 0, 0, 0, 1 }, 1, true)]
+        [InlineData(new[] { 1, 0, 0, 0, 1 }, 2, false)]
+        [InlineData(new[] { 1, 0, 0, 0, 0, 1 }, 2, false)]
+        [InlineData(new[] { 0, 0, 1, 0, 0 }, 1, true)]
+        [InlineData(new[] { 0, 0 }, 2, false)]
+        public void Test(int[] flowerbed, int n, bool assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.CanPlaceFlowers(flowerbed, n);
+            result.Should().Be(assertResult);
+        }
+    }
+}
