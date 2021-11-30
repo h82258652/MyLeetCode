ae1ed24ec4f07c33c8eff3a929ef15a3bab34e6d
h82258652 <842053625@qq.com>
2021-11-30 15:13:58 +0800

    submit 434 solution

---
+++ src/LeetCode434/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode434
+{
+    public class Solution
+    {
+        public int CountSegments(string s)
+        {
+            char preChar = ' ';
+            int result = 0;
+            foreach (var c in s)
+            {
+                if (c == ' ' && preChar != ' ')
+                {
+                    result++;
+                }
+
+                preChar = c;
+            }
+
+            if (preChar != ' ')
+            {
+                result++;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode434/TestCases.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode434
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("Hello, my name is John", 5)]
+        public void Test(string s, int assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.CountSegments(s);
+            result.Should().Be(assertResult);
+        }
+    }
+}
