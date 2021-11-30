321702ce7828a4b4efc8d5a81af30d5824c989d6
h82258652 <842053625@qq.com>
2021-11-30 09:30:28 +0800

    submit 349 solution

---
+++ src/LeetCode349/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode349
+{
+    public class Solution
+    {
+        public int[] Intersection(int[] nums1, int[] nums2)
+        {
+            var set1 = new HashSet<int>(nums1);
+            var set2 = new HashSet<int>(nums2);
+
+            var result = new List<int>();
+            foreach (var i in set1)
+            {
+                if (set2.Contains(i))
+                {
+                    result.Add(i);
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
