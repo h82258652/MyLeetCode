ca1cbaaddad2714f80e55595b62b65f8c9fddbe9
h82258652 <842053625@qq.com>
2021-11-30 23:14:08 +0800

    submit 1154 solution

---
+++ src/LeetCode1154/Solution.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace LeetCode1154
+{
+    public class Solution
+    {
+        public int DayOfYear(string date)
+        {
+            var year = int.Parse(date.Substring(0, 4));
+            var month = int.Parse(date.Substring(5, 2));
+            var day = int.Parse(date.Substring(8, 2));
+
+            var result = day;
+
+            for (var i = 1; i < month; i++)
+            {
+                result += GetDaysInMonth(year, i);
+            }
+
+            return result;
+        }
+
+        private int GetDaysInMonth(int year, int month)
+        {
+            switch (month)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    return 31;
+
+                case 2:
+                    return IsRunNian(year) ? 29 : 28;
+
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    return 30;
+
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+
+        private bool IsRunNian(int year)
+        {
+            if (year % 400 == 0)
+            {
+                return true;
+            }
+
+            if (year % 100 == 0)
+            {
+                return false;
+            }
+
+            if (year % 4 == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ src/LeetCode1154/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1154
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("2019-01-09", 9)]
+        [InlineData("2019-02-10", 41)]
+        [InlineData("2003-03-01", 60)]
+        [InlineData("2004-03-01", 61)]
+        public void Test(string date, int assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.DayOfYear(date);
+            result.Should().Be(assertResult);
+        }
+    }
+}
