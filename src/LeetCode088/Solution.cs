2dd2331e2372f179485b2735d89aa1e8f238c752
h82258652 <842053625@qq.com>
2021-11-30 08:58:10 +0800

    submit 088 solution

---
+++ src/LeetCode088/Solution.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace LeetCode088
+{
+    public class Solution
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            int[] result = new int[m + n];
+            int i = 0;
+            int j = 0;
+            int k = 0;
+            while (true)
+            {
+                if (i >= m)
+                {
+                    break;
+                }
+
+                if (j >= n)
+                {
+                    break;
+                }
+
+                var num1 = nums1[i];
+                var num2 = nums2[j];
+                if (num1 < num2)
+                {
+                    result[k] = num1;
+                    i++;
+                    k++;
+                }
+                else
+                {
+                    result[k] = num2;
+                    j++;
+                    k++;
+                }
+            }
+
+            if (i < m)
+            {
+                for (int temp = i; temp < m; temp++)
+                {
+                    result[k] = nums1[temp];
+                    k++;
+                }
+            }
+
+            if (j < n)
+            {
+                for (int temp = j; temp < n; temp++)
+                {
+                    result[k] = nums2[temp];
+                    k++;
+                }
+            }
+
+            Array.Copy(result, nums1, m + n);
+        }
+    }
+}
---
+++ src/LeetCode088/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode088
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 0, 0, 0 }, 3, new[] { 2, 5, 6 }, 3, new[] { 1, 2, 2, 3, 5, 6 })]
+        [InlineData(new[] { 1 }, 1, new int[] { }, 0, new[] { 1 })]
+        [InlineData(new[] { 0 }, 0, new[] { 1 }, 1, new[] { 1 })]
+        [InlineData(new[] { 4, 5, 6, 0, 0, 0 }, 3, new[] { 1, 2, 3 }, 3, new[] { 1, 2, 3, 4, 5, 6 })]
+        public void Test(int[] nums1, int m, int[] nums2, int n, int[] assertResult)
+        {
+            var solution = new Solution();
+            solution.Merge(nums1, m, nums2, n);
+            nums1.Should().Equal(assertResult);
+        }
+    }
+}
