3d6195e4a2efcf653c86c2e4d74cd7104be3c9ed
h82258652 <842053625@qq.com>
2021-11-30 13:00:24 +0800

    submit 977 solution

---
+++ src/LeetCode977/Solution.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace LeetCode977
+{
+    public class Solution
+    {
+        public int[] SortedSquares(int[] nums)
+        {
+            return nums.Select(temp => temp * temp).OrderBy(temp => temp).ToArray();
+        }
+    }
+}
---
+++ src/LeetCode977/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode977
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { -4, -1, 0, 3, 10 }, new[] { 0, 1, 9, 16, 100 })]
+        [InlineData(new[] { -7, -3, 2, 3, 11 }, new[] { 4, 9, 9, 49, 121 })]
+        public void Test(int[] nums, int[] assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.SortedSquares(nums);
+            result.Should().Equal(assertResult);
+        }
+    }
+}
