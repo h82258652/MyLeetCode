3554b65f3c8e36480492e5a106a8fd47b8439f93
h82258652 <842053625@qq.com>
2021-11-30 09:19:27 +0800

    submit 169 solution

---
+++ src/LeetCode169/Solution.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace LeetCode169
+{
+    public class Solution
+    {
+        public int MajorityElement(int[] nums)
+        {
+            return nums.GroupBy(temp => temp).OrderByDescending(temp => temp.Count()).First().Key;
+        }
+    }
+}
---
+++ src/LeetCode169/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode169
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 3, 2, 3 }, 3)]
+        [InlineData(new[] { 2, 2, 1, 1, 1, 2, 2 }, 2)]
+        [InlineData(new[] { -1, 1, 1, 1, 2, 1 }, 1)]
+        public void Test(int[] nums, int assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.MajorityElement(nums);
+            result.Should().Be(assertResult);
+        }
+    }
+}
