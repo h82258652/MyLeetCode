175bf056c3f0be22f8933b2973e4d43b2889fad5
h82258652 <842053625@qq.com>
2021-11-30 22:11:50 +0800

    submit 067 solution

---
+++ src/LeetCode067/Solution.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+
+namespace LeetCode067
+{
+    public class Solution
+    {
+        public string AddBinary(string a, string b)
+        {
+            var result = new StringBuilder();
+
+            var plus = 0;
+
+            int i = a.Length - 1;
+            int j = b.Length - 1;
+
+            while (i >= 0 || j >= 0)
+            {
+                var bit = plus;
+
+                if (i >= 0)
+                {
+                    if (a[i] == '1')
+                    {
+                        bit++;
+                    }
+                }
+
+                if (j >= 0)
+                {
+                    if (b[j] == '1')
+                    {
+                        bit++;
+                    }
+                }
+
+                plus = bit > 1 ? 1 : 0;
+
+                if (bit % 2 == 0)
+                {
+                    result.Insert(0, '0');
+                }
+                else
+                {
+                    result.Insert(0, '1');
+                }
+
+                i--;
+                j--;
+            }
+
+            if (plus > 0)
+            {
+                result.Insert(0, '1');
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ src/LeetCode067/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode067
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("11", "1", "100")]
+        [InlineData("1010", "1011", "10101")]
+        public void Test(string a, string b, string assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.AddBinary(a, b);
+            result.Should().Be(assertResult);
+        }
+    }
+}
