a241f6041c38ecc0ef2b48fe72abd876a8ab5e8a
h82258652 <842053625@qq.com>
2021-11-30 21:28:10 +0800

    submit 119 solution

---
+++ src/LeetCode119/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode119
+{
+    public class Solution
+    {
+        public IList<int> GetRow(int rowIndex)
+        {
+            var result = new List<int>();
+
+            for (var i = 0; i <= rowIndex; i++)
+            {
+                var tempRow = new List<int>();
+
+                for (var j = 0; j < result.Count; j++)
+                {
+                    if (j == 0)
+                    {
+                        tempRow.Add(result[j]);
+                    }
+                    else
+                    {
+                        tempRow.Add(result[j] + result[j - 1]);
+                    }
+                }
+
+                tempRow.Add(1);
+
+                result = tempRow;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode119/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode119
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(3, new[] { 1, 3, 3, 1 })]
+        [InlineData(0, new[] { 1 })]
+        [InlineData(1, new[] { 1, 1 })]
+        public void Test(int rowIndex, int[] assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.GetRow(rowIndex);
+            result.Should().Equal(assertResult);
+        }
+    }
+}
