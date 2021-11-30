a47c3586fe63557428ec143cd990cc6c6f492a27
h82258652 <842053625@qq.com>
2021-11-30 18:03:55 +0800

    submit 206 solution

---
+++ src/LeetCode206/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode206
+{
+    public class Solution
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            var node = head;
+            ListNode previous = null;
+            while (node != null)
+            {
+                var next = node.next;
+                node.next = previous;
+                previous = node;
+                node = next;
+            }
+
+            return previous;
+        }
+    }
+}
---
+++ src/LeetCode206/TestCases.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode206
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 4, 5 }, new[] { 5, 4, 3, 2, 1 })]
+        [InlineData(new[] { 1, 2 }, new[] { 2, 1 })]
+        [InlineData(new int[] { }, new int[] { })]
+        public void Test(int[] headArray, int[] assertResult)
+        {
+            var head = ListNode.Create(headArray);
+            var solution = new Solution();
+            var result = solution.ReverseList(head);
+            (result?.ToArray() ?? Array.Empty<int>()).Should().Equal(assertResult);
+        }
+    }
+}
