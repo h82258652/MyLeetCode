265036783cd427d6ab7db7045f8c73d67c984c1b
h82258652 <842053625@qq.com>
2021-11-30 13:56:37 +0800

    submit 205 solution

---
+++ src/LeetCode205/Solution.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode205
+{
+    public class Solution
+    {
+        public bool IsIsomorphic(string s, string t)
+        {
+            var length = s.Length;
+            var sToTMap = new Dictionary<char, char>();
+            var tToSMap = new Dictionary<char, char>();
+            for (var i = 0; i < length; i++)
+            {
+                var sc = s[i];
+                var tc = t[i];
+
+                if (sToTMap.TryGetValue(sc, out var exist))
+                {
+                    if (exist != tc)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    sToTMap[sc] = tc;
+                }
+
+                if (tToSMap.TryGetValue(tc, out var exist2))
+                {
+                    if (exist2 != sc)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    tToSMap[tc] = sc;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/LeetCode205/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode205
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("egg", "add", true)]
+        [InlineData("foo", "bar", false)]
+        [InlineData("paper", "title", true)]
+        [InlineData("badc", "baba", false)]
+        public void Test(string s, string t, bool assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.IsIsomorphic(s, t);
+            result.Should().Be(assertResult);
+        }
+    }
+}
