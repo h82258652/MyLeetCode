38314a2be47b0739924f14783266c71458ec42c1
h82258652 <842053625@qq.com>
2021-11-30 12:22:20 +0800

    submit 290 solution

---
+++ src/LeetCode290/Solution.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode290
+{
+    public class Solution
+    {
+        public bool WordPattern(string pattern, string s)
+        {
+            var words = s.Split(' ');
+            if (pattern.Length != words.Length)
+            {
+                return false;
+            }
+
+            var patternWordMap = new Dictionary<char, string>();
+            var wordPatternMap = new Dictionary<string, char>();
+
+            var patternLength = pattern.Length;
+
+            for (var i = 0; i < patternLength; i++)
+            {
+                var c = pattern[i];
+                var word = words[i];
+
+                if (patternWordMap.ContainsKey(c))
+                {
+                    if (patternWordMap[c] != word)
+                    {
+                        return false;
+                    }
+                }
+
+                if (wordPatternMap.ContainsKey(word))
+                {
+                    if (wordPatternMap[word] != c)
+                    {
+                        return false;
+                    }
+                }
+
+                patternWordMap[c] = word;
+                wordPatternMap[word] = c;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/LeetCode290/TestCases.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode290
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("abba", "dog cat cat dog", true)]
+        [InlineData("abba", "dog cat cat fish", false)]
+        [InlineData("aaaa", "dog cat cat dog", false)]
+        [InlineData("abba", "dog dog dog dog", false)]
+        [InlineData("aaa", "aa aa aa aa", false)]
+        public void Test(string pattern, string s, bool assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.WordPattern(pattern, s);
+            result.Should().Be(assertResult);
+        }
+    }
+}
