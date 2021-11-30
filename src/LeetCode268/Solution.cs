711a268997abcf8d60288c8a4cc4f9032f9eddca
h82258652 <842053625@qq.com>
2021-11-30 14:06:19 +0800

    submit 268 solution

---
+++ src/LeetCode268/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode268
+{
+    public class Solution
+    {
+        public int MissingNumber(int[] nums)
+        {
+            var length = nums.Length;
+            var result = length * (length + 1) / 2;
+            foreach (var num in nums)
+            {
+                result -= num;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode268/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode268
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 3, 0, 1 }, 2)]
+        [InlineData(new[] { 0, 1 }, 2)]
+        [InlineData(new[] { 9, 6, 4, 2, 3, 5, 7, 0, 1 }, 8)]
+        [InlineData(new[] { 0 }, 1)]
+        public void Test(int[] nums, int assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.MissingNumber(nums);
+            result.Should().Be(assertResult);
+        }
+    }
+}
