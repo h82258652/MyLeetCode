3406427e020c946a914df228de11fd79f5df97d4
h82258652 <842053625@qq.com>
2021-11-30 14:17:11 +0800

    submit 389 solution

---
+++ src/LeetCode389/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LeetCode389
+{
+    public class Solution
+    {
+        public char FindTheDifference(string s, string t)
+        {
+            var tArray = BuildCharCountArray(t);
+            var sArray = BuildCharCountArray(s);
+            for (var i = 0; i < 26; i++)
+            {
+                if (tArray[i] != sArray[i])
+                {
+                    return (char)(i + 'a');
+                }
+            }
+
+            throw new InvalidOperationException();
+        }
+
+        private int[] BuildCharCountArray(string s)
+        {
+            var charCountArray = new int[26];
+            foreach (var c in s)
+            {
+                charCountArray[c - 'a']++;
+            }
+
+            return charCountArray;
+        }
+    }
+}
---
+++ src/LeetCode389/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode389
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("abcd", "abcde", 'e')]
+        [InlineData("", "y", 'y')]
+        [InlineData("a", "aa", 'a')]
+        [InlineData("ae", "aea", 'a')]
+        public void Test(string s, string t, char assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.FindTheDifference(s, t);
+            result.Should().Be(assertResult);
+        }
+    }
+}
