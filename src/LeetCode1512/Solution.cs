84534c9b106ba61b2e0a63e6b306c311d9deca2f
h82258652 <842053625@qq.com>
2021-11-30 14:42:26 +0800

    submit 1512 solution

---
+++ src/LeetCode1512/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode1512
+{
+    public class Solution
+    {
+        public int NumIdenticalPairs(int[] nums)
+        {
+            var length = nums.Length;
+            var result = 0;
+            for (int i = 0; i < length; i++)
+            {
+                var num = nums[i];
+                for (int j = i + 1; j < length; j++)
+                {
+                    if (num == nums[j])
+                    {
+                        result++;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode1512/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1512
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 1, 1, 3 }, 4)]
+        [InlineData(new[] { 1, 1, 1, 1 }, 6)]
+        [InlineData(new[] { 1, 2, 3 }, 0)]
+        public void Test(int[] nums, int assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.NumIdenticalPairs(nums);
+            result.Should().Be(assertResult);
+        }
+    }
+}
