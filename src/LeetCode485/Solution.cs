3404064c3fcaabb422edfb40b0468eee2812a632
h82258652 <842053625@qq.com>
2021-11-30 15:28:34 +0800

    submit 485 solution

---
+++ src/LeetCode485/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeetCode485
+{
+    public class Solution
+    {
+        public int FindMaxConsecutiveOnes(int[] nums)
+        {
+            var result = 0;
+            var length = nums.Length;
+            var temp = 0;
+            for (var i = 0; i < length; i++)
+            {
+                var num = nums[i];
+                if (num == 1)
+                {
+                    temp++;
+                }
+                else
+                {
+                    result = Math.Max(result, temp);
+                    temp = 0;
+                }
+            }
+
+            result = Math.Max(result, temp);
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode485/TestCases.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode485
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1, 1, 0, 1, 1, 1 }, 3)]
+        public void Test(int[] nums, int assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.FindMaxConsecutiveOnes(nums);
+            result.Should().Be(assertResult);
+        }
+    }
+}
