a346cf864cbed1d1b5f3fbda9d59eb32f5e849f1
h82258652 <842053625@qq.com>
2021-11-30 11:16:49 +0800

    submit 019 solution

---
+++ src/LeetCode019/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode019
+{
+    public class Solution
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            var length = 0;
+            var node = head;
+            while (node != null)
+            {
+                length++;
+                node = node.next;
+            }
+
+            if (length == n)
+            {
+                return head.next;
+            }
+
+            node = head;
+            var move = length - n;
+            while (move > 1)
+            {
+                node = node.next;
+                move--;
+            }
+
+            node.next = node.next.next;
+
+            return head;
+        }
+    }
+}
---
+++ src/LeetCode019/TestCases.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode019
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 4, 5 }, 2, new[] { 1, 2, 3, 5 })]
+        [InlineData(new[] { 1 }, 1, new int[] { })]
+        [InlineData(new[] { 1, 2 }, 1, new[] { 1 })]
+        [InlineData(new[] { 1, 2 }, 2, new[] { 2 })]
+        public void Test(int[] headArray, int n, int[] assertResult)
+        {
+            var head = ListNode.Create(headArray);
+            var solution = new Solution();
+            var result = solution.RemoveNthFromEnd(head, n);
+            (result?.ToArray() ?? Array.Empty<int>()).Should().Equal(assertResult);
+        }
+    }
+}
