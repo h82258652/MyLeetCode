4500c3510fc1347a8d80d9e84ed4f1cfc410b778
h82258652 <842053625@qq.com>
2021-11-30 14:35:34 +0800

    submit 645 solution

---
+++ src/LeetCode645/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode645
+{
+    public class Solution
+    {
+        public int[] FindErrorNums(int[] nums)
+        {
+            var repeatNum = 0;
+            var length = nums.Length;
+
+            var set = new HashSet<int>();
+            foreach (var num in nums)
+            {
+                if (set.Contains(num))
+                {
+                    repeatNum = num;
+                }
+                else
+                {
+                    set.Add(num);
+                }
+            }
+
+            var sum = length * (length + 1) / 2;
+            var missingNum = sum - set.Sum();
+            return new[] { repeatNum, missingNum };
+        }
+    }
+}
---
+++ src/LeetCode645/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode645
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 2, 4 }, new[] { 2, 3 })]
+        [InlineData(new[] { 1, 1 }, new[] { 1, 2 })]
+        public void Test(int[] nums, int[] assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.FindErrorNums(nums);
+            result.Should().Equal(assertResult);
+        }
+    }
+}
