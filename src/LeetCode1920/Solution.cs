bd44009f43db79c163e6b6073b2cff9d6e464d1d
h82258652 <842053625@qq.com>
2021-12-01 13:36:49 +0800

    submit 1920 solution

---
+++ src/LeetCode1920/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode1920
+{
+    public class Solution
+    {
+        public int[] BuildArray(int[] nums)
+        {
+            var length = nums.Length;
+            var result = new int[length];
+            for (var i = 0; i < length; i++)
+            {
+                result[i] = nums[nums[i]];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode1920/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1920
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 0, 2, 1, 5, 3, 4 }, new[] { 0, 1, 2, 4, 5, 3 })]
+        [InlineData(new[] { 5, 0, 1, 2, 3, 4 }, new[] { 4, 5, 0, 1, 2, 3 })]
+        public void Test(int[] nums, int[] assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.BuildArray(nums);
+            result.Should().Equal(assertResult);
+        }
+    }
+}
