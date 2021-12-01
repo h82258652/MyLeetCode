a36c3d2abb112be7088ae8fc7027934cc6a5071f
h82258652 <842053625@qq.com>
2021-12-01 09:01:23 +0800

    submit 1446 solution

---
+++ src/LeetCode1446/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LeetCode1446
+{
+    public class Solution
+    {
+        public int MaxPower(string s)
+        {
+            var result = 1;
+            var currentCount = 1;
+            var previousChar = s[0];
+            var sLength = s.Length;
+            for (var i = 1; i < sLength; i++)
+            {
+                var c = s[i];
+                if (c != previousChar)
+                {
+                    result = Math.Max(result, currentCount);
+                    currentCount = 1;
+                    previousChar = c;
+                }
+                else
+                {
+                    currentCount++;
+                }
+            }
+
+            result = Math.Max(result, currentCount);
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode1446/TestCases.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1446
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("leetcode", 2)]
+        [InlineData("abbcccddddeeeeedcba", 5)]
+        [InlineData("triplepillooooow", 5)]
+        [InlineData("hooraaaaaaaaaaay", 11)]
+        [InlineData("tourist", 1)]
+        public void Test(string s, int assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.MaxPower(s);
+            result.Should().Be(assertResult);
+        }
+    }
+}
