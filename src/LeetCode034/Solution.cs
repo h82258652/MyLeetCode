1041276d0da4f76e9acb753673c3d6e3e0641b82
h82258652 <842053625@qq.com>
2021-12-01 17:48:32 +0800

    submit 034 solution

---
+++ src/LeetCode034/Solution.cs
@@ -0,0 +1,69 @@
+﻿namespace LeetCode034
+{
+    public class Solution
+    {
+        public int[] SearchRange(int[] nums, int target)
+        {
+            var length = nums.Length;
+            var result = InternalSearch(nums, target);
+            if (result == -1)
+            {
+                return new[] { -1, -1 };
+            }
+
+            var first = result;
+            while (first > 0)
+            {
+                if (nums[first - 1] == target)
+                {
+                    first--;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            var last = result;
+            while (last < length - 1)
+            {
+                if (nums[last + 1] == target)
+                {
+                    last++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return new[] { first, last };
+        }
+
+        private int InternalSearch(int[] nums, int target)
+        {
+            int low = 0;
+            int high = nums.Length - 1;
+            while (low <= high)
+            {
+                var midIndex = (low + high) / 2;
+                var midValue = nums[midIndex];
+                if (midValue == target)
+                {
+                    return midIndex;
+                }
+
+                if (midValue < target)
+                {
+                    low = midIndex + 1;
+                }
+                else
+                {
+                    high = midIndex - 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ src/LeetCode034/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode034
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 5, 7, 7, 8, 8, 10 }, 8, new[] { 3, 4 })]
+        [InlineData(new[] { 5, 7, 7, 8, 8, 10 }, 6, new[] { -1, -1 })]
+        [InlineData(new int[] { }, 0, new[] { -1, -1 })]
+        [InlineData(new[] { 1 }, 1, new[] { 0, 0 })]
+        public void Test(int[] nums, int target, int[] assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.SearchRange(nums, target);
+            result.Should().Equal(assertResult);
+        }
+    }
+}
