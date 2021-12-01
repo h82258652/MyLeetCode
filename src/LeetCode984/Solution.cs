8e32d0d2bdf5cbe66831c0b0b76d57961481a6e2
h82258652 <842053625@qq.com>
2021-12-01 11:02:46 +0800

    submit 984 solution

---
+++ src/LeetCode984/Solution.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace LeetCode984
+{
+    public class Solution
+    {
+        public string StrWithout3a3b(int a, int b)
+        {
+            var result = new StringBuilder(a + b);
+            char? lastChar = null;
+            while (a >= 0 || b >= 0)
+            {
+                if (a <= 0)
+                {
+                    result.Append('b', b);
+                    break;
+                }
+
+                if (b <= 0)
+                {
+                    result.Append('a', a);
+                    break;
+                }
+
+                if (a > b)
+                {
+                    result.Append("aab");
+                    a -= 2;
+                    b--;
+                    lastChar = 'b';
+                }
+                else if (a < b)
+                {
+                    result.Append("bba");
+                    b -= 2;
+                    a--;
+                    lastChar = 'a';
+                }
+                else
+                {
+                    if (lastChar.HasValue && lastChar.Value == 'a')
+                    {
+                        result.Append(string.Concat(Enumerable.Repeat("ba", a)));
+                        break;
+                    }
+                    else
+                    {
+                        result.Append(string.Concat(Enumerable.Repeat("ab", a)));
+                        break;
+                    }
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ src/LeetCode984/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode984
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(1, 2)]
+        [InlineData(4, 1)]
+        public void Test(int a, int b)
+        {
+            var solution = new Solution();
+            var result = solution.StrWithout3a3b(a, b);
+            result.Should().NotContainAny("aaa", "bbb");
+        }
+    }
+}
