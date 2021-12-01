c93a37aa3cd17f6e8f7abc24a0edfa53bb61a06c
h82258652 <842053625@qq.com>
2021-12-01 14:15:08 +0800

    submit 452 solution

---
+++ src/LeetCode452/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+
+namespace LeetCode452
+{
+    public class Solution
+    {
+        public int FindMinArrowShots(int[][] points)
+        {
+            points = points.OrderBy(temp => temp[1]).ToArray();
+
+            var result = 0;
+            var length = points.Length;
+            for (var i = 0; i < length; i++)
+            {
+                var right = points[i][1];
+
+                var j = i + 1;
+                for (; j < length;)
+                {
+                    var left = points[j][0];
+                    if (left <= right)
+                    {
+                        j++;
+                        continue;
+                    }
+
+                    j--;
+                    break;
+                }
+
+                i = j;
+                result++;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode452/TestCases.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode452
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("[[10,16],[2,8],[1,6],[7,12]]", 2)]
+        [InlineData("[[1,2],[3,4],[5,6],[7,8]]", 4)]
+        [InlineData("[[1,2],[2,3],[3,4],[4,5]]", 2)]
+        [InlineData("[[1,2]]", 1)]
+        [InlineData("[[2,3],[2,3]]", 1)]
+        public void Test(string pointsJson, int assertResult)
+        {
+            var points = JsonSerializer.Deserialize<int[][]>(pointsJson);
+            var solution = new Solution();
+            var result = solution.FindMinArrowShots(points);
+            result.Should().Be(assertResult);
+        }
+    }
+}
