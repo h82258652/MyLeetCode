dc53af6d503d29b5ccb2b003e77f18ad92bd6464
h82258652 <842053625@qq.com>
2021-12-01 13:03:19 +0800

    submit 1441 solution

---
+++ src/LeetCode1441/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode1441
+{
+    public class Solution
+    {
+        public IList<string> BuildArray(int[] target, int n)
+        {
+            var result = new List<string>();
+
+            var targetLength = target.Length;
+            var targetIndex = 0;
+            for (var i = 1; i <= n; i++)
+            {
+                if (target[targetIndex] == i)
+                {
+                    result.Add("Push");
+                    targetIndex++;
+
+                    if (targetIndex == targetLength)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    result.Add("Push");
+                    result.Add("Pop");
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode1441/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1441
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1, 3 }, 3, new[] { "Push", "Push", "Pop", "Push" })]
+        [InlineData(new[] { 1, 2, 3 }, 3, new[] { "Push", "Push", "Push" })]
+        [InlineData(new[] { 1, 2 }, 4, new[] { "Push", "Push" })]
+        [InlineData(new[] { 2, 3, 4 }, 4, new[] { "Push", "Pop", "Push", "Push", "Push" })]
+        public void Test(int[] target, int n, string[] assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.BuildArray(target, n);
+            result.Should().Equal(assertResult);
+        }
+    }
+}
