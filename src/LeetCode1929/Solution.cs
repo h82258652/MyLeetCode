1ba119297c184d91b947a0bcaaa63171eb9af669
h82258652 <842053625@qq.com>
2021-12-01 13:42:36 +0800

    submit 1929 solution

---
+++ src/LeetCode1929/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LeetCode1929
+{
+    public class Solution
+    {
+        public int[] GetConcatenation(int[] nums)
+        {
+            var length = nums.Length;
+            var result = new int[length << 1];
+            Array.Copy(nums, 0, result, 0, length);
+            Array.Copy(nums, 0, result, length, length);
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode1929/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1929
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 1 }, new[] { 1, 2, 1, 1, 2, 1 })]
+        [InlineData(new[] { 1, 3, 2, 1 }, new[] { 1, 3, 2, 1, 1, 3, 2, 1 })]
+        public void Test(int[] nums, int[] assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.GetConcatenation(nums);
+            result.Should().Equal(assertResult);
+        }
+    }
+}
