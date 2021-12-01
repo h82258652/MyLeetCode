6f323572203d3c8b06df0bddee53254c09ae26a6
h82258652 <842053625@qq.com>
2021-12-01 22:37:18 +0800

    submit 322 solution

---
+++ src/LeetCode322/Solution.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace LeetCode322
+{
+    public class Solution
+    {
+        public int CoinChange(int[] coins, int amount)
+        {
+            var counts = new int[amount + 1];
+            Array.Fill(counts, -1);
+            counts[0] = 0;
+            foreach (var coin in coins)
+            {
+                if (coin < counts.Length)
+                {
+                    counts[coin] = 1;
+                }
+            }
+
+            for (var i = 1; i <= amount; i++)
+            {
+                if (counts[i] >= 0)
+                {
+                    continue;
+                }
+
+                var count = -1;
+                foreach (var coin in coins)
+                {
+                    if (i - coin < 0)
+                    {
+                        continue;
+                    }
+
+                    var temp = counts[i - coin];
+                    if (temp == -1)
+                    {
+                        continue;
+                    }
+
+                    if (count == -1)
+                    {
+                        count = temp + 1;
+                    }
+                    else
+                    {
+                        count = Math.Min(count, temp + 1);
+                    }
+                }
+
+                if (count != -1)
+                {
+                    counts[i] = count;
+                }
+            }
+
+            return counts[amount];
+        }
+    }
+}
---
+++ src/LeetCode322/TestCases.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode322
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 5 }, 11, 3)]
+        [InlineData(new[] { 2 }, 3, -1)]
+        [InlineData(new[] { 1 }, 0, 0)]
+        [InlineData(new[] { 1 }, 1, 1)]
+        [InlineData(new[] { 1 }, 2, 2)]
+        [InlineData(new[] { 1, 2, 5 }, 100, 20)]
+        public void Test(int[] coins, int amount, int assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.CoinChange(coins, amount);
+            result.Should().Be(assertResult);
+        }
+    }
+}
