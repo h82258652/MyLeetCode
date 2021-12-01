c36580bfe6cc0809381f9b8505042af8c132f4b0
h82258652 <842053625@qq.com>
2021-12-01 13:18:07 +0800

    submit 1160 solution

---
+++ src/LeetCode1160/Solution.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode1160
+{
+    public class Solution
+    {
+        public int CountCharacters(string[] words, string chars)
+        {
+            var charsCount = new int[26];
+            foreach (var c in chars)
+            {
+                charsCount[c - 'a']++;
+            }
+
+            var result = 0;
+            foreach (var word in words)
+            {
+                var wordCharsCount = new int[26];
+                foreach (var c in word)
+                {
+                    wordCharsCount[c - 'a']++;
+                }
+
+                var learn = true;
+
+                for (var i = 0; i < 26; i++)
+                {
+                    if (charsCount[i] < wordCharsCount[i])
+                    {
+                        learn = false;
+                        break;
+                    }
+                }
+
+                if (learn)
+                {
+                    result += word.Length;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode1160/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1160
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { "cat", "bt", "hat", "tree" }, "atach", 6)]
+        [InlineData(new[] { "hello", "world", "leetcode" }, "welldonehoneyr", 10)]
+        public void Test(string[] words, string chars, int assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.CountCharacters(words, chars);
+            result.Should().Be(assertResult);
+        }
+    }
+}
