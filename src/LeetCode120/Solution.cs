76a1e991e0562f5c60c5488a4a1e58036f015acc
h82258652 <842053625@qq.com>
2021-12-01 21:25:23 +0800

    submit 120 solution

---
+++ src/LeetCode120/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode120
+{
+    public class Solution
+    {
+        public int MinimumTotal(IList<IList<int>> triangle)
+        {
+            var sumTriangle = new List<List<int>>();
+
+            for (var i = triangle.Count - 1; i >= 0; i--)
+            {
+                var line = triangle[i];
+                if (i == triangle.Count - 1)
+                {
+                    sumTriangle.Add(new List<int>(line));
+                    continue;
+                }
+
+                var previousLine = sumTriangle[sumTriangle.Count - 1];
+                var sumLine = new List<int>();
+                for (var j = 0; j < line.Count; j++)
+                {
+                    var val = line[j];
+                    val += Math.Min(previousLine[j], previousLine[j + 1]);
+                    sumLine.Add(val);
+                }
+
+                sumTriangle.Add(sumLine);
+            }
+
+            return sumTriangle[sumTriangle.Count - 1][0];
+        }
+    }
+}
---
+++ src/LeetCode120/TestCases.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode120
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("[[2],[3,4],[6,5,7],[4,1,8,3]]", 11)]
+        [InlineData("[[-10]]", -10)]
+        public void Test(string triangleJson, int assertResult)
+        {
+            var triangle = JsonSerializer.Deserialize<IList<IList<int>>>(triangleJson);
+            var solution = new Solution();
+            var result = solution.MinimumTotal(triangle);
+            result.Should().Be(assertResult);
+        }
+    }
+}
