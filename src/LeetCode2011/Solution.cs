f6041f44b122016c5e6c034581a458f105aee34c
h82258652 <842053625@qq.com>
2021-12-01 13:49:37 +0800

    submit 2011 solution

---
+++ src/LeetCode2011/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode2011
+{
+    public class Solution
+    {
+        public int FinalValueAfterOperations(string[] operations)
+        {
+            var result = 0;
+
+            foreach (var operation in operations)
+            {
+                switch (operation[1])
+                {
+                    case '+':
+                        result++;
+                        break;
+
+                    case '-':
+                        result--;
+                        break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode2011/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2011
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { "--X", "X++", "X++" }, 1)]
+        [InlineData(new[] { "++X", "++X", "X++" }, 3)]
+        [InlineData(new[] { "X++", "++X", "--X", "X--" }, 0)]
+        public void Test(string[] operations, int assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.FinalValueAfterOperations(operations);
+            result.Should().Be(assertResult);
+        }
+    }
+}
