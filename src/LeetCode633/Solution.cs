8f68783a863fe2bfb7d90749f0a9c81a9cce2663
h82258652 <842053625@qq.com>
2021-12-01 17:13:48 +0800

    submit 633 solution

---
+++ src/LeetCode633/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LeetCode633
+{
+    public class Solution
+    {
+        public bool JudgeSquareSum(int c)
+        {
+            if (c == 0)
+            {
+                return true;
+            }
+
+            long left = 0;
+            long right = (int)Math.Sqrt(c);
+            while (left <= right)
+            {
+                var sum = left * left + right * right;
+                if (sum == c)
+                {
+                    return true;
+                }
+
+                if (sum < c)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ src/LeetCode633/TestCases.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode633
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(5, true)]
+        [InlineData(3, false)]
+        [InlineData(4, true)]
+        [InlineData(2, true)]
+        [InlineData(1, true)]
+        [InlineData(2147483600, true)]
+        public void Test(int c, bool assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.JudgeSquareSum(c);
+            result.Should().Be(assertResult);
+        }
+    }
+}
