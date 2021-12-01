776548190891dd4d025276130b4848f347af596b
h82258652 <842053625@qq.com>
2021-12-01 11:21:32 +0800

    submit 1979 solution

---
+++ src/LeetCode1979/Solution.cs
@@ -0,0 +1,47 @@
+﻿namespace LeetCode1979
+{
+    public class Solution
+    {
+        public int FindGCD(int[] nums)
+        {
+            var min = nums[0];
+            var max = nums[0];
+            for (var i = 1; i < nums.Length; i++)
+            {
+                var num = nums[i];
+                if (num < min)
+                {
+                    min = num;
+                }
+
+                if (num > max)
+                {
+                    max = num;
+                }
+            }
+
+            return GetGCD(max, min);
+        }
+
+        private int GetGCD(int max, int min)
+        {
+            if (max % min == 0)
+            {
+                return min;
+            }
+
+            var temp = min - 1;
+            while (temp > 0)
+            {
+                if (min % temp == 0 && max % temp == 0)
+                {
+                    return temp;
+                }
+
+                temp--;
+            }
+
+            return 1;
+        }
+    }
+}
---
+++ src/LeetCode1979/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1979
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 2, 5, 6, 9, 10 }, 2)]
+        [InlineData(new[] { 7, 5, 6, 8, 3 }, 1)]
+        [InlineData(new[] { 3, 3 }, 3)]
+        public void Test(int[] nums, int assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.FindGCD(nums);
+            result.Should().Be(assertResult);
+        }
+    }
+}
