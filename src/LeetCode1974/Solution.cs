957cd46c72ce33573c866aceaae1477f5ca17ad3
h82258652 <842053625@qq.com>
2021-12-01 12:50:29 +0800

    submit 1974 solution

---
+++ src/LeetCode1974/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LeetCode1974
+{
+    public class Solution
+    {
+        public int MinTimeToType(string word)
+        {
+            var length = word.Length;
+            var result = length;
+            char previous = 'a';
+            for (var i = 0; i < length; i++)
+            {
+                var current = word[i];
+                var diff = Math.Abs(current - previous);
+                if (diff >= 13)
+                {
+                    diff = 26 - diff;
+                }
+
+                result += diff;
+                previous = current;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode1974/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1974
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("abc", 5)]
+        [InlineData("bza", 7)]
+        [InlineData("zjpc", 34)]
+        public void Test(string word, int assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.MinTimeToType(word);
+            result.Should().Be(assertResult);
+        }
+    }
+}
