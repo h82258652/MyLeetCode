d1d1d26bcf3102d36ff1dad4a219ffb2498007e8
h82258652 <842053625@qq.com>
2021-12-01 13:28:28 +0800

    submit 1323 solution

---
+++ src/LeetCode1323/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode1323
+{
+    public class Solution
+    {
+        public int Maximum69Number(int num)
+        {
+            var charArray = num.ToString().ToCharArray();
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                var c = charArray[i];
+                if (c == '6')
+                {
+                    charArray[i] = '9';
+                    break;
+                }
+            }
+
+            return int.Parse(new string(charArray));
+        }
+    }
+}
---
+++ src/LeetCode1323/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1323
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(9669, 9969)]
+        [InlineData(9996, 9999)]
+        [InlineData(9999, 9999)]
+        public void Test(int num, int assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.Maximum69Number(num);
+            result.Should().Be(assertResult);
+        }
+    }
+}
