ee3604df9b107017bd60dd57606ac0cec167de53
h82258652 <842053625@qq.com>
2021-12-01 12:34:06 +0800

    submit 2086 solution

---
+++ src/LeetCode2086/Solution.cs
@@ -0,0 +1,86 @@
+﻿namespace LeetCode2086
+{
+    public class Solution
+    {
+        public int MinimumBuckets(string street)
+        {
+            var result = 0;
+
+            var streetArray = street.ToCharArray();
+            var length = streetArray.Length;
+            for (var i = 0; i < length; i++)
+            {
+                var c = streetArray[i];
+                if (c != 'H')
+                {
+                    continue;
+                }
+
+                // 第一个字符
+                if (i == 0)
+                {
+                    if (length <= 1 ||// 整个字符串只有一个 H
+                        streetArray[1] == 'H')// 第二个字符是 H
+                    {
+                        return -1;
+                    }
+
+                    // 将水桶放到第二个字符的地方
+                    streetArray[1] = 'B';
+                    result++;
+                }
+                // 最后一个字符
+                else if (i == length - 1)
+                {
+                    var previous = streetArray[i - 1];
+                    if (previous == 'H')// 倒数第二个字符是 H
+                    {
+                        return -1;
+                    }
+
+                    if (previous == 'B')
+                    {
+                        // 前一个已经是水桶，不做处理
+                        continue;
+                    }
+
+                    // 前一个是点，放置水桶
+                    streetArray[i - 1] = 'B';
+                    result++;
+                }
+                else
+                {
+                    var previous = streetArray[i - 1];
+                    if (previous == 'B')
+                    {
+                        // 前一个已经是水桶，不做处理
+                        continue;
+                    }
+
+                    var next = streetArray[i + 1];
+                    if (next == 'H')
+                    {
+                        // 后一个是 H，水桶只能放到前面
+
+                        if (previous == 'H')
+                        {
+                            return -1;
+                        }
+
+                        // 水桶放到前一个
+                        streetArray[i - 1] = 'B';
+                        result++;
+                    }
+                    else
+                    {
+                        // 后一个是 .，水桶放到后面去
+                        streetArray[i + 1] = 'B';
+                        result++;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode2086/TestCases.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2086
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("H..H", 2)]
+        [InlineData(".H.H.", 1)]
+        [InlineData(".HHH.", -1)]
+        [InlineData("H", -1)]
+        [InlineData(".", 0)]
+        public void Test(string street, int assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.MinimumBuckets(street);
+            result.Should().Be(assertResult);
+        }
+    }
+}
