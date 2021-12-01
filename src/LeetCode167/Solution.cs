e185fe8a7fe7b2a71934534e978c35710df16271
h82258652 <842053625@qq.com>
2021-12-01 14:38:17 +0800

    submit 167 solution

---
+++ src/LeetCode167/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LeetCode167
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            var left = 0;
+            var right = numbers.Length - 1;
+            while (left < right)
+            {
+                var leftValue = numbers[left];
+                var rightValue = numbers[right];
+                var sum = leftValue + rightValue;
+                if (sum == target)
+                {
+                    return new[] { left + 1, right + 1 };
+                }
+
+                if (sum < target)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+
+            throw new InvalidOperationException();
+        }
+    }
+}
---
+++ src/LeetCode167/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode167
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 2, 7, 11, 15 }, 9, new[] { 1, 2 })]
+        [InlineData(new[] { 2, 3, 4 }, 6, new[] { 1, 3 })]
+        [InlineData(new[] { -1, 0 }, -1, new[] { 1, 2 })]
+        public void Test(int[] numbers, int target, int[] assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.TwoSum(numbers, target);
+            result.Should().Equal(assertResult);
+        }
+    }
+}
