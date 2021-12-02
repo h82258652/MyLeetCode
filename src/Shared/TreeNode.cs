42daa97469eda34ad91dea5f10c6ccf6d4ca6d31
h82258652 <842053625@qq.com>
2021-12-02 13:26:58 +0800

    submit TreeNode.Create method

--- src/Shared/TreeNode.cs
+++ src/Shared/TreeNode.cs
@@ -1,3 +1,5 @@
+﻿using System.Collections.Generic;
+
 public class TreeNode
 {
     public int val;
@@ -9,5 +11,42 @@ public class TreeNode
         this.val = val;
         this.left = left;
         this.right = right;
+    }
+
+    public static TreeNode Create(int?[] array)
+    {
+        if (array.Length <= 0 || !array[0].HasValue)
+        {
+            return null;
+        }
+
+        var indexToNodeMap = new Dictionary<int, TreeNode>();
+        var root = new TreeNode(array[0].Value);
+        indexToNodeMap[1] = root;
+
+        for (var i = 1; i < array.Length; i++)
+        {
+            var val = array[i];
+            if (val.HasValue)
+            {
+                var node = new TreeNode(val.Value);
+                var parentIndex = (i + 1) / 2;
+                var parent = indexToNodeMap[parentIndex];
+                if ((i + 1) % 2 == 0)
+                {
+                    // 左节点
+                    parent.left = node;
+                }
+                else
+                {
+                    // 右节点
+                    parent.right = node;
+                }
+
+                indexToNodeMap[i + 1] = node;
+            }
+        }
+
+        return root;
     }
 }
