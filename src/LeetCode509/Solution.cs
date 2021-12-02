e958243256e6ad5cd337cf01d5a91fa7def55031
h82258652 <842053625@qq.com>
2021-12-02 12:50:11 +0800

    submit 509 solution

---
+++ src/LeetCode509/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode509
+{
+    public class Solution
+    {
+        public int Fib(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+
+            var nums = new int[n + 1];
+            nums[0] = 0;
+            nums[1] = 1;
+            for (var i = 2; i <= n; i++)
+            {
+                nums[i] = nums[i - 1] + nums[i - 2];
+            }
+
+            return nums[n];
+        }
+    }
+}
---
+++ src/LeetCode509/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode509
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(2, 1)]
+        [InlineData(3, 2)]
+        [InlineData(4, 3)]
+        [InlineData(0, 0)]
+        public void Test(int n, int assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.Fib(n);
+            result.Should().Be(assertResult);
+        }
+    }
+}
