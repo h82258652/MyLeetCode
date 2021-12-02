5f3d6b113daa39f5e38e8249e79d37cbfba8b3ad
h82258652 <842053625@qq.com>
2021-12-02 16:55:42 +0800

    submit 938 solution

---
+++ src/LeetCode938/Solution.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode938
+{
+    public class Solution
+    {
+        public int RangeSumBST(TreeNode root, int low, int high)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            var val = root.val;
+            var sum = 0;
+            if (val < low)
+            {
+                sum += RangeSumBST(root.right, low, high);
+            }
+            else if (val > high)
+            {
+                sum += RangeSumBST(root.left, low, high);
+            }
+            else
+            {
+                sum += val;
+                sum += RangeSumBST(root.left, low, high);
+                sum += RangeSumBST(root.right, low, high);
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ src/LeetCode938/TestCases.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode938
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("[10,5,15,3,7,null,18]", 7, 15, 32)]
+        [InlineData("[10,5,15,3,7,13,18,1,null,6]", 6, 10, 23)]
+        public void Test(string rootJson, int low, int high, int assertResult)
+        {
+            var rootArray = JsonSerializer.Deserialize<int?[]>(rootJson);
+            var root = TreeNode.Create(rootArray);
+            var solution = new Solution();
+            var result = solution.RangeSumBST(root, low, high);
+            result.Should().Be(assertResult);
+        }
+    }
+}
