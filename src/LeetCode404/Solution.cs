d30962b0c9dd2d24659fca94fd635ce79011211f
h82258652 <842053625@qq.com>
2021-12-02 13:55:57 +0800

    submit 404 solution

---
+++ src/LeetCode404/Solution.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCode404
+{
+    public class Solution
+    {
+        public int SumOfLeftLeaves(TreeNode root)
+        {
+            return SumOfLeftLeaves(root, false);
+        }
+
+        private int SumOfLeftLeaves(TreeNode node, bool isLeft)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+
+            if (node.left == null && node.right == null) // 是叶子节点
+            {
+                if (isLeft)
+                {
+                    // 是左叶子节点
+                    return node.val;
+                }
+                else
+                {
+                    // 是右叶子节点
+                    return 0;
+                }
+            }
+
+            // 不是叶子节点
+
+            var leftSum = SumOfLeftLeaves(node.left, true);
+            var rightSum = SumOfLeftLeaves(node.right, false);
+            return leftSum + rightSum;
+        }
+    }
+}
---
+++ src/LeetCode404/TestCases.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode404
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var root = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+            var solution = new Solution();
+            var result = solution.SumOfLeftLeaves(root);
+            result.Should().Be(24);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var root = new TreeNode(1);
+            var solution = new Solution();
+            var result = solution.SumOfLeftLeaves(root);
+            result.Should().Be(0);
+        }
+    }
+}
