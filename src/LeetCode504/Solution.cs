10466fdc38ecf82908ccdae691ce0327155c5118
h82258652 <842053625@qq.com>
2021-12-02 13:05:54 +0800

    submit 504 solution

---
+++ src/LeetCode504/Solution.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode504
+{
+    public class Solution
+    {
+        public string ConvertToBase7(int num)
+        {
+            if (num == 0)
+            {
+                return "0";
+            }
+
+            var isMinus = false;
+            if (num < 0)
+            {
+                isMinus = true;
+                num = -num;
+            }
+
+            var modStack = new Stack<int>();
+            while (num > 0)
+            {
+                var mod = num % 7;
+                modStack.Push(mod);
+                num /= 7;
+            }
+
+            var result = new StringBuilder(modStack.Count);
+
+            while (modStack.Count > 0)
+            {
+                result.Append(modStack.Pop());
+            }
+
+            if (isMinus)
+            {
+                return "-" + result;
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ src/LeetCode504/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode504
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(100, "202")]
+        [InlineData(-7, "-10")]
+        public void Test(int num, string assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.ConvertToBase7(num);
+            result.Should().Be(assertResult);
+        }
+    }
+}
