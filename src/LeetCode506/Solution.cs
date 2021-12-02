9f82543f73cf1add604e17fb929275ea5c687439
h82258652 <842053625@qq.com>
2021-12-02 09:24:08 +0800

    submit 506 solution

---
+++ src/LeetCode506/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+
+namespace LeetCode506
+{
+    public class Solution
+    {
+        public string[] FindRelativeRanks(int[] score)
+        {
+            return score
+                .Select((value, originIndex) => new { value, originIndex })
+                .OrderByDescending(temp => temp.value)
+                .Select((obj, sortedIndex) =>
+                {
+                    string rank;
+                    switch (sortedIndex)
+                    {
+                        case 0:
+                            rank = "Gold Medal";
+                            break;
+
+                        case 1:
+                            rank = "Silver Medal";
+                            break;
+
+                        case 2:
+                            rank = "Bronze Medal";
+                            break;
+
+                        default:
+                            rank = (sortedIndex + 1).ToString();
+                            break;
+                    }
+
+                    return new { obj.originIndex, rank };
+                })
+                .OrderBy(temp => temp.originIndex)
+                .Select(temp => temp.rank)
+                .ToArray();
+        }
+    }
+}
---
+++ src/LeetCode506/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode506
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 5, 4, 3, 2, 1 }, new[] { "Gold Medal", "Silver Medal", "Bronze Medal", "4", "5" })]
+        [InlineData(new[] { 10, 3, 8, 9, 4 }, new[] { "Gold Medal", "5", "Bronze Medal", "Silver Medal", "4" })]
+        public void Test(int[] score, string[] assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.FindRelativeRanks(score);
+            result.Should().Equal(assertResult);
+        }
+    }
+}
