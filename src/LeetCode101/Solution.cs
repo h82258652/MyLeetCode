b392e89cb3e301776f22db22fb70041f327dd7e3
h82258652 <842053625@qq.com>
2021-12-02 13:40:29 +0800

    submit 101 solution

---
+++ src/LeetCode101/Solution.cs
@@ -0,0 +1,51 @@
+﻿namespace LeetCode101
+{
+    public class Solution
+    {
+        public bool IsSymmetric(TreeNode root)
+        {
+            if (root == null)
+            {
+                return true;
+            }
+
+            var left = root.left;
+            var right = root.right;
+            Reverse(right);
+
+            return IsSame(left, right);
+        }
+
+        private bool IsSame(TreeNode node1, TreeNode node2)
+        {
+            if (node1 == null && node2 == null)
+            {
+                return true;
+            }
+
+            if (node1 == null || node2 == null)
+            {
+                return false;
+            }
+
+            if (node1.val != node2.val)
+            {
+                return false;
+            }
+
+            return IsSame(node1.left, node2.left) && IsSame(node1.right, node2.right);
+        }
+
+        private void Reverse(TreeNode node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            (node.left, node.right) = (node.right, node.left);
+            Reverse(node.left);
+            Reverse(node.right);
+        }
+    }
+}
---
+++ src/LeetCode101/TestCases.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode101
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("[1,2,2,3,4,4,3]", true)]
+        [InlineData("[1,2,2,null,3,null,3]", false)]
+        public void Test(string rootJson, bool assertResult)
+        {
+            var rootArray = JsonSerializer.Deserialize<int?[]>(rootJson);
+            var root = TreeNode.Create(rootArray);
+            var solution = new Solution();
+            var result = solution.IsSymmetric(root);
+            result.Should().Be(assertResult);
+        }
+    }
+}
