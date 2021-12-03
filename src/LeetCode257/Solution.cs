d4f4988ce2e0a0db5929858fec21c93d85e20edf
h82258652 <842053625@qq.com>
2021-12-03 13:48:12 +0800

    submit 257 solution

---
+++ src/LeetCode257/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode257
+{
+    public class Solution
+    {
+        public IList<string> BinaryTreePaths(TreeNode root)
+        {
+            if (root == null)
+            {
+                return new List<string>();
+            }
+
+            if (root.left == null && root.right == null)
+            {
+                return new List<string> { root.val.ToString() };
+            }
+
+            var result = new List<string>();
+            foreach (var leftPath in BinaryTreePaths(root.left))
+            {
+                result.Add(root.val + "->" + leftPath);
+            }
+
+            foreach (var rightPath in BinaryTreePaths(root.right))
+            {
+                result.Add(root.val + "->" + rightPath);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode257/TestCases.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode257
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("[1,2,3,null,5]", new[] { "1->2->5", "1->3" })]
+        [InlineData("[1]", new[] { "1" })]
+        public void Test(string rootJson, string[] assertResult)
+        {
+            var rootArray = JsonSerializer.Deserialize<int?[]>(rootJson);
+            var root = TreeNode.Create(rootArray);
+            var solution = new Solution();
+            var result = solution.BinaryTreePaths(root);
+            result.Should().Equal(assertResult);
+        }
+    }
+}
