f01c43285278278c5be6c85448d3e5e9dd34fe48
h82258652 <842053625@qq.com>
2021-12-04 19:45:56 +0800

    submit 746 solution

---
+++ src/LeetCode746/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LeetCode746
+{
+    public class Solution
+    {
+        public int MinCostClimbingStairs(int[] cost)
+        {
+            var n = cost.Length;
+            var totalCost = new int[n + 1];
+
+            totalCost[0] = 0;
+            totalCost[1] = 0;
+
+            for (var i = 2; i <= n; i++)
+            {
+                totalCost[i] = Math.Min(totalCost[i - 1] + cost[i - 1], totalCost[i - 2] + cost[i - 2]);
+            }
+
+            return totalCost[n];
+        }
+    }
+}
---
+++ src/LeetCode746/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode746
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 10, 15, 20 }, 15)]
+        [InlineData(new[] { 1, 100, 1, 1, 1, 100, 1, 1, 100, 1 }, 6)]
+        public void Test(int[] cost, int assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.MinCostClimbingStairs(cost);
+            result.Should().Be(assertResult);
+        }
+    }
+}
