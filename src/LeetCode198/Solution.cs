b91ecd6aacaf2e94a2b6db965cec3eb435ff5094
h82258652 <842053625@qq.com>
2021-12-04 20:08:13 +0800

    submit 198 solution

---
+++ src/LeetCode198/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LeetCode198
+{
+    public class Solution
+    {
+        public int Rob(int[] nums)
+        {
+            var length = nums.Length;
+            if (length == 1)
+            {
+                return nums[0];
+            }
+
+            if (length == 2)
+            {
+                return Math.Max(nums[0], nums[1]);
+            }
+
+            var rob = new int[nums.Length];
+            rob[0] = nums[0];
+            rob[1] = Math.Max(nums[0], nums[1]);
+            rob[2] = Math.Max(nums[0] + nums[2], nums[1]);
+
+            for (var i = 3; i < length; i++)
+            {
+                rob[i] = Math.Max(rob[i - 2] + nums[i], rob[i - 3] + nums[i]);
+            }
+
+            return Math.Max(rob[length - 1], rob[length - 2]);
+        }
+    }
+}
---
+++ src/LeetCode198/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode198
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 1 }, 4)]
+        [InlineData(new[] { 2, 7, 9, 3, 1 }, 12)]
+        public void Test(int[] nums, int assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.Rob(nums);
+            result.Should().Be(assertResult);
+        }
+    }
+}
