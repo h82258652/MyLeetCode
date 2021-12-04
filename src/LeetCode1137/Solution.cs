bb7d41b74dc44fd9c89095584c86e48a45989af0
h82258652 <842053625@qq.com>
2021-12-04 19:03:07 +0800

    submit 1137 solution

---
+++ src/LeetCode1137/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode1137
+{
+    public class Solution
+    {
+        public int Tribonacci(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+
+            if (n == 2)
+            {
+                return 1;
+            }
+
+            var nums = new int[n + 1];
+            nums[0] = 0;
+            nums[1] = 1;
+            nums[2] = 1;
+
+            for (var i = 3; i <= n; i++)
+            {
+                nums[i] = nums[i - 1] + nums[i - 2] + nums[i - 3];
+            }
+
+            return nums[n];
+        }
+    }
+}
---
+++ src/LeetCode1137/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1137
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(4, 4)]
+        [InlineData(25, 1389537)]
+        public void Test(int n, int assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.Tribonacci(n);
+            result.Should().Be(assertResult);
+        }
+    }
+}
