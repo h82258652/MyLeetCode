405aa98f8b6f9c381fbd740a5a9f8b15bff4a15a
h82258652 <842053625@qq.com>
2021-12-04 12:25:13 +0800

    submit 383 solution

---
+++ src/LeetCode383/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode383
+{
+    public class Solution
+    {
+        public bool CanConstruct(string ransomNote, string magazine)
+        {
+            var magazineCharCounts = new int[26];
+            foreach (var c in magazine)
+            {
+                magazineCharCounts[c - 'a']++;
+            }
+
+            foreach (var c in ransomNote)
+            {
+                if (magazineCharCounts[c - 'a'] > 0)
+                {
+                    magazineCharCounts[c - 'a']--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/LeetCode383/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode383
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("a","b",false)]
+        [InlineData("aa","ab",false)]
+        [InlineData("aa","aab",true)]
+        public void Test(string ransomNote, string magazine, bool assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.CanConstruct(ransomNote, magazine);
+            result.Should().Be(assertResult);
+        }
+    }
+}
