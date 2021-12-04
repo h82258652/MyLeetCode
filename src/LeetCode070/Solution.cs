969563c5fad178f06b4fd3fcebb294b39f18d1f1
h82258652 <842053625@qq.com>
2021-12-04 19:16:47 +0800

    submit 070 solution

---
+++ src/LeetCode070/Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode070
+{
+    public class Solution
+    {
+        public int ClimbStairs(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            if (n == 2)
+            {
+                return 2;
+            }
+
+            var nums = new int[n + 1];
+            nums[0] = 0;
+            nums[1] = 1;
+            nums[2] = 2;
+            for (var i = 3; i <= n; i++)
+            {
+                nums[i] = nums[i - 1] + nums[i - 2];
+            }
+
+            return nums[n];
+        }
+    }
+}
---
+++ src/LeetCode070/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode070
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(2, 2)]
+        [InlineData(3, 3)]
+        public void Test(int n, int assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.ClimbStairs(n);
+            result.Should().Be(assertResult);
+        }
+    }
+}
