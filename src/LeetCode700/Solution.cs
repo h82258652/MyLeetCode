042a9cd8a5c7f0a3ff1f04e9dd8377e4eac899d1
h82258652 <842053625@qq.com>
2021-12-06 22:45:07 +0800

    submit 700 solution

---
+++ src/LeetCode700/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode700
+{
+    public class Solution
+    {
+        public TreeNode SearchBST(TreeNode root, int val)
+        {
+            while (true)
+            {
+                if (root == null)
+                {
+                    return null;
+                }
+
+                if (root.val == val)
+                {
+                    return root;
+                }
+
+                root = val < root.val ? root.left : root.right;
+            }
+        }
+    }
+}
---
+++ src/LeetCode700/TestCases.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode700
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var root = new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(7));
+            var solution = new Solution();
+            var result = solution.SearchBST(root, 2);
+            result.Should().NotBeNull();
+            result.val.Should().Be(2);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var root = new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(7));
+            var solution = new Solution();
+            var result = solution.SearchBST(root, 5);
+            result.Should().BeNull();
+        }
+    }
+}
