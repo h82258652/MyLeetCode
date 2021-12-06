a557c90a17058be638643cabb572d9447154b5bc
h82258652 <842053625@qq.com>
2021-12-06 23:27:54 +0800

    submit 392 solution

---
+++ src/LeetCode392/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode392
+{
+    public class Solution
+    {
+        public bool IsSubsequence(string s, string t)
+        {
+            if (s == string.Empty)
+            {
+                return true;
+            }
+
+            var sIndex = 0;
+            var sLength = s.Length;
+            var tLength = t.Length;
+            for (var i = 0; i < tLength; i++)
+            {
+                if (s[sIndex] == t[i])
+                {
+                    sIndex++;
+                    if (sIndex == sLength)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ src/LeetCode392/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode392
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("abc", "ahbgdc", true)]
+        [InlineData("axc", "ahbgdc", false)]
+        public void Test(string s, string t, bool assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.IsSubsequence(s, t);
+            result.Should().Be(assertResult);
+        }
+    }
+}
