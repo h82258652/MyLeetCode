c25510176edc5254c60f8854ecbc7894bb083db6
h82258652 <842053625@qq.com>
2021-12-06 21:56:37 +0800

    submit 200 solution

---
+++ src/LeetCode200/Solution.cs
@@ -0,0 +1,52 @@
+﻿namespace LeetCode200
+{
+    public class Solution
+    {
+        public int NumIslands(char[][] grid)
+        {
+            var m = grid.Length;
+            var n = grid[0].Length;
+
+            var result = 0;
+            for (var j = 0; j < m; j++)
+            {
+                var line = grid[j];
+                for (var i = 0; i < n; i++)
+                {
+                    var c = line[i];
+                    if (c == '1')
+                    {
+                        result++;
+                        SetDownIsland(grid, i, j, m, n);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private void SetDownIsland(char[][] grid, int i, int j, int m, int n)
+        {
+            if (i < 0 || j < 0)
+            {
+                return;
+            }
+
+            if (i >= n || j >= m)
+            {
+                return;
+            }
+
+            if (grid[j][i] == '0')
+            {
+                return;
+            }
+
+            grid[j][i] = '0';
+            SetDownIsland(grid, i, j - 1, m, n);
+            SetDownIsland(grid, i + 1, j, m, n);
+            SetDownIsland(grid, i, j + 1, m, n);
+            SetDownIsland(grid, i - 1, j, m, n);
+        }
+    }
+}
---
+++ src/LeetCode200/TestCases.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode200
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(@"[[""1"",""1"",""1"",""1"",""0""],
+                       [""1"",""1"",""0"",""1"",""0""],
+                       [""1"",""1"",""0"",""0"",""0""],
+                       [""0"",""0"",""0"",""0"",""0""]]", 1)]
+        [InlineData(@"[[""1"",""1"",""0"",""0"",""0""],
+                       [""1"",""1"",""0"",""0"",""0""],
+                       [""0"",""0"",""1"",""0"",""0""],
+                       [""0"",""0"",""0"",""1"",""1""]]", 3)]
+        public void Test(string gridJson, int assertResult)
+        {
+            var grid = JsonSerializer.Deserialize<char[][]>(gridJson);
+            var solution = new Solution();
+            var result = solution.NumIslands(grid);
+            result.Should().Be(assertResult);
+        }
+    }
+}
