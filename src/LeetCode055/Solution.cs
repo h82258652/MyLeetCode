8d480ab37b264e6181c04bd3e51f79109aef993b
h82258652 <842053625@qq.com>
2021-12-06 13:38:35 +0800

    submit 055 solution

---
+++ src/LeetCode055/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LeetCode055
+{
+    public class Solution
+    {
+        public bool CanJump(int[] nums)
+        {
+            var length = nums.Length;
+            var reach = 0;// 每次能到达的最大 index
+            for (var i = 0; i < length - 1; i++)
+            {
+                if (reach < i)
+                {
+                    // 已经到不了当前 index
+                    return false;
+                }
+
+                reach = Math.Max(reach, i + nums[i]);
+            }
+
+            return reach >= length - 1;
+        }
+    }
+}
---
+++ src/LeetCode055/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode055
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 2, 3, 1, 1, 4 }, true)]
+        [InlineData(new[] { 3, 2, 1, 0, 4 }, false)]
+        [InlineData(new[] { 0, 2, 3 }, false)]
+        public void Test(int[] nums, bool assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.CanJump(nums);
+            result.Should().Be(assertResult);
+        }
+    }
+}
