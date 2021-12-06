2c8bd93aee8dc76b8af0eba18ed96d9902e2a0da
h82258652 <842053625@qq.com>
2021-12-06 14:10:14 +0800

    submit 045 solution

---
+++ src/LeetCode045/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LeetCode045
+{
+    public class Solution
+    {
+        public int Jump(int[] nums)
+        {
+            var length = nums.Length;
+            var dp = new int[length];
+            Array.Fill(dp, int.MaxValue);
+            dp[0] = 0;
+            for (var i = 0; i < length - 1; i++)
+            {
+                var num = nums[i];
+                var currentStep = dp[i];
+                for (var j = 1; j <= num && j < length - i; j++)
+                {
+                    dp[i + j] = Math.Min(dp[i + j], currentStep + 1);
+                }
+            }
+
+            return dp[length - 1];
+        }
+    }
+}
---
+++ src/LeetCode045/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode045
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 2, 3, 1, 1, 4 }, 2)]
+        [InlineData(new[] { 2, 3, 0, 1, 4 }, 2)]
+        public void Test(int[] nums, int assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.Jump(nums);
+            result.Should().Be(assertResult);
+        }
+    }
+}
