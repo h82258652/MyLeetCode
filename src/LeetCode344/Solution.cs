e99511675c24babc6295d5be5214ecd079f3e1c1
h82258652 <842053625@qq.com>
2021-12-06 22:54:04 +0800

    submit 344 solution

---
+++ src/LeetCode344/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode344
+{
+    public class Solution
+    {
+        public void ReverseString(char[] s)
+        {
+            var left = 0;
+            var right = s.Length - 1;
+            while (left < right)
+            {
+                var temp = s[left];
+                s[left] = s[right];
+                s[right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
---
+++ src/LeetCode344/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode344
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 'h', 'e', 'l', 'l', 'o' }, new[] { 'o', 'l', 'l', 'e', 'h' })]
+        [InlineData(new[] { 'H', 'a', 'n', 'n', 'a', 'h' }, new[] { 'h', 'a', 'n', 'n', 'a', 'H' })]
+        public void Test(char[] s, char[] assertResult)
+        {
+            var solution = new Solution();
+            solution.ReverseString(s);
+            s.Should().Equal(assertResult);
+        }
+    }
+}
