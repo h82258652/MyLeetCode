3c80d648d86639d62356b0d113e8b9d82886bcb1
h82258652 <842053625@qq.com>
2021-12-06 23:06:07 +0800

    submit 350 solution

---
+++ src/LeetCode350/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode350
+{
+    public class Solution
+    {
+        public int[] Intersect(int[] nums1, int[] nums2)
+        {
+            var numCount = new Dictionary<int, int>();
+            foreach (var num1 in nums1)
+            {
+                numCount.TryGetValue(num1, out var count);
+                numCount[num1] = count + 1;
+            }
+
+            var result = new List<int>();
+
+            foreach (var num2 in nums2)
+            {
+                numCount.TryGetValue(num2, out var count);
+                if (count > 0)
+                {
+                    result.Add(num2);
+                    numCount[num2] = count - 1;
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ src/LeetCode350/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode350
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 2, 1 }, new[] { 2, 2 }, new[] { 2, 2 })]
+        [InlineData(new[] { 4, 9, 5 }, new[] { 9, 4, 9, 8, 4 }, new[] { 4, 9 })]
+        public void Test(int[] nums1, int[] nums2, int[] assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.Intersect(nums1, nums2);
+            result.Should().BeEquivalentTo(assertResult);
+        }
+    }
+}
