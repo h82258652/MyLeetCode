4369f7634d4f5982f6f6d29ddc8f9e2bebef2946
h82258652 <842053625@qq.com>
2021-12-06 08:57:15 +0800

    submit 1816 solution

---
+++ src/LeetCode1816/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace LeetCode1816
+{
+    public class Solution
+    {
+        public string TruncateSentence(string s, int k)
+        {
+            var length = s.Length;
+
+            var index = -1;
+            for (var i = 0; i < length; i++)
+            {
+                if (i == length - 1)
+                {
+                    return s;
+                }
+
+                var c = s[i];
+                if (c == ' ')
+                {
+                    k--;
+                    index = i;
+
+                    if (k == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            var builder = new StringBuilder();
+            for (var i = 0; i < index; i++)
+            {
+                var c = s[i];
+                builder.Append(c);
+            }
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ src/LeetCode1816/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1816
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("Hello how are you Contestant", 4, "Hello how are you")]
+        [InlineData("What is the solution to this problem", 4, "What is the solution")]
+        [InlineData("chopper is not a tanuki", 5, "chopper is not a tanuki")]
+        public void Test(string s, int k, string assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.TruncateSentence(s, k);
+            result.Should().Be(assertResult);
+        }
+    }
+}
