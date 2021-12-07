c44325230260196bda794ce1884babaa4a351ff2
h82258652 <842053625@qq.com>
2021-12-07 12:51:17 +0800

    submit 917 solution

---
+++ src/LeetCode917/Solution.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode917
+{
+    public class Solution
+    {
+        public string ReverseOnlyLetters(string s)
+        {
+            var chars = s.ToCharArray();
+            var left = 0;
+            var right = chars.Length - 1;
+            while (left < right)
+            {
+                var leftChar = chars[left];
+                if (!char.IsLetter(leftChar))
+                {
+                    left++;
+                    continue;
+                }
+
+                var rightChar = chars[right];
+                if (!char.IsLetter(rightChar))
+                {
+                    right--;
+                    continue;
+                }
+
+                chars[left] = rightChar;
+                chars[right] = leftChar;
+                left++;
+                right--;
+            }
+
+            return new string(chars);
+        }
+    }
+}
---
+++ src/LeetCode917/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode917
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("ab-cd", "dc-ba")]
+        [InlineData("a-bC-dEf-ghIj", "j-Ih-gfE-dCba")]
+        [InlineData("Test1ng-Leet=code-Q!", "Qedo1ct-eeLg=ntse-T!")]
+        public void Test(string s, string assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.ReverseOnlyLetters(s);
+            result.Should().Be(assertResult);
+        }
+    }
+}
