ae0963bbddd2dab5dc7aba12539a668b0d996c5c
h82258652 <842053625@qq.com>
2021-12-07 21:08:08 +0800

    submit 141 solution

---
+++ src/LeetCode141/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode141
+{
+    public class Solution
+    {
+        public bool HasCycle(ListNode head)
+        {
+            if (head == null)
+            {
+                return false;
+            }
+
+            var slow = head;
+            var fast = head.next;
+
+            while (fast != null)
+            {
+                if (slow == fast)
+                {
+                    return true;
+                }
+
+                slow = slow.next;
+                fast = fast.next?.next;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ src/LeetCode141/TestCases.cs
@@ -0,0 +1,66 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode141
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            var node3 = new ListNode(3);
+            var node2 = new ListNode(2);
+            // ReSharper disable once RedundantArgumentDefaultValue
+            var node0 = new ListNode(0);
+            var nodeMinus4 = new ListNode(-4);
+
+            node3.next = node2;
+            node2.next = node0;
+            node0.next = nodeMinus4;
+            nodeMinus4.next = node2;
+
+            var solution = new Solution();
+
+            // Act
+            var result = solution.HasCycle(node3);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            // Arrange
+            var node1 = new ListNode(1);
+            var node2 = new ListNode(2);
+
+            node1.next = node2;
+            node2.next = node1;
+
+            var solution = new Solution();
+
+            // Act
+            var result = solution.HasCycle(node1);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            // Arrange
+            var node1 = new ListNode(1);
+
+            var solution = new Solution();
+
+            // Act
+            var result = solution.HasCycle(node1);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+    }
+}
