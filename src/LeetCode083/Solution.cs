8a6a9ea8ee357aaf43584a0cf3bbff1d4d2b763e
h82258652 <842053625@qq.com>
2021-12-07 21:37:56 +0800

    submit 083 solution

---
+++ src/LeetCode083/Solution.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode083
+{
+    public class Solution
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            if (head == null || head.next == null)
+            {
+                return head;
+            }
+
+            ListNode result = head;
+
+            ListNode prev = null;
+            int previousValue = int.MinValue;
+
+            while (head != null)
+            {
+                var value = head.val;
+                if (previousValue != value)
+                {
+                    previousValue = value;
+                    prev = head;
+                    head = head.next;
+                    continue;
+                }
+
+                prev!.next = head.next;
+                head = head.next;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode083/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode083
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1, 1, 2 }, new[] { 1, 2 })]
+        [InlineData(new[] { 1, 1, 2, 3, 3 }, new[] { 1, 2, 3 })]
+        public void Test(int[] headArray, int[] assertResult)
+        {
+            var head = ListNode.Create(headArray);
+            var solution = new Solution();
+            var result = solution.DeleteDuplicates(head);
+            (result?.ToArray() ?? Array.Empty<int>()).Should().Equal(assertResult);
+        }
+    }
+}
