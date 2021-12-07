2d00d8197a4b703f7cf29aa6b396c68b0bfa812b
h82258652 <842053625@qq.com>
2021-12-07 21:29:30 +0800

    submit 203 solution

---
+++ src/LeetCode203/Solution.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCode203
+{
+    public class Solution
+    {
+        public ListNode RemoveElements(ListNode head, int val)
+        {
+            if (head == null)
+            {
+                return null;
+            }
+
+            ListNode result = null;// 指向第一个不是 val 的节点
+
+            ListNode prev = null;
+            while (head != null)
+            {
+                if (head.val != val)
+                {
+                    if (result == null)
+                    {
+                        result = head;
+                    }
+                }
+                else
+                {
+                    // 是 val 的时候，将前一个节点的 next 指向当前节点的下一个节点，相当于跳过当前节点
+                    if (prev != null)
+                    {
+                        prev.next = head.next;
+
+                        // 因为当前 head 是被删除的节点，所以 prev 还是指向前一个节点不用动，head 挪到下一个节点
+                        head = head.next;
+                        continue;
+                    }
+                }
+
+                prev = head;
+                head = head.next;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode203/TestCases.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode203
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 6, 3, 4, 5, 6 }, 6, new[] { 1, 2, 3, 4, 5 })]
+        [InlineData(new int[] { }, 1, new int[] { })]
+        [InlineData(new[] { 7, 7, 7, 7 }, 7, new int[] { })]
+        [InlineData(new[] { 1, 2, 2, 1 }, 2, new[] { 1, 1 })]
+        public void Test(int[] headArray, int val, int[] assertResultArray)
+        {
+            var head = ListNode.Create(headArray);
+            var solution = new Solution();
+            var result = solution.RemoveElements(head, val);
+            (result?.ToArray() ?? Array.Empty<int>()).Should().Equal(assertResultArray);
+        }
+    }
+}
