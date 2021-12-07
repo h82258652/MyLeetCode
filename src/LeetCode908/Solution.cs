8ec172e8865e15f8f3b43515e3b28c753f42e2d6
h82258652 <842053625@qq.com>
2021-12-07 12:59:08 +0800

    submit 908 solution

---
+++ src/LeetCode908/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeetCode908
+{
+    public class Solution
+    {
+        public int SmallestRangeI(int[] nums, int k)
+        {
+            var max = int.MinValue;
+            var min = int.MaxValue;
+            foreach (var num in nums)
+            {
+                max = Math.Max(max, num);
+                min = Math.Min(min, num);
+            }
+
+            max -= k;
+            min += k;
+
+            if (max <= min)
+            {
+                return 0;
+            }
+            else
+            {
+                return max - min;
+            }
+        }
+    }
+}
---
+++ src/LeetCode908/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode908
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1 }, 0, 0)]
+        [InlineData(new[] { 0, 10 }, 2, 6)]
+        [InlineData(new[] { 1, 3, 6 }, 3, 0)]
+        public void Test(int[] nums, int k, int assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.SmallestRangeI(nums, k);
+            result.Should().Be(assertResult);
+        }
+    }
+}
