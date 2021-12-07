578fe2e29e419abaa42f75e23cdf7d59fcf27d40
h82258652 <842053625@qq.com>
2021-12-07 21:56:01 +0800

    submit 876 solution

---
+++ src/LeetCode876/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode876
+{
+    public class Solution
+    {
+        public ListNode MiddleNode(ListNode head)
+        {
+            var length = 0;// 节点数量
+            var node = head;
+            while (node != null)
+            {
+                length++;
+                node = node.next;
+            }
+
+            // 因为已经在第一个节点的位置，所以对于偶数个节点的情况，从第一个节点移动 midLength 必然到达中间靠右的那个节点
+            var midLength = length / 2;
+
+            node = head;
+            while (midLength > 0)
+            {
+                node = node.next;
+                midLength--;
+            }
+
+            return node;
+        }
+    }
+}
---
+++ src/LeetCode876/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode876
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 4, 5 }, 3)]
+        [InlineData(new[] { 1, 2, 3, 4, 5, 6 }, 4)]
+        public void Test(int[] headArray, int resultNodeVal)
+        {
+            var head = ListNode.Create(headArray);
+            var solution = new Solution();
+            var result = solution.MiddleNode(head);
+            result.val.Should().Be(resultNodeVal);
+        }
+    }
+}
