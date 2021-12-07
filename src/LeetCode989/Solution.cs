d78f6c6bd4dba5c13523517133e2e90c646b917f
h82258652 <842053625@qq.com>
2021-12-07 14:11:32 +0800

    submit 989 solution

---
+++ src/LeetCode989/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode989
+{
+    public class Solution
+    {
+        public IList<int> AddToArrayForm(int[] num, int k)
+        {
+            for (var i = num.Length - 1; i >= 0; i--)
+            {
+                var val = num[i] + k;
+                num[i] = val % 10;
+                k = val / 10;
+            }
+
+            var result = new List<int>();
+            while (k > 0)
+            {
+                result.Insert(0, k % 10);
+                k /= 10;
+            }
+
+            result.AddRange(num);
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode989/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode989
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 0, 0 }, 34, new[] { 1, 2, 3, 4 })]
+        [InlineData(new[] { 2, 7, 4 }, 181, new[] { 4, 5, 5 })]
+        [InlineData(new[] { 2, 1, 5 }, 806, new[] { 1, 0, 2, 1 })]
+        public void Test(int[] num, int k, int[] assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.AddToArrayForm(num, k);
+            result.Should().Equal(assertResult);
+        }
+    }
+}
