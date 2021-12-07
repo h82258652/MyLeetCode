86427ce394e6004937b4c2ed1cef7f30bcc9b173
h82258652 <842053625@qq.com>
2021-12-07 20:39:38 +0800

    submit 111 solution

---
+++ src/LeetCode111/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LeetCode111
+{
+    public class Solution
+    {
+        public int MinDepth(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            if (root.left == null && root.right == null)
+            {
+                return 1;
+            }
+
+            int minDepth = int.MaxValue;
+            if (root.left != null)
+            {
+                minDepth = Math.Min(minDepth, MinDepth(root.left) + 1);
+            }
+
+            if (root.right != null)
+            {
+                minDepth = Math.Min(minDepth, MinDepth(root.right) + 1);
+            }
+
+            return minDepth;
+        }
+    }
+}
---
+++ src/LeetCode111/TestCases.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode111
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var root = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+            var solution = new Solution();
+            var result = solution.MinDepth(root);
+            result.Should().Be(2);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var root = new TreeNode(2, right: new TreeNode(3, right: new TreeNode(4, right: new TreeNode(5, right: new TreeNode(6)))));
+            var solution = new Solution();
+            var result = solution.MinDepth(root);
+            result.Should().Be(5);
+        }
+    }
+}
