f9a06f3a5cfba78e5991ff1ced42844c7ea81f99
h82258652 <842053625@qq.com>
2021-12-07 09:45:20 +0800

    submit 1034 solution

---
+++ src/LeetCode1034/Solution.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode1034
+{
+    public class Solution
+    {
+        public struct Point
+        {
+            public Point(int row, int column)
+            {
+                this.row = row;
+                this.column = column;
+            }
+
+            public int row;
+            public int column;
+        }
+
+        public int[][] ColorBorder(int[][] grid, int row, int col, int color)
+        {
+            var totalRow = grid.Length;
+            var totalColumn = grid[0].Length;
+
+            var copy = new int[totalRow][];
+            for (var i = 0; i < totalRow; i++)
+            {
+                copy[i] = new int[totalColumn];
+                for (var j = 0; j < totalColumn; j++)
+                {
+                    copy[i][j] = grid[i][j];
+                }
+            }
+
+            var target = grid[row][col];
+            var edgePoints = new HashSet<Point>();
+            var visitedPoints = new HashSet<Point>();
+            LoopEdge(grid, row, col, target, totalRow, totalColumn, edgePoints, visitedPoints);
+
+            foreach (var point in edgePoints)
+            {
+                copy[point.row][point.column] = color;
+            }
+
+            return copy;
+        }
+
+        private void LoopEdge(
+            int[][] grid,
+            int row,
+            int col,
+            int target,
+            int totalRow,
+            int totalColumn,
+            HashSet<Point> edgePoints,
+            HashSet<Point> visitedPoints)
+        {
+            if (row < 0 || col < 0 ||
+                row >= totalRow || col >= totalColumn)
+            {
+                return;
+            }
+
+            var point = new Point(row, col);
+            if (visitedPoints.Contains(point))
+            {
+                return;
+            }
+
+            var val = grid[row][col];
+            if (val != target)
+            {
+                visitedPoints.Add(point);
+                return;
+            }
+
+            var isEdge = false;
+            if (row <= 0 || grid[row - 1][col] != target)// Top
+            {
+                isEdge = true;
+            }
+            else if (row >= totalRow - 1 || grid[row + 1][col] != target)// Bottom
+            {
+                isEdge = true;
+            }
+            else if (col <= 0 || grid[row][col - 1] != target)// Left
+            {
+                isEdge = true;
+            }
+            else if (col >= totalColumn - 1 || grid[row][col + 1] != target) // Right
+            {
+                isEdge = true;
+            }
+
+            if (isEdge)
+            {
+                edgePoints.Add(new Point(row, col));
+            }
+
+            visitedPoints.Add(point);
+
+            // Top
+            LoopEdge(grid, row - 1, col, target, totalRow, totalColumn, edgePoints, visitedPoints);
+            // Bottom
+            LoopEdge(grid, row + 1, col, target, totalRow, totalColumn, edgePoints, visitedPoints);
+            // Left
+            LoopEdge(grid, row, col - 1, target, totalRow, totalColumn, edgePoints, visitedPoints);
+            // Right
+            LoopEdge(grid, row, col + 1, target, totalRow, totalColumn, edgePoints, visitedPoints);
+        }
+    }
+}
---
+++ src/LeetCode1034/TestCases.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1034
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("[[1,1],[1,2]]", 0, 0, 3, "[[3,3],[3,2]]")]
+        [InlineData("[[1,2,2],[2,3,2]]", 0, 1, 3, "[[1,3,3],[2,3,3]]")]
+        [InlineData("[[1,1,1],[1,1,1],[1,1,1]]", 1, 1, 2, "[[2,2,2],[2,1,2],[2,2,2]]")]
+        [InlineData(@"[[2,1,3,2,1,1,2],
+                       [1,2,3,1,2,1,2],
+                       [1,2,1,2,2,2,2],
+                       [2,1,2,2,2,2,2],
+                       [2,3,3,3,2,1,2]]", 4, 4, 3,
+                    @"[[2,1,3,2,1,1,3],
+                       [1,2,3,1,3,1,3],
+                       [1,2,1,3,2,3,3],
+                       [2,1,3,3,2,3,3],
+                       [2,3,3,3,3,1,3]]")]
+        public void Test(string gridJson, int row, int col, int color, string assertResultJson)
+        {
+            var grid = JsonSerializer.Deserialize<int[][]>(gridJson);
+            var assertResult = JsonSerializer.Deserialize<int[][]>(assertResultJson);
+            var solution = new Solution();
+            var result = solution.ColorBorder(grid, row, col, color);
+            result.Should().BeEquivalentTo(assertResult);
+        }
+    }
+}
