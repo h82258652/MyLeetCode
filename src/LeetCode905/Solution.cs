ae4430d04a2f2f07b3adfd20231047300302f5b5
h82258652 <842053625@qq.com>
2021-12-07 13:06:18 +0800

    submit 905 solution

---
+++ src/LeetCode905/Solution.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace LeetCode905
+{
+    public class Solution
+    {
+        public int[] SortArrayByParity(int[] nums)
+        {
+            return nums.OrderBy(temp => temp % 2 != 0).ToArray();
+        }
+    }
+}
---
+++ src/LeetCode905/TestCases.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode905
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var result = solution.SortArrayByParity(new[] { 3, 1, 2, 4 });
+            (result.SequenceEqual(new[] { 2, 4, 3, 1 }) ||
+             result.SequenceEqual(new[] { 4, 2, 3, 1 }) ||
+             result.SequenceEqual(new[] { 2, 4, 1, 3 }) ||
+             result.SequenceEqual(new[] { 4, 2, 1, 3 }))
+                .Should()
+                .BeTrue();
+        }
+    }
+}
