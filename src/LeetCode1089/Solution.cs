a800bb52fe4ed7ed6484fe39fb0329adb0bd4fae
h82258652 <842053625@qq.com>
2021-12-08 11:19:08 +0800

    submit 1089 solution

---
+++ src/LeetCode1089/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode1089
+{
+    public class Solution
+    {
+        public void DuplicateZeros(int[] arr)
+        {
+            var length = arr.Length;
+            for (var i = 0; i < length; i++)
+            {
+                var num = arr[i];
+                if (num != 0)
+                {
+                    continue;
+                }
+
+                MoveElements(arr, i + 1, length);
+                if (i + 1 < length)
+                {
+                    arr[i + 1] = 0;
+                }
+                i++;
+            }
+        }
+
+        private void MoveElements(int[] nums, int start, int length)
+        {
+            for (var i = length - 1; i >= start; i--)
+            {
+                nums[i] = nums[i - 1];
+            }
+        }
+    }
+}
---
+++ src/LeetCode1089/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1089
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1, 0, 2, 3, 0, 4, 5, 0 }, new[] { 1, 0, 0, 2, 3, 0, 0, 4 })]
+        [InlineData(new[] { 1, 2, 3 }, new[] { 1, 2, 3 })]
+        public void Test(int[] arr, int[] assertResult)
+        {
+            var solution = new Solution();
+            solution.DuplicateZeros(arr);
+            arr.Should().Equal(assertResult);
+        }
+    }
+}
