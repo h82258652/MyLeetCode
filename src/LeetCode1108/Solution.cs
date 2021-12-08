c76a521ff73df8c658ca878daef6cec08859788b
h82258652 <842053625@qq.com>
2021-12-08 10:59:02 +0800

    submit 1108 solution

---
+++ src/LeetCode1108/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace LeetCode1108
+{
+    public class Solution
+    {
+        public string DefangIPaddr(string address)
+        {
+            var length = address.Length;
+            var result = new StringBuilder(length + 6);
+            for (var i = 0; i < length; i++)
+            {
+                var c = address[i];
+                if (c == '.')
+                {
+                    result.Append("[.]");
+                }
+                else
+                {
+                    result.Append(c);
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ src/LeetCode1108/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1108
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("1.1.1.1", "1[.]1[.]1[.]1")]
+        [InlineData("255.100.50.0", "255[.]100[.]50[.]0")]
+        public void Test(string address, string assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.DefangIPaddr(address);
+            result.Should().Be(assertResult);
+        }
+    }
+}
