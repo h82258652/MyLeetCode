19859093acd83d963c073625ab5caa0ca1d03e61
h82258652 <842053625@qq.com>
2021-12-08 13:41:08 +0800

    submit 413 solution

---
+++ src/LeetCode413/Solution.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode413
+{
+    public class Solution
+    {
+        public int NumberOfArithmeticSlices(int[] nums)
+        {
+            var length = nums.Length;
+            if (length <= 2)
+            {
+                return 0;
+            }
+
+            var result = 0;
+
+            var currentDivCount = 0;
+            var div = nums[1] - nums[0];
+            for (var i = 2; i < length; i++)
+            {
+                var currentDiv = nums[i] - nums[i - 1];
+                if (currentDiv != div)
+                {
+                    currentDivCount = 0;
+                    div = currentDiv;
+                }
+                else
+                {
+                    currentDivCount++;
+                    result += currentDivCount;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode413/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode413
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 4 }, 3)]
+        [InlineData(new[] { 1 }, 0)]
+        public void Test(int[] nums, int assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.NumberOfArithmeticSlices(nums);
+            result.Should().Be(assertResult);
+        }
+    }
+}
