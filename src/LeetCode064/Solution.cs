d72266ecefb0580ecdd2fa431aec1706d12d6a16
h82258652 <842053625@qq.com>
2021-12-08 15:00:55 +0800

    submit 064 solution

---
+++ src/LeetCode064/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace LeetCode064
+{
+    public class Solution
+    {
+        public int MinPathSum(int[][] grid)
+        {
+            var m = grid.Length;
+            var n = grid[0].Length;
+
+            // 初始化 dp 数组
+            var dp = new int[m][];
+            for (var j = 0; j < m; j++)
+            {
+                dp[j] = new int[n];
+            }
+
+            // 计算 dp 左上角
+            dp[0][0] = grid[0][0];
+
+            // 计算 dp 数组的第一行
+            for (var i = 1; i < n; i++)
+            {
+                dp[0][i] = dp[0][i - 1] + grid[0][i];
+            }
+
+            // 计算 dp 数组的第一列
+            for (var j = 1; j < m; j++)
+            {
+                dp[j][0] = dp[j - 1][0] + grid[j][0];
+            }
+
+            // 计算 dp 剩余的单元格
+            for (var i = 1; i < n; i++)
+            {
+                for (var j = 1; j < m; j++)
+                {
+                    dp[j][i] = Math.Min(dp[j - 1][i], dp[j][i - 1]) + grid[j][i];
+                }
+            }
+
+            return dp[m - 1][n - 1];
+        }
+    }
+}
---
+++ src/LeetCode064/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode064
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("[[1,3,1],[1,5,1],[4,2,1]]", 7)]
+        [InlineData("[[1,2,3],[4,5,6]]", 12)]
+        public void Test(string gridJson, int assertResult)
+        {
+            var grid = JsonSerializer.Deserialize<int[][]>(gridJson);
+            var solution = new Solution();
+            var result = solution.MinPathSum(grid);
+            result.Should().Be(assertResult);
+        }
+    }
+}
