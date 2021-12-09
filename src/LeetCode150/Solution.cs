c1aaeac59e8a1401c769d1b8f7e4f16b4f4b5f44
h82258652 <842053625@qq.com>
2021-12-09 21:41:01 +0800

    submit 150 solution

---
+++ src/LeetCode150/Solution.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode150
+{
+    public class Solution
+    {
+        public int EvalRPN(string[] tokens)
+        {
+            var stack = new Stack<int>();
+            foreach (var token in tokens)
+            {
+                switch (token)
+                {
+                    case "+":
+                        {
+                            var b = stack.Pop();
+                            var a = stack.Pop();
+                            stack.Push(a + b);
+                            break;
+                        }
+                    case "-":
+                        {
+                            var b = stack.Pop();
+                            var a = stack.Pop();
+                            stack.Push(a - b);
+                            break;
+                        }
+                    case "*":
+                        {
+                            var b = stack.Pop();
+                            var a = stack.Pop();
+                            stack.Push(a * b);
+                            break;
+                        }
+                    case "/":
+                        {
+                            var b = stack.Pop();
+                            var a = stack.Pop();
+                            stack.Push(a / b);
+                            break;
+                        }
+                    default:
+                        stack.Push(int.Parse(token));
+                        break;
+                }
+            }
+
+            return stack.Peek();
+        }
+    }
+}
---
+++ src/LeetCode150/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode150
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { "2", "1", "+", "3", "*" }, 9)]
+        [InlineData(new[] { "4", "13", "5", "/", "+" }, 6)]
+        [InlineData(new[] { "10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+" }, 22)]
+        public void Test(string[] tokens, int assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.EvalRPN(tokens);
+            result.Should().Be(assertResult);
+        }
+    }
+}
