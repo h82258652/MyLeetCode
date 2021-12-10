b932102190b8a1432159952463b90b76e8b101e0
h82258652 <842053625@qq.com>
2021-12-10 17:24:35 +0800

    submit 860 solution

---
+++ src/LeetCode860/Solution.cs
@@ -0,0 +1,59 @@
+﻿namespace LeetCode860
+{
+    public class Solution
+    {
+        public bool LemonadeChange(int[] bills)
+        {
+            var fiveCount = 0;
+            var tenCount = 0;
+
+            var length = bills.Length;
+            for (var i = 0; i < length; i++)
+            {
+                var bill = bills[i];
+                switch (bill)
+                {
+                    case 5:
+                        fiveCount++;
+                        break;
+
+                    case 10:
+                        if (fiveCount <= 0)
+                        {
+                            return false;
+                        }
+
+                        fiveCount--;
+                        tenCount++;
+                        break;
+
+                    case 20:
+
+                        if (tenCount > 0)
+                        {
+                            if (fiveCount <= 0)
+                            {
+                                return false;
+                            }
+
+                            tenCount--;
+                            fiveCount--;
+                        }
+                        else
+                        {
+                            if (fiveCount <= 2)
+                            {
+                                return false;
+                            }
+
+                            fiveCount -= 3;
+                        }
+
+                        break;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/LeetCode860/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode860
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 5, 5, 5, 10, 20 }, true)]
+        [InlineData(new[] { 5, 5, 10, 10, 20 }, false)]
+        [InlineData(new[] { 5, 5, 10 }, true)]
+        [InlineData(new[] { 10, 10 }, false)]
+        public void Test(int[] bills, bool expected)
+        {
+            var solution = new Solution();
+            var result = solution.LemonadeChange(bills);
+            result.Should().Be(expected);
+        }
+    }
+}
