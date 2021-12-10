155aa08efe9fd73dee9ef12c0144c8bfb2e826cc
h82258652 <842053625@qq.com>
2021-12-10 09:31:30 +0800

    submit 748 solution

---
+++ src/LeetCode748/Solution.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode748
+{
+    public class Solution
+    {
+        public string ShortestCompletingWord(string licensePlate, string[] words)
+        {
+            words = words
+                .Select((value, index) => new
+                {
+                    value = value,
+                    index = index
+                })
+                .OrderBy(temp => temp.value.Length)
+                .ThenBy(temp => temp.index)
+                .Select(temp => temp.value)
+                .ToArray();
+
+            var charCountInLicensePlate = new int[26];
+            foreach (var c in licensePlate)
+            {
+                if (char.IsLetter(c))
+                {
+                    charCountInLicensePlate[char.ToLower(c) - 'a']++;
+                }
+            }
+
+            foreach (var word in words)
+            {
+                var charCountInWord = new int[26];
+                foreach (var c in word)
+                {
+                    charCountInWord[c - 'a']++;
+                }
+
+                var match = true;
+                for (var i = 0; i < 26; i++)
+                {
+                    if (charCountInWord[i] < charCountInLicensePlate[i])
+                    {
+                        match = false;
+                        break;
+                    }
+                }
+
+                if (match)
+                {
+                    return word;
+                }
+            }
+
+            throw new InvalidOperationException();
+        }
+    }
+}
---
+++ src/LeetCode748/TestCases.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode748
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("1s3 PSt", new[] { "step", "steps", "stripe", "stepple" }, "steps")]
+        [InlineData("1s3 456", new[] { "looks", "pest", "stew", "show" }, "pest")]
+        [InlineData("Ah71752", new[] { "suggest", "letter", "of", "husband", "easy", "education", "drug", "prevent", "writer", "old" }, "husband")]
+        [InlineData("OgEu755", new[] { "enough", "these", "play", "wide", "wonder", "box", "arrive", "money", "tax", "thus" }, "enough")]
+        [InlineData("iMSlpe4", new[] { "claim", "consumer", "student", "camera", "public", "never", "wonder", "simple", "thought", "use" }, "simple")]
+        public void Test(string licensePlate, string[] words, string assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.ShortestCompletingWord(licensePlate, words);
+            result.Should().Be(assertResult);
+        }
+    }
+}
