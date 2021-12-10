db6473dfebee9f07c0490aab4753082d2e7134bf
h82258652 <842053625@qq.com>
2021-12-10 18:33:14 +0800

    submit 2027 solution

---
+++ src/LeetCode2027/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode2027
+{
+    public class Solution
+    {
+        public int MinimumMoves(string s)
+        {
+            var result = 0;
+
+            var length = s.Length;
+            for (var i = 0; i < length; i++)
+            {
+                var c = s[i];
+                if (c == 'X')
+                {
+                    result++;
+                    i += 2;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode2027/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2027
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("XXX", 1)]
+        [InlineData("XXOX", 2)]
+        [InlineData("OOOO", 0)]
+        public void Test(string s, int excepted)
+        {
+            var solution = new Solution();
+            var result = solution.MinimumMoves(s);
+            result.Should().Be(excepted);
+        }
+    }
+}
