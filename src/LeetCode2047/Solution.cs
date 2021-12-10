33a5b2c0cd6843b2833967ac83b77eae402b6cfa
h82258652 <842053625@qq.com>
2021-12-11 00:34:56 +0800

    submit 2047 solution

---
+++ src/LeetCode2047/Solution.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace LeetCode2047
+{
+    public class Solution
+    {
+        public int CountValidWords(string sentence)
+        {
+            var words = sentence.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            var result = 0;
+            foreach (var word in words)
+            {
+                if (IsValid(word))
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+
+        private bool IsValid(string word)
+        {
+            var length = word.Length;
+            var connectCount = 0;
+            for (var i = 0; i < length; i++)
+            {
+                var c = word[i];
+                if (c >= '0' && c <= '9')
+                {
+                    return false;
+                }
+
+                if (c == '-')
+                {
+                    if (connectCount > 0)
+                    {
+                        return false;
+                    }
+
+                    if (i <= 0 || i >= length - 1)
+                    {
+                        return false;
+                    }
+
+                    if (!char.IsLower(word[i - 1]) || !char.IsLower(word[i + 1]))
+                    {
+                        return false;
+                    }
+
+                    connectCount++;
+                }
+
+                if (c == '!' || c == ',' || c == '.')
+                {
+                    if (i != length - 1)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/LeetCode2047/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2047
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("cat and  dog", 3)]
+        [InlineData("!this  1-s b8d!", 0)]
+        [InlineData("alice and  bob are playing stone-game10", 5)]
+        [InlineData("he bought 2 pencils, 3 erasers, and 1  pencil-sharpener.", 6)]
+        public void Test(string sentence, int expected)
+        {
+            var solution = new Solution();
+            var result = solution.CountValidWords(sentence);
+            result.Should().Be(expected);
+        }
+    }
+}
