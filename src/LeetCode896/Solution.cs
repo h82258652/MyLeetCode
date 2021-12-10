d3ceddb7b2b69d7452d5c27faae98e28ea7e7377
h82258652 <842053625@qq.com>
2021-12-10 17:10:51 +0800

    submit 896 solution

---
+++ src/LeetCode896/Solution.cs
@@ -0,0 +1,55 @@
+﻿namespace LeetCode896
+{
+    public class Solution
+    {
+        public bool IsMonotonic(int[] nums)
+        {
+            var length = nums.Length;
+            if (length <= 2)
+            {
+                return true;
+            }
+
+            var div = nums[1] - nums[0];
+            var sign = 0;
+            if (div > 0)
+            {
+                sign = 1;
+            }
+            else if (div < 0)
+            {
+                sign = -1;
+            }
+
+            for (var i = 2; i < length; i++)
+            {
+                div = nums[i] - nums[i - 1];
+                if (div == 0)
+                {
+                    continue;
+                }
+
+                if (div < 0)
+                {
+                    if (sign == 1)
+                    {
+                        return false;
+                    }
+
+                    sign = -1;
+                }
+                else
+                {
+                    if (sign == -1)
+                    {
+                        return false;
+                    }
+
+                    sign = 1;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/LeetCode896/TestCases.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode896
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 2, 3 }, true)]
+        [InlineData(new[] { 6, 5, 4, 4 }, true)]
+        [InlineData(new[] { 1, 3, 2 }, false)]
+        [InlineData(new[] { 1, 2, 4, 5 }, true)]
+        [InlineData(new[] { 1, 1, 1 }, true)]
+        [InlineData(new[] { 1, 1, 0 }, true)]
+        public void Test(int[] nums, bool expected)
+        {
+            var solution = new Solution();
+            var result = solution.IsMonotonic(nums);
+            result.Should().Be(expected);
+        }
+    }
+}
