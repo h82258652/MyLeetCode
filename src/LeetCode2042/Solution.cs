04663d7ad332a98484ead527fb3d141bc43b05c0
h82258652 <842053625@qq.com>
2021-12-10 18:01:11 +0800

    submit 2042 solution

---
+++ src/LeetCode2042/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode2042
+{
+    public class Solution
+    {
+        public bool AreNumbersAscending(string s)
+        {
+            var numbers = new List<int>();
+
+            foreach (var word in s.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+            {
+                if (int.TryParse(word, out var number))
+                {
+                    numbers.Add(number);
+                }
+            }
+
+            var count = numbers.Count;
+            for (var i = 1; i < count; i++)
+            {
+                if (numbers[i] <= numbers[i - 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/LeetCode2042/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2042
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("1 box has 3 blue 4 red 6 green and 12 yellow marbles", true)]
+        [InlineData("hello world 5 x 5", false)]
+        [InlineData("sunset is at 7 51 pm overnight lows will be in the low 50 and 60 s", false)]
+        [InlineData("4 5 11 26", true)]
+        public void Test(string s, bool excepted)
+        {
+            var solution = new Solution();
+            var result = solution.AreNumbersAscending(s);
+            result.Should().Be(excepted);
+        }
+    }
+}
