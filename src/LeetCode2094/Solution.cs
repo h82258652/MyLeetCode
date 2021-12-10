2943d373a6f5682cb9015505f07e1b44b54c9877
h82258652 <842053625@qq.com>
2021-12-10 21:28:33 +0800

    submit 2094 solution

---
+++ src/LeetCode2094/Solution.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode2094
+{
+    public class Solution
+    {
+        public int[] FindEvenNumbers(int[] digits)
+        {
+            var nums = new int[10];
+            foreach (var digit in digits)
+            {
+                nums[digit]++;
+            }
+
+            var result = new List<int>();
+
+            for (var i = 1; i < 10; i++)
+            {
+                var iCount = nums[i];
+                if (iCount <= 0)
+                {
+                    continue;
+                }
+
+                for (var j = 0; j < 10; j++)
+                {
+                    var jCount = nums[j];
+                    if (jCount <= 0 ||
+                        i == j && jCount <= 1)
+                    {
+                        continue;
+                    }
+
+                    for (var k = 0; k < 10; k += 2)
+                    {
+                        var kCount = nums[k];
+                        if (kCount <= 0 ||
+                            i == k && kCount <= 1 ||
+                            j == k && kCount <= 1 ||
+                            i == j && i == k && j == k && kCount <= 2)
+                        {
+                            continue;
+                        }
+
+                        result.Add(i * 100 + j * 10 + k);
+                    }
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ src/LeetCode2094/TestCases.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2094
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 2, 1, 3, 0 }, new[] { 102, 120, 130, 132, 210, 230, 302, 310, 312, 320 })]
+        [InlineData(new[] { 2, 2, 8, 8, 2 }, new[] { 222, 228, 282, 288, 822, 828, 882 })]
+        [InlineData(new[] { 3, 7, 5 }, new int[] { })]
+        [InlineData(new[] { 0, 2, 0, 0 }, new[] { 200 })]
+        [InlineData(new[] { 0, 0, 0 }, new int[] { })]
+        public void Test(int[] digits, int[] excepted)
+        {
+            var solution = new Solution();
+            var result = solution.FindEvenNumbers(digits);
+            result.Should().Equal(excepted);
+        }
+    }
+}
