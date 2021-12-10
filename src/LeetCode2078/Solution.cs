861444a971e62102d34e8554f56746819991926d
h82258652 <842053625@qq.com>
2021-12-10 23:07:53 +0800

    submit 2078 solution

---
+++ src/LeetCode2078/Solution.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace LeetCode2078
+{
+    public class Solution
+    {
+        public int MaxDistance(int[] colors)
+        {
+            var colorMinIndex = new int[101];
+            Array.Fill(colorMinIndex, int.MaxValue);
+            var colorMaxIndex = new int[101];
+            Array.Fill(colorMaxIndex, int.MinValue);
+
+            var length = colors.Length;
+            for (var i = 0; i < length; i++)
+            {
+                var color = colors[i];
+                colorMinIndex[color] = Math.Min(colorMinIndex[color], i);
+                colorMaxIndex[color] = Math.Max(colorMaxIndex[color], i);
+            }
+
+            var result = 0;
+            for (var a = 0; a <= 100; a++)
+            {
+                var aLeft = colorMinIndex[a];
+                if (aLeft == int.MaxValue)
+                {
+                    continue;
+                }
+
+                var aRight = colorMaxIndex[a];
+                if (aRight == int.MinValue)
+                {
+                    continue;
+                }
+
+                for (var b = 0; b <= 100; b++)
+                {
+                    if (a == b)
+                    {
+                        continue;
+                    }
+
+                    var bLeft = colorMinIndex[b];
+                    if (bLeft == int.MaxValue)
+                    {
+                        continue;
+                    }
+
+                    var bRight = colorMaxIndex[b];
+                    if (bRight == int.MinValue)
+                    {
+                        continue;
+                    }
+
+                    result = Math.Max(result, Math.Max(Math.Abs(aLeft - bRight), Math.Abs(bLeft - aRight)));
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode2078/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2078
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1, 1, 1, 6, 1, 1, 1 }, 3)]
+        [InlineData(new[] { 1, 8, 3, 8, 3 }, 4)]
+        [InlineData(new[] { 0, 1 }, 1)]
+        public void Test(int[] colors, int expected)
+        {
+            var solution = new Solution();
+            var result = solution.MaxDistance(colors);
+            result.Should().Be(expected);
+        }
+    }
+}
