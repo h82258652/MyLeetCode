990d9110f0ac36780cc267c37e4090611173e9b3
h82258652 <842053625@qq.com>
2021-12-10 17:47:03 +0800

    submit 961 solution

---
+++ src/LeetCode961/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode961
+{
+    public class Solution
+    {
+        public int RepeatedNTimes(int[] nums)
+        {
+            var n = nums.Length / 2;
+            var numCount = new Dictionary<int, int>();
+            foreach (var num in nums)
+            {
+                numCount.TryGetValue(num, out var count);
+                count++;
+                if (count == n)
+                {
+                    return num;
+                }
+
+                numCount[num] = count;
+            }
+
+            throw new InvalidOperationException();
+        }
+    }
+}
---
+++ src/LeetCode961/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode961
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 3 }, 3)]
+        [InlineData(new[] { 2, 1, 2, 5, 3, 2 }, 2)]
+        [InlineData(new[] { 5, 1, 5, 2, 5, 3, 5, 4 }, 5)]
+        public void Test(int[] nums, int expected)
+        {
+            var solution = new Solution();
+            var result = solution.RepeatedNTimes(nums);
+            result.Should().Be(expected);
+        }
+    }
+}
