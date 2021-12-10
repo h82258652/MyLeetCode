9b675bed7ea1c1f6f3a6dadc41122017f2acb6b6
h82258652 <842053625@qq.com>
2021-12-10 16:51:07 +0800

    submit 844 solution

---
+++ src/LeetCode844/Solution.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode844
+{
+    public class Solution
+    {
+        public bool BackspaceCompare(string s, string t)
+        {
+            var sLength = s.Length;
+            var tLength = t.Length;
+
+            var sStack = new Stack<char>(sLength);
+            var tStack = new Stack<char>(tLength);
+
+            for (var i = 0; i < sLength; i++)
+            {
+                var c = s[i];
+                if (c == '#')
+                {
+                    sStack.TryPop(out _);
+                }
+                else
+                {
+                    sStack.Push(c);
+                }
+            }
+
+            for (var i = 0; i < tLength; i++)
+            {
+                var c = t[i];
+                if (c == '#')
+                {
+                    tStack.TryPop(out _);
+                }
+                else
+                {
+                    tStack.Push(c);
+                }
+            }
+
+            if (sStack.Count != tStack.Count)
+            {
+                return false;
+            }
+
+            while (sStack.Count > 0 && tStack.Count > 0)
+            {
+                var sChar = sStack.Pop();
+                var tChar = tStack.Pop();
+                if (sChar != tChar)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/LeetCode844/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode844
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("ab#c", "ad#c", true)]
+        [InlineData("ab##", "c#d#", true)]
+        [InlineData("a##c", "#a#c", true)]
+        [InlineData("a#c", "b", false)]
+        public void Test(string s, string t, bool assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.BackspaceCompare(s, t);
+            result.Should().Be(assertResult);
+        }
+    }
+}
