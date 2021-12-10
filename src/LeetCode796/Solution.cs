4bb95dd042ea4f71abb8220c325b24709f41412a
h82258652 <842053625@qq.com>
2021-12-10 16:35:20 +0800

    submit 796 solution

---
+++ src/LeetCode796/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LeetCode796
+{
+    public class Solution
+    {
+        public bool RotateString(string s, string goal)
+        {
+            var sTwice = s + s;
+
+            var index = sTwice.IndexOf(goal, StringComparison.Ordinal);
+            if (index <= -1)
+            {
+                return false;
+            }
+
+            var left = sTwice.Substring(0, index);
+            var right = sTwice.Substring(index + goal.Length);
+            return left + right == s;
+        }
+    }
+}
---
+++ src/LeetCode796/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode796
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("abcde", "cdeab", true)]
+        [InlineData("abcde", "abced", false)]
+        public void Test(string s, string goal, bool assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.RotateString(s, goal);
+            result.Should().Be(assertResult);
+        }
+    }
+}
