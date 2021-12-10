abb880107433bcc78a03723c2952ba8ce4fecdb3
h82258652 <842053625@qq.com>
2021-12-10 21:58:09 +0800

    submit 2089 solution

---
+++ src/LeetCode2089/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode2089
+{
+    public class Solution
+    {
+        public IList<int> TargetIndices(int[] nums, int target)
+        {
+            Array.Sort(nums);
+            var result = new List<int>();
+            var length = nums.Length;
+            for (var i = 0; i < length; i++)
+            {
+                if (nums[i] == target)
+                {
+                    result.Add(i);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode2089/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2089
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 5, 2, 3 }, 2, new[] { 1, 2 })]
+        [InlineData(new[] { 1, 2, 5, 2, 3 }, 3, new[] { 3 })]
+        [InlineData(new[] { 1, 2, 5, 2, 3 }, 5, new[] { 4 })]
+        public void Test(int[] nums, int target, int[] expected)
+        {
+            var solution = new Solution();
+            var result = solution.TargetIndices(nums, target);
+            result.Should().Equal(expected);
+        }
+    }
+}
