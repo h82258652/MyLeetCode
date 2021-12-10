31cfdcb18d156872fc590fdb1607dc8fc2f72859
h82258652 <842053625@qq.com>
2021-12-11 00:14:48 +0800

    submit 2068 solution

---
+++ src/LeetCode2068/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LeetCode2068
+{
+    public class Solution
+    {
+        public bool CheckAlmostEquivalent(string word1, string word2)
+        {
+            var word1CharCount = new int[26];
+            foreach (var c in word1)
+            {
+                word1CharCount[c - 'a']++;
+            }
+
+            var word2CharCount = new int[26];
+            foreach (var c in word2)
+            {
+                word2CharCount[c - 'a']++;
+            }
+
+            for (var i = 0; i < 26; i++)
+            {
+                if (Math.Abs(word1CharCount[i] - word2CharCount[i]) > 3)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/LeetCode2068/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2068
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("aaaa", "bccb", false)]
+        [InlineData("abcdeef", "abaaacc", true)]
+        [InlineData("cccddabba", "babababab", true)]
+        public void Test(string word1, string word2, bool expected)
+        {
+            var solution = new Solution();
+            var result = solution.CheckAlmostEquivalent(word1, word2);
+            result.Should().Be(expected);
+        }
+    }
+}
