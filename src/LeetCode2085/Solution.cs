99bab1b07921cdbbc2049087b4993312943057a7
h82258652 <842053625@qq.com>
2021-12-10 22:25:09 +0800

    submit 2085 solution

---
+++ src/LeetCode2085/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode2085
+{
+    public class Solution
+    {
+        public int CountWords(string[] words1, string[] words2)
+        {
+            var word1Count = new Dictionary<string, int>();
+            var word2Count = new Dictionary<string, int>();
+
+            foreach (var word in words1)
+            {
+                word1Count.TryGetValue(word, out var count);
+                word1Count[word] = count + 1;
+            }
+
+            foreach (var word in words2)
+            {
+                word2Count.TryGetValue(word, out var count);
+                word2Count[word] = count + 1;
+            }
+
+            var result = 0;
+            foreach (var (word, countInWord1) in word1Count)
+            {
+                if (countInWord1 != 1)
+                {
+                    continue;
+                }
+
+                if (word2Count.TryGetValue(word, out var countInWord2) &&
+                    countInWord2 == 1)
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode2085/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2085
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { "leetcode", "is", "amazing", "as", "is" }, new[] { "amazing", "leetcode", "is" }, 2)]
+        [InlineData(new[] { "b", "bb", "bbb" }, new[] { "a", "aa", "aaa" }, 0)]
+        [InlineData(new[] { "a", "ab" }, new[] { "a", "a", "a", "ab" }, 1)]
+        public void Test(string[] words1, string[] words2, int expected)
+        {
+            var solution = new Solution();
+            var result = solution.CountWords(words1, words2);
+            result.Should().Be(expected);
+        }
+    }
+}
