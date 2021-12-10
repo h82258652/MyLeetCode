06e2168d84699bd90b1324286672a2ef99eb12d4
h82258652 <842053625@qq.com>
2021-12-11 00:14:30 +0800

    submit 2057 solution

---
+++ src/LeetCode2057/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode2057
+{
+    public class Solution
+    {
+        public int SmallestEqual(int[] nums)
+        {
+            var length = nums.Length;
+            for (int i = 0; i < length; i++)
+            {
+                if (i % 10 == nums[i])
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ src/LeetCode2057/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2057
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 0, 1, 2 }, 0)]
+        [InlineData(new[] { 4, 3, 2, 1 }, 2)]
+        [InlineData(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }, -1)]
+        [InlineData(new[] { 2, 1, 3, 5, 2 }, 1)]
+        public void Test(int[] nums, int excepted)
+        {
+            var solution = new Solution();
+            var result = solution.SmallestEqual(nums);
+            result.Should().Be(excepted);
+        }
+    }
+}
