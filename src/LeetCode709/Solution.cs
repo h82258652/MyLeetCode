2e9ae5a81558e4a704974e26bbae84c85277f1c2
h82258652 <842053625@qq.com>
2021-12-12 11:24:54 +0800

    submit 709 solution

---
+++ src/LeetCode709/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace LeetCode709
+{
+    public class Solution
+    {
+        public string ToLowerCase(string s)
+        {
+            var length = s.Length;
+            var buffer = new StringBuilder(length);
+            for (var i = 0; i < length; i++)
+            {
+                var c = s[i];
+                c = char.ToLower(c);
+                buffer.Append(c);
+            }
+
+            return buffer.ToString();
+        }
+    }
+}
---
+++ src/LeetCode709/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode709
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("Hello", "hello")]
+        [InlineData("here", "here")]
+        [InlineData("LOVELY", "lovely")]
+        public void Test(string s, string excepted)
+        {
+            var solution = new Solution();
+            var result = solution.ToLowerCase(s);
+            result.Should().Be(excepted);
+        }
+    }
+}
