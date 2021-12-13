e76a38925c2308b754b0590acaecb4cf937270a3
h82258652 <842053625@qq.com>
2021-12-13 15:08:23 +0800

    submit 1768 solution

---
+++ src/LeetCode1768/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace LeetCode1768
+{
+    public class Solution
+    {
+        public string MergeAlternately(string word1, string word2)
+        {
+            var word1Length = word1.Length;
+            var word2Length = word2.Length;
+
+            var result = new StringBuilder();
+
+            var word1Index = 0;
+            var word2Index = 0;
+
+            while (word1Index < word1Length && word2Index < word2Length)
+            {
+                result.Append(word1[word1Index]);
+                result.Append(word2[word2Index]);
+                word1Index++;
+                word2Index++;
+            }
+
+            while (word1Index < word1Length)
+            {
+                result.Append(word1[word1Index]);
+                word1Index++;
+            }
+
+            while (word2Index < word2Length)
+            {
+                result.Append(word2[word2Index]);
+                word2Index++;
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ src/LeetCode1768/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1768
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("abc", "pqr", "apbqcr")]
+        [InlineData("ab", "pqrs", "apbqrs")]
+        [InlineData("abcd", "pq", "apbqcd")]
+        public void Test(string word1, string word2, string expected)
+        {
+            var solution = new Solution();
+            var result = solution.MergeAlternately(word1, word2);
+            result.Should().Be(expected);
+        }
+    }
+}
