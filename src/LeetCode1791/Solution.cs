745ed2f168801549454b9c03cb86871305a9cbc0
h82258652 <842053625@qq.com>
2021-12-13 16:25:07 +0800

    submit 1791 solution

---
+++ src/LeetCode1791/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode1791
+{
+    public class Solution
+    {
+        public int FindCenter(int[][] edges)
+        {
+            var edge00 = edges[0][0];
+            if (edge00 == edges[1][0])
+            {
+                return edge00;
+            }
+
+            if (edge00 == edges[1][1])
+            {
+                return edge00;
+            }
+
+            return edges[0][1];
+        }
+    }
+}
---
+++ src/LeetCode1791/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1791
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("[[1,2],[2,3],[4,2]]", 2)]
+        [InlineData("[[1,2],[5,1],[1,3],[1,4]]", 1)]
+        public void Test(string edgesJson, int expected)
+        {
+            var edges = JsonSerializer.Deserialize<int[][]>(edgesJson);
+            var solution = new Solution();
+            var result = solution.FindCenter(edges);
+            result.Should().Be(expected);
+        }
+    }
+}
