b941fb43c224e1895e305e9620e929a0c05e0739
h82258652 <842053625@qq.com>
2021-12-13 14:59:46 +0800

    submit 1748 solution

---
+++ src/LeetCode1748/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode1748
+{
+    public class Solution
+    {
+        public int SumOfUnique(int[] nums)
+        {
+            var length = nums.Length;
+            var numCount = new Dictionary<int, int>();
+            for (var i = 0; i < length; i++)
+            {
+                var num = nums[i];
+                numCount.TryGetValue(num, out var count);
+                numCount[num] = count + 1;
+            }
+
+            var result = 0;
+            foreach (var (num, count) in numCount)
+            {
+                if (count == 1)
+                {
+                    result += num;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode1748/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1748
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 2 }, 4)]
+        [InlineData(new[] { 1, 1, 1, 1, 1 }, 0)]
+        [InlineData(new[] { 1, 2, 3, 4, 5 }, 15)]
+        public void Test(int[] nums, int excepted)
+        {
+            var solution = new Solution();
+            var result = solution.SumOfUnique(nums);
+            result.Should().Be(excepted);
+        }
+    }
+}
