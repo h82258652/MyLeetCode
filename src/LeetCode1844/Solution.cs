a65e4fff9fe9c4f9fdc9a933dfbfdc0fddade7db
h82258652 <842053625@qq.com>
2021-12-13 17:20:37 +0800

    submit 1844 solution

---
+++ src/LeetCode1844/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace LeetCode1844
+{
+    public class Solution
+    {
+        public string ReplaceDigits(string s)
+        {
+            var length = s.Length;
+            var result = new StringBuilder(length);
+
+            var previous = '\0';
+            for (var i = 0; i < length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    previous = s[i];
+                    result.Append(previous);
+                    continue;
+                }
+
+                result.Append((char)(previous + (s[i] - '0')));
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ src/LeetCode1844/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1844
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("a1c1e1", "abcdef")]
+        [InlineData("a1b2c3d4e", "abbdcfdhe")]
+        public void Test(string s, string excepted)
+        {
+            var solution = new Solution();
+            var result = solution.ReplaceDigits(s);
+            result.Should().Be(excepted);
+        }
+    }
+}
