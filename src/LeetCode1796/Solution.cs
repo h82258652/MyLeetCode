8a9673dbe398f040794e0fdaf9b5a91686b43000
h82258652 <842053625@qq.com>
2021-12-13 16:59:07 +0800

    submit 1796 solution

---
+++ src/LeetCode1796/Solution.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode1796
+{
+    public class Solution
+    {
+        public int SecondHighest(string s)
+        {
+            var numCount = new int[10];
+            foreach (var c in s)
+            {
+                if (char.IsDigit(c))
+                {
+                    numCount[c - '0']++;
+                }
+            }
+
+            for (var i = 9; i >= 0; i--)
+            {
+                var count = numCount[i];
+                if (count > 0)
+                {
+                    for (var j = i - 1; j >= 0; j--)
+                    {
+                        count = numCount[j];
+                        if (count > 0)
+                        {
+                            return j;
+                        }
+                    }
+
+                    break;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ src/LeetCode1796/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1796
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("dfa12321afd", 2)]
+        [InlineData("abc1111", -1)]
+        public void Test(string s, int expected)
+        {
+            var solution = new Solution();
+            var result = solution.SecondHighest(s);
+            result.Should().Be(expected);
+        }
+    }
+}
