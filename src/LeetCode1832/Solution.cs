50b9389c00f0854613ae6fddb9b01e4015ac46cd
h82258652 <842053625@qq.com>
2021-12-13 17:06:15 +0800

    submit 1832 solution

---
+++ src/LeetCode1832/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace LeetCode1832
+{
+    public class Solution
+    {
+        public bool CheckIfPangram(string sentence)
+        {
+            var charCount = new int[26];
+            foreach (var c in sentence)
+            {
+                charCount[c - 'a']++;
+            }
+
+            return charCount.All(temp => temp > 0);
+        }
+    }
+}
---
+++ src/LeetCode1832/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1832
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("thequickbrownfoxjumpsoverthelazydog", true)]
+        [InlineData("leetcode", false)]
+        public void Test(string sentence, bool assertResult)
+        {
+            var solution = new Solution();
+            var result = solution.CheckIfPangram(sentence);
+            result.Should().Be(assertResult);
+        }
+    }
+}
