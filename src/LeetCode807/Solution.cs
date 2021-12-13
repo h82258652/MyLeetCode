a91ca5f03fb09328157a0aea0b58397414136251
h82258652 <842053625@qq.com>
2021-12-13 08:51:32 +0800

    submit 807 solution

---
+++ src/LeetCode807/Solution.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace LeetCode807
+{
+    public class Solution
+    {
+        public int MaxIncreaseKeepingSkyline(int[][] grid)
+        {
+            var size = grid.Length;
+
+            var vertical = new int[size];
+            for (var j = 0; j < size; j++)
+            {
+                var max = int.MinValue;
+                for (var i = 0; i < size; i++)
+                {
+                    var high = grid[i][j];
+                    max = Math.Max(max, high);
+                }
+
+                vertical[j] = max;
+            }
+
+            var horizontal = new int[size];
+            for (var i = 0; i < size; i++)
+            {
+                var max = int.MinValue;
+                var line = grid[i];
+                for (var j = 0; j < size; j++)
+                {
+                    var high = line[j];
+                    max = Math.Max(max, high);
+                }
+
+                horizontal[i] = max;
+            }
+
+            var result = 0;
+            for (var i = 0; i < size; i++)
+            {
+                for (var j = 0; j < size; j++)
+                {
+                    var high = grid[i][j];
+                    var canAddHigh = Math.Min(vertical[j], horizontal[i]) - high;
+                    result += canAddHigh;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode807/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode807
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("[[3,0,8,4],[2,4,5,7],[9,2,6,3],[0,3,1,0]]", 35)]
+        [InlineData("[[0,0,0],[0,0,0],[0,0,0]]", 0)]
+        public void Test(string gridJson, int excepted)
+        {
+            var grid = JsonSerializer.Deserialize<int[][]>(gridJson);
+            var solution = new Solution();
+            var result = solution.MaxIncreaseKeepingSkyline(grid);
+            result.Should().Be(excepted);
+        }
+    }
+}
