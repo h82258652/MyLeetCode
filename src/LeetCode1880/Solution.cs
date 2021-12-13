f415f798b23ebd35cbbe584c324f0f41897e5823
h82258652 <842053625@qq.com>
2021-12-13 17:50:46 +0800

    submit 1880 solution

---
+++ src/LeetCode1880/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode1880
+{
+    public class Solution
+    {
+        public bool IsSumEqual(string firstWord, string secondWord, string targetWord)
+        {
+            var first = 0;
+            var firstWordLength = firstWord.Length;
+            for (var i = 0; i < firstWordLength; i++)
+            {
+                first = first * 10 + firstWord[i] - 'a';
+            }
+
+            var second = 0;
+            var secondWordLength = secondWord.Length;
+            for (var i = 0; i < secondWordLength; i++)
+            {
+                second = second * 10 + secondWord[i] - 'a';
+            }
+
+            var target = 0;
+            var targetWordLength = targetWord.Length;
+            for (var i = 0; i < targetWordLength; i++)
+            {
+                target = target * 10 + targetWord[i] - 'a';
+            }
+
+            return first + second == target;
+        }
+    }
+}
---
+++ src/LeetCode1880/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1880
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("acb", "cba", "cdb", true)]
+        [InlineData("aaa", "a", "aab", false)]
+        [InlineData("aaa", "a", "aaaa", true)]
+        public void Test(string firstWord, string secondWord, string targetWord, bool expected)
+        {
+            var solution = new Solution();
+            var result = solution.IsSumEqual(firstWord, secondWord, targetWord);
+            result.Should().Be(expected);
+        }
+    }
+}
