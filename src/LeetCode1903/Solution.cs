10b34344be6664723c11bfa6a083d11ce13a6eac
h82258652 <842053625@qq.com>
2021-12-13 18:01:00 +0800

    submit 1903 solution

---
+++ src/LeetCode1903/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode1903
+{
+    public class Solution
+    {
+        public string LargestOddNumber(string num)
+        {
+            var length = num.Length;
+            for (var i = length - 1; i >= 0; i--)
+            {
+                var c = num[i];
+                if (c == '0' || c == '2' || c == '4' || c == '6' || c == '8')
+                {
+                    continue;
+                }
+
+                return num.Substring(0, i + 1);
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ src/LeetCode1903/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1903
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("52", "5")]
+        [InlineData("4206", "")]
+        [InlineData("35427", "35427")]
+        public void Test(string num, string expected)
+        {
+            var solution = new Solution();
+            var result = solution.LargestOddNumber(num);
+            result.Should().Be(expected);
+        }
+    }
+}
