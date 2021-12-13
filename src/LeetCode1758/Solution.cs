bd0c2b96ca4129bd4aef22d5eeb18eb56feeb15c
h82258652 <842053625@qq.com>
2021-12-13 15:31:45 +0800

    submit 1758 solution

---
+++ src/LeetCode1758/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace LeetCode1758
+{
+    public class Solution
+    {
+        public int MinOperations(string s)
+        {
+            var length = s.Length;
+
+            var zeroBaseCount = 0;
+            var oneBaseCount = 0;
+            for (var i = 0; i < length; i++)
+            {
+                var c = s[i];
+                if (i % 2 == 0)
+                {
+                    if (c != '0')
+                    {
+                        zeroBaseCount++;
+                    }
+                    else
+                    {
+                        oneBaseCount++;
+                    }
+                }
+                else
+                {
+                    if (c != '1')
+                    {
+                        zeroBaseCount++;
+                    }
+                    else
+                    {
+                        oneBaseCount++;
+                    }
+                }
+            }
+
+            return Math.Min(zeroBaseCount, oneBaseCount);
+        }
+    }
+}
---
+++ src/LeetCode1758/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1758
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("0100", 1)]
+        [InlineData("10", 0)]
+        [InlineData("1111", 2)]
+        [InlineData("10010100", 3)]
+        public void Test(string s, int expected)
+        {
+            var solution = new Solution();
+            var result = solution.MinOperations(s);
+            result.Should().Be(expected);
+        }
+    }
+}
