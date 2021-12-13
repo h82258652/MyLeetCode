bb2e84b1b4927f35b324dd88bae3e89f96fd61f0
h82258652 <842053625@qq.com>
2021-12-13 16:42:45 +0800

    submit 1790 solution

---
+++ src/LeetCode1790/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode1790
+{
+    public class Solution
+    {
+        public bool AreAlmostEqual(string s1, string s2)
+        {
+            var length = s1.Length;
+
+            var unMatchIndexList = new List<int>();
+            for (var i = 0; i < length; i++)
+            {
+                var c1 = s1[i];
+                var c2 = s2[i];
+                if (c1 != c2)
+                {
+                    unMatchIndexList.Add(i);
+                }
+            }
+
+            if (unMatchIndexList.Count == 0)
+            {
+                return true;
+            }
+
+            if (unMatchIndexList.Count == 1)
+            {
+                return false;
+            }
+
+            if (unMatchIndexList.Count > 2)
+            {
+                return false;
+            }
+
+            var firstUnMatchIndex = unMatchIndexList[0];
+            var lastUnMatchIndex = unMatchIndexList[1];
+            return s1[firstUnMatchIndex] == s2[lastUnMatchIndex] && s2[firstUnMatchIndex] == s1[lastUnMatchIndex];
+        }
+    }
+}
---
+++ src/LeetCode1790/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1790
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("bank", "kanb", true)]
+        [InlineData("attack", "defend", false)]
+        [InlineData("kelb", "kelb", true)]
+        [InlineData("abcd", "dcba", false)]
+        public void Test(string s1, string s2, bool expected)
+        {
+            var solution = new Solution();
+            var result = solution.AreAlmostEqual(s1, s2);
+            result.Should().Be(expected);
+        }
+    }
+}
