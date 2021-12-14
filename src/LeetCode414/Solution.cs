0d35edf3840d9b8316f5504d26a31929381404a5
h82258652 <842053625@qq.com>
2021-12-14 11:14:21 +0800

    submit 414 solution

---
+++ src/LeetCode414/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace LeetCode414
+{
+    public class Solution
+    {
+        public int ThirdMax(int[] nums)
+        {
+            nums = nums.Distinct().OrderByDescending(temp => temp).ToArray();
+            if (nums.Length < 3)
+            {
+                return nums[0];
+            }
+
+            return nums[2];
+        }
+    }
+}
---
+++ src/LeetCode414/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode414
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 3, 2, 1 }, 1)]
+        [InlineData(new[] { 1, 2 }, 2)]
+        [InlineData(new[] { 2, 2, 3, 1 }, 1)]
+        public void Test(int[] nums, int expected)
+        {
+            var solution = new Solution();
+            var result = solution.ThirdMax(nums);
+            result.Should().Be(expected);
+        }
+    }
+}
