b1d358502e65feac00a6f53e451d6c2e4dda10d2
h82258652 <842053625@qq.com>
2021-12-14 13:33:43 +0800

    submit 704 solution

---
+++ src/LeetCode704/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode704
+{
+    public class Solution
+    {
+        public int Search(int[] nums, int target)
+        {
+            var left = 0;
+            var right = nums.Length - 1;
+            while (left <= right)
+            {
+                var mid = left + (right - left) / 2;
+                var val = nums[mid];
+                if (val == target)
+                {
+                    return mid;
+                }
+
+                if (val < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ src/LeetCode704/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode704
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { -1, 0, 3, 5, 9, 12 }, 9, 4)]
+        [InlineData(new[] { -1, 0, 3, 5, 9, 12 }, 2, -1)]
+        [InlineData(new[] { 5 }, 5, 0)]
+        public void Test(int[] nums, int target, int expected)
+        {
+            var solution = new Solution();
+            var result = solution.Search(nums, target);
+            result.Should().Be(expected);
+        }
+    }
+}
