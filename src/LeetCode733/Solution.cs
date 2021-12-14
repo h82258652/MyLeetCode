1f5e5b3aeecf0b16c11fa25fcdc25c518d20aa46
h82258652 <842053625@qq.com>
2021-12-14 17:27:23 +0800

    submit 733 solution

---
+++ src/LeetCode733/Solution.cs
@@ -0,0 +1,47 @@
+﻿namespace LeetCode733
+{
+    public class Solution
+    {
+        public int[][] FloodFill(int[][] image, int sr, int sc, int newColor)
+        {
+            var height = image.Length;
+            var width = image[0].Length;
+
+            var oldColor = image[sr][sc];
+            var visited = new bool[height][];
+            for (var i = 0; i < height; i++)
+            {
+                visited[i] = new bool[width];
+            }
+
+            InternalFloodFill(image, sr, sc, height, width, oldColor, newColor, visited);
+
+            return image;
+        }
+
+        private void InternalFloodFill(int[][] image, int i, int j, int height, int width, int oldColor, int newColor, bool[][] visited)
+        {
+            if (i < 0 || j < 0 || i >= height || j >= width)
+            {
+                return;
+            }
+
+            if (visited[i][j])
+            {
+                return;
+            }
+
+            visited[i][j] = true;
+            if (image[i][j] != oldColor)
+            {
+                return;
+            }
+
+            image[i][j] = newColor;
+            InternalFloodFill(image, i - 1, j, height, width, oldColor, newColor, visited);
+            InternalFloodFill(image, i + 1, j, height, width, oldColor, newColor, visited);
+            InternalFloodFill(image, i, j - 1, height, width, oldColor, newColor, visited);
+            InternalFloodFill(image, i, j + 1, height, width, oldColor, newColor, visited);
+        }
+    }
+}
---
+++ src/LeetCode733/TestCases.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode733
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(@"[[1,1,1],
+                       [1,1,0],
+                       [1,0,1]]", 1, 1, 2,
+                    @"[[2,2,2],
+                       [2,2,0],
+                       [2,0,1]]")]
+        public void Test(string imageJson, int sr, int sc, int newColor, string exceptedJson)
+        {
+            var image = JsonSerializer.Deserialize<int[][]>(imageJson);
+            var excepted = JsonSerializer.Deserialize<int[][]>(exceptedJson);
+            var solution = new Solution();
+            var result = solution.FloodFill(image, sr, sc, newColor);
+            result.Should().BeEquivalentTo(excepted);
+        }
+    }
+}
