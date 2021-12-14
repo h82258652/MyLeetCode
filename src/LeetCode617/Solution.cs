d76052010fdb59de1710d437081d4d91220fb05c
h82258652 <842053625@qq.com>
2021-12-14 18:01:14 +0800

    submit 617 solution

---
+++ src/LeetCode617/Solution.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode617
+{
+    public class Solution
+    {
+        public TreeNode MergeTrees(TreeNode root1, TreeNode root2)
+        {
+            if (root1 == null && root2 == null)
+            {
+                return null;
+            }
+
+            if (root1 == null)
+            {
+                var node = new TreeNode(root2.val)
+                {
+                    left = MergeTrees(null, root2.left),
+                    right = MergeTrees(null, root2.right)
+                };
+                return node;
+            }
+
+            if (root2 == null)
+            {
+                var node = new TreeNode(root1.val)
+                {
+                    left = MergeTrees(null, root1.left),
+                    right = MergeTrees(null, root1.right)
+                };
+                return node;
+            }
+
+            {
+                var node = new TreeNode(root1.val + root2.val)
+                {
+                    left = MergeTrees(root1.left, root2.left),
+                    right = MergeTrees(root1.right, root2.right)
+                };
+                return node;
+            }
+        }
+    }
+}
---
+++ src/LeetCode617/TestCases.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode617
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var root1 = new TreeNode(1, new TreeNode(3, new TreeNode(5)), new TreeNode(2));
+            var root2 = new TreeNode(2, new TreeNode(1, right: new TreeNode(4)), new TreeNode(3, right: new TreeNode(7)));
+
+            var solution = new Solution();
+            var result = solution.MergeTrees(root1, root2);
+            result.val.Should().Be(3);
+            result.left.val.Should().Be(4);
+            result.right.val.Should().Be(5);
+            result.left.left.val.Should().Be(5);
+            result.left.right.val.Should().Be(4);
+            result.right.right.val.Should().Be(7);
+        }
+    }
+}
