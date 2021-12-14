50c77019bdb916e790a51aebd7315063ca40c2d8
h82258652 <842053625@qq.com>
2021-12-14 14:41:20 +0800

    submit 557 solution

---
+++ src/LeetCode557/Solution.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode557
+{
+    public class Solution
+    {
+        public string ReverseWords(string s)
+        {
+            var chars = s.ToCharArray();
+            var length = chars.Length;
+
+            var left = 0;
+            var right = 0;
+            for (; right < length; right++)
+            {
+                if (chars[right] == ' ')
+                {
+                    int innerLeft = left;
+                    int innerRight = right - 1;
+                    while (innerLeft < innerRight)
+                    {
+                        (chars[innerLeft], chars[innerRight]) = (chars[innerRight], chars[innerLeft]);
+                        innerLeft++;
+                        innerRight--;
+                    }
+
+                    left = right + 1;
+                }
+            }
+
+            right--;
+            while (left < right)
+            {
+                (chars[left], chars[right]) = (chars[right], chars[left]);
+                left++;
+                right--;
+            }
+
+            return new string(chars);
+        }
+    }
+}
---
+++ src/LeetCode557/TestCases.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode557
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("Let's take LeetCode contest", "s'teL ekat edoCteeL tsetnoc")]
+        public void Test(string s, string excepted)
+        {
+            var solution = new Solution();
+            var result = solution.ReverseWords(s);
+            result.Should().Be(excepted);
+        }
+    }
+}
