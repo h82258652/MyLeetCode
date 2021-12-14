962a3149f69e662e1a0a44d340a0bf2368e8a4c8
h82258652 <842053625@qq.com>
2021-12-14 10:56:23 +0800

    submit 1828 solution

---
+++ src/LeetCode1828/Solution.cs
@@ -0,0 +1,43 @@
+﻿namespace LeetCode1828
+{
+    public class Solution
+    {
+        public int[] CountPoints(int[][] points, int[][] queries)
+        {
+            var pointsLength = points.Length;
+            var queriesLength = queries.Length;
+            var result = new int[queriesLength];
+            for (var queryIndex = 0; queryIndex < queriesLength; queryIndex++)
+            {
+                var query = queries[queryIndex];
+                var x = query[0];
+                var y = query[1];
+                var r = query[2];
+                var rSquare = r * r;
+
+                var count = 0;
+
+                for (var pointIndex = 0; pointIndex < pointsLength; pointIndex++)
+                {
+                    var point = points[pointIndex];
+                    var pointX = point[0];
+                    var pointY = point[1];
+
+                    var distanceX = x - pointX;
+                    var distanceY = y - pointY;
+                    var distanceXSquare = distanceX * distanceX;
+                    var distanceYSquare = distanceY * distanceY;
+
+                    if (distanceXSquare + distanceYSquare <= rSquare)
+                    {
+                        count++;
+                    }
+                }
+
+                result[queryIndex] = count;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode1828/TestCases.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1828
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("[[1,3],[3,3],[5,3],[2,2]]", "[[2,3,1],[4,3,1],[1,1,2]]", new[] { 3, 2, 2 })]
+        [InlineData("[[1,1],[2,2],[3,3],[4,4],[5,5]]", "[[1,2,2],[2,2,2],[4,3,2],[4,3,3]]", new[] { 2, 3, 2, 4 })]
+        public void Test(string pointsJson, string queriesJson, int[] expected)
+        {
+            var points = JsonSerializer.Deserialize<int[][]>(pointsJson);
+            var queries = JsonSerializer.Deserialize<int[][]>(queriesJson);
+            var solution = new Solution();
+            var result = solution.CountPoints(points, queries);
+            result.Should().Equal(expected);
+        }
+    }
+}
