7de6361a470cb28a2dfa9c5ffa01fb0a20ebf82d
h82258652 <842053625@qq.com>
2021-12-14 13:51:47 +0800

    submit 189 solution

---
+++ src/LeetCode189/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LeetCode189
+{
+    public class Solution
+    {
+        public void Rotate(int[] nums, int k)
+        {
+            var length = nums.Length;
+            k %= length;
+            Array.Reverse(nums);
+            Array.Reverse(nums, 0, k);
+            Array.Reverse(nums, k, length - k);
+        }
+    }
+}
---
+++ src/LeetCode189/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode189
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 4, 5, 6, 7 }, 3, new[] { 5, 6, 7, 1, 2, 3, 4 })]
+        [InlineData(new[] { -1, -100, 3, 99 }, 2, new[] { 3, 99, -1, -100 })]
+        public void Test(int[] nums, int k, int[] expected)
+        {
+            var solution = new Solution();
+            solution.Rotate(nums, k);
+            nums.Should().Equal(expected);
+        }
+    }
+}
