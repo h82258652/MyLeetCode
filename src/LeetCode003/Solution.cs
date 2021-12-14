c0d985d30ffe557e52cd011eb69bd8d91f74966c
h82258652 <842053625@qq.com>
2021-12-14 16:02:21 +0800

    submit 003 solution

---
+++ src/LeetCode003/Solution.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode003
+{
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            var length = s.Length;
+
+            var result = 0;
+
+            var currentChars = new List<char>();
+            var currentLength = 0;
+
+            for (var i = 0; i < length; i++)
+            {
+                var c = s[i];
+                var existIndex = currentChars.IndexOf(c);
+                if (existIndex <= -1)
+                {
+                    // 字符没有重复
+                    currentLength++;
+                    currentChars.Add(c);
+                    continue;
+                }
+
+                // 字符重复，更新最大长度
+                result = Math.Max(result, currentLength);
+
+                // 清空重复字符和它之前的字符
+                while (existIndex >= 0)
+                {
+                    currentChars.RemoveAt(0);
+                    existIndex--;
+                }
+
+                currentChars.Add(c);
+                currentLength = currentChars.Count;
+            }
+
+            result = Math.Max(result, currentLength);
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode003/TestCases.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode003
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("abcabcbb", 3)]
+        [InlineData("bbbbb", 1)]
+        [InlineData("pwwkew", 3)]
+        [InlineData("", 0)]
+        [InlineData("dvdf", 3)]
+        public void Test(string s, int excepted)
+        {
+            var solution = new Solution();
+            var result = solution.LengthOfLongestSubstring(s);
+            result.Should().Be(excepted);
+        }
+    }
+}
