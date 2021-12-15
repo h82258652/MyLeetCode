cac36cdc2698b3b9f7204362063893a095b905f1
h82258652 <842053625@qq.com>
2021-12-15 20:59:56 +0800

    submit 073 solution

---
+++ src/LeetCode073/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode073
+{
+    public class Solution
+    {
+        public void SetZeroes(int[][] matrix)
+        {
+            var m = matrix.Length;
+            var n = matrix[0].Length;
+
+            var zeroLines = new HashSet<int>();
+            var zeroColumns = new HashSet<int>();
+
+            for (var i = 0; i < m; i++)
+            {
+                for (var j = 0; j < n; j++)
+                {
+                    if (matrix[i][j] == 0)
+                    {
+                        zeroLines.Add(i);
+                        zeroColumns.Add(j);
+                    }
+                }
+            }
+
+            for (var i = 0; i < m; i++)
+            {
+                for (var j = 0; j < n; j++)
+                {
+                    if (zeroLines.Contains(i) ||
+                        zeroColumns.Contains(j))
+                    {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/LeetCode073/TestCases.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode073
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(@"[[1,1,1],
+                       [1,0,1],
+                       [1,1,1]]",
+                    @"[[1,0,1],
+                       [0,0,0],
+                       [1,0,1]]")]
+        [InlineData(@"[[0,1,2,0],
+                       [3,4,5,2],
+                       [1,3,1,5]]",
+                    @"[[0,0,0,0],
+                       [0,4,5,0],
+                       [0,3,1,0]]")]
+        public void Test(string matrixJson, string expectationJson)
+        {
+            var matrix = JsonSerializer.Deserialize<int[][]>(matrixJson);
+            var expectation = JsonSerializer.Deserialize<int[][]>(expectationJson);
+            var solution = new Solution();
+            solution.SetZeroes(matrix);
+            matrix.Should().BeEquivalentTo(expectation);
+        }
+    }
+}
