97975ca73ed77100a893393ed1edae42e199385f
h82258652 <842053625@qq.com>
2021-12-15 15:35:24 +0800

    submit 190 solution

---
+++ src/LeetCode190/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode190
+{
+    public class Solution
+    {
+        public uint reverseBits(uint n)
+        {
+            var result = 0u;
+            for (var i = 0; i < 32; i++)
+            {
+                result <<= 1;
+                result += n & 1;
+                n >>= 1;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode190/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode190
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(0b00000010100101000001111010011100u, 0b00111001011110000010100101000000u)]
+        [InlineData(0b11111111111111111111111111111101u, 0b10111111111111111111111111111111u)]
+        public void Test(uint n, uint excepted)
+        {
+            var solution = new Solution();
+            var result = solution.reverseBits(n);
+            result.Should().Be(excepted);
+        }
+    }
+}
