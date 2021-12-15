a580fcf66a9ec68c1ffd03ef2983d752e2f0deab
h82258652 <842053625@qq.com>
2021-12-15 20:40:01 +0800

    submit 102 solution

---
+++ src/LeetCode102/Solution.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode102
+{
+    public class Solution
+    {
+        public IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            if (root == null)
+            {
+                return new List<IList<int>>();
+            }
+
+            var result = new List<IList<int>>();
+
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                var levelCount = queue.Count;
+
+                var level = new List<int>();
+
+                for (var i = 0; i < levelCount; i++)
+                {
+                    var node = queue.Dequeue();
+
+                    level.Add(node.val);
+
+                    if (node.left != null)
+                    {
+                        queue.Enqueue(node.left);
+                    }
+
+                    if (node.right != null)
+                    {
+                        queue.Enqueue(node.right);
+                    }
+                }
+
+                result.Add(level);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode102/TestCases.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode102
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var root = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+            var expectation = new List<IList<int>>
+            {
+                new List<int> { 3 },
+                new List<int> { 9, 20 },
+                new List<int> { 15, 7 }
+            };
+
+            var solution = new Solution();
+            var result = solution.LevelOrder(root);
+            result.Should().BeEquivalentTo(expectation);
+        }
+    }
+}
