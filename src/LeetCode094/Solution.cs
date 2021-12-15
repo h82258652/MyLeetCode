9b4ed28ef5a3a63a6ae35f58e3278d7aace6ec9b
h82258652 <842053625@qq.com>
2021-12-15 18:09:27 +0800

    submit 094 solution

---
+++ src/LeetCode094/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode094
+{
+    public class Solution
+    {
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            var list = new List<int>();
+            InternalInorderTraversal(root, list);
+            return list;
+        }
+
+        private void InternalInorderTraversal(TreeNode node, List<int> list)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            InternalInorderTraversal(node.left, list);
+            list.Add(node.val);
+            InternalInorderTraversal(node.right, list);
+        }
+    }
+}
---
+++ src/LeetCode094/TestCases.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode094
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var root = new TreeNode(1, right: new TreeNode(2, new TreeNode(3)));
+            var expected = new List<int> { 1, 3, 2 };
+
+            var solution = new Solution();
+            var result = solution.InorderTraversal(root);
+            result.Should().Equal(expected);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var result = solution.InorderTraversal(null);
+            result.Should().Equal(new List<int>());
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var root = new TreeNode(1);
+            var expected = new List<int> { 1 };
+
+            var solution = new Solution();
+            var result = solution.InorderTraversal(root);
+            result.Should().Equal(expected);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            var root = new TreeNode(1, new TreeNode(2));
+            var expected = new List<int> { 2, 1 };
+
+            var solution = new Solution();
+            var result = solution.InorderTraversal(root);
+            result.Should().Equal(expected);
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            var root = new TreeNode(1, right: new TreeNode(2));
+            var expected = new List<int> { 1, 2 };
+
+            var solution = new Solution();
+            var result = solution.InorderTraversal(root);
+            result.Should().Equal(expected);
+        }
+    }
+}
