5302e915a9a2f9f97f4c3ba4a81bb84f13197494
h82258652 <842053625@qq.com>
2021-12-15 22:24:10 +0800

    submit 226 solution

---
+++ src/LeetCode226/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode226
+{
+    public class Solution
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+
+            var left = root.left;
+            var right = root.right;
+            root.right = left;
+            root.left = right;
+
+            InvertTree(left);
+            InvertTree(right);
+
+            return root;
+        }
+    }
+}
---
+++ src/LeetCode226/TestCases.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode226
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var root = new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(7, new TreeNode(6), new TreeNode(9)));
+
+            var solution = new Solution();
+            var result = solution.InvertTree(root);
+
+            result.Should().NotBeNull();
+            result.val.Should().Be(4);
+            result.left.Should().NotBeNull();
+            result.left.val.Should().Be(7);
+            result.left.left.Should().NotBeNull();
+            result.left.left.val.Should().Be(9);
+            result.left.right.Should().NotBeNull();
+            result.left.right.val.Should().Be(6);
+            result.right.Should().NotBeNull();
+            result.right.val.Should().Be(2);
+            result.right.left.Should().NotBeNull();
+            result.right.left.val.Should().Be(3);
+            result.right.right.Should().NotBeNull();
+            result.right.right.val.Should().Be(1);
+        }
+    }
+}
