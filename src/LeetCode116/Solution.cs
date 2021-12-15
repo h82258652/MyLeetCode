c0a4ea33cb19faeffa36f9e10af4334e054e288c
h82258652 <842053625@qq.com>
2021-12-15 11:19:26 +0800

    submit 116 solution

---
+++ src/LeetCode116/Solution.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode116
+{
+    public class Solution
+    {
+        public Node Connect(Node root)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+
+            var queue = new Queue<Node>();
+            var index = 1;
+            queue.Enqueue(root);
+            Node previous = null;
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+
+                if (node.left != null)
+                {
+                    queue.Enqueue(node.left);
+                }
+                if (node.right != null)
+                {
+                    queue.Enqueue(node.right);
+                }
+
+                // 将前一个的 next 指向当前节点
+                if (previous != null)
+                {
+                    previous.next = node;
+                }
+
+                previous = !IsLineLastIndex(index) ? node : null;
+
+                index++;
+            }
+
+            return root;
+        }
+
+        /// <summary>
+        /// 当前序号是不是这一层的最后一个
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        private bool IsLineLastIndex(int index)
+        {
+            index++;
+            return (index & (index - 1)) == 0 && index > 0;
+        }
+    }
+}
---
+++ src/LeetCode116/TestCases.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode116
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var root = new Node(1)
+            {
+                left = new Node(2)
+                {
+                    left = new Node(4),
+                    right = new Node(5)
+                },
+                right = new Node(3)
+                {
+                    left = new Node(6),
+                    right = new Node(7)
+                }
+            };
+
+            var solution = new Solution();
+            var result = solution.Connect(root);
+            result.val.Should().Be(1);
+            result.next.Should().BeNull();
+
+            result.left.val.Should().Be(2);
+            result.left.next.Should().NotBeNull();
+            result.left.next.val.Should().Be(3);
+            result.left.next.next.Should().BeNull();
+
+            result.left.left.val.Should().Be(4);
+            result.left.left.next.Should().NotBeNull();
+            result.left.left.next.val.Should().Be(5);
+            result.left.left.next.next.Should().NotBeNull();
+            result.left.left.next.next.val.Should().Be(6);
+            result.left.left.next.next.next.Should().NotBeNull();
+            result.left.left.next.next.next.val.Should().Be(7);
+            result.left.left.next.next.next.next.Should().BeNull();
+        }
+    }
+}
