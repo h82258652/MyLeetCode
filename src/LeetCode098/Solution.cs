0b3fbce2feed072561fe800728b32fdfcb9b18f4
h82258652 <842053625@qq.com>
2021-12-15 23:28:05 +0800

    submit 098 solution

---
+++ src/LeetCode098/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode098
+{
+    public class Solution
+    {
+        public bool IsValidBST(TreeNode root)
+        {
+            return IsBST(root.left, long.MinValue, root.val) && IsBST(root.right, root.val, long.MaxValue);
+        }
+
+        private bool IsBST(TreeNode node, long min, long max)
+        {
+            if (node == null)
+            {
+                return true;
+            }
+
+            var val = node.val;
+            if (val <= min || val >= max)
+            {
+                return false;
+            }
+
+            return IsBST(node.left, min, val) && IsBST(node.right, val, max);
+        }
+    }
+}
---
+++ src/LeetCode098/TestCases.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode098
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var root = new TreeNode(2,
+                new TreeNode(1),
+                new TreeNode(3));
+
+            var solution = new Solution();
+            var result = solution.IsValidBST(root);
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var root = new TreeNode(5,
+                new TreeNode(1),
+                new TreeNode(4,
+                    new TreeNode(3),
+                    new TreeNode(6)));
+
+            var solution = new Solution();
+            var result = solution.IsValidBST(root);
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var root = new TreeNode(5,
+                new TreeNode(4),
+                new TreeNode(6,
+                    new TreeNode(3),
+                    new TreeNode(7)));
+
+            var solution = new Solution();
+            var result = solution.IsValidBST(root);
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            var root = new TreeNode(-2147483648,
+                right: new TreeNode(2147483647));
+
+            var solution = new Solution();
+            var result = solution.IsValidBST(root);
+            result.Should().BeTrue();
+        }
+    }
+}
