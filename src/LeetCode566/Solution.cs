e56a2031db587fa27c2bf39abb578d6231120b5f
h82258652 <842053625@qq.com>
2021-12-15 17:02:37 +0800

    submit 566 solution

---
+++ src/LeetCode566/Solution.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode566
+{
+    public class Solution
+    {
+        public int[][] MatrixReshape(int[][] mat, int r, int c)
+        {
+            var m = mat.Length;
+            var n = mat[0].Length;
+
+            if (m * n != r * c)
+            {
+                return mat;
+            }
+
+            var result = new int[r][];
+            for (var i = 0; i < r; i++)
+            {
+                result[i] = new int[c];
+            }
+
+            var resultI = 0;
+            var resultJ = 0;
+            for (var i = 0; i < m; i++)
+            {
+                for (var j = 0; j < n; j++)
+                {
+                    result[resultI][resultJ] = mat[i][j];
+                    resultJ++;
+                    if (resultJ == c)
+                    {
+                        resultJ = 0;
+                        resultI++;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode566/TestCases.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode566
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("[[1,2],[3,4]]", 1, 4, "[[1,2,3,4]]")]
+        [InlineData("[[1,2],[3,4]]", 2, 4, "[[1,2],[3,4]]")]
+        public void Test(string matJson, int r, int c, string expectationJson)
+        {
+            var mat = JsonSerializer.Deserialize<int[][]>(matJson);
+            var expectation = JsonSerializer.Deserialize<int[][]>(expectationJson);
+            var solution = new Solution();
+            var result = solution.MatrixReshape(mat, r, c);
+            result.Should().BeEquivalentTo(expectation);
+        }
+    }
+}
