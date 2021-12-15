6183bb74b51f61c64a2553585ccb16e68ee9b245
h82258652 <842053625@qq.com>
2021-12-15 22:47:47 +0800

    submit 112 solution

---
+++ src/LeetCode112/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode112
+{
+    public class Solution
+    {
+        public bool HasPathSum(TreeNode root, int targetSum)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+
+            var isLeafNode = IsLeafNode(root);
+            if (isLeafNode)
+            {
+                return targetSum == root.val;
+            }
+
+            return HasPathSum(root.left, targetSum - root.val) ||
+                   HasPathSum(root.right, targetSum - root.val);
+        }
+
+        private bool IsLeafNode(TreeNode node)
+        {
+            return node.left == null && node.right == null;
+        }
+    }
+}
---
+++ src/LeetCode112/TestCases.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode112
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var root = new TreeNode(5,
+                new TreeNode(4,
+                    new TreeNode(11,
+                        new TreeNode(7),
+                        new TreeNode(2))),
+                new TreeNode(8,
+                    new TreeNode(13),
+                    new TreeNode(4,
+                        right: new TreeNode(1))));
+
+            var solution = new Solution();
+            var result = solution.HasPathSum(root, 22);
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var root = new TreeNode(1,
+                new TreeNode(2),
+                new TreeNode(3));
+
+            var solution = new Solution();
+            var result = solution.HasPathSum(root, 5);
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var solution = new Solution();
+            var result = solution.HasPathSum(null, 0);
+            result.Should().BeFalse();
+        }
+    }
+}
