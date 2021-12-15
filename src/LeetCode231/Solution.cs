761209c62ccbeef923cc2ec44cb4018331f3cd4a
h82258652 <842053625@qq.com>
2021-12-15 15:20:26 +0800

    submit 231 solution

---
+++ src/LeetCode231/Solution.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCode231
+{
+    public class Solution
+    {
+        public bool IsPowerOfTwo(int n)
+        {
+            // 如果 n 是 2 的幂，则 n 的二进制形式最左边第一位是 1，其余是 0
+            // 而 n-1 的位全是 1
+            return (n & (n - 1)) == 0 && n > 0;
+        }
+    }
+}
---
+++ src/LeetCode231/TestCases.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode231
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(1, true)]
+        [InlineData(16, true)]
+        [InlineData(3, false)]
+        [InlineData(4, true)]
+        [InlineData(5, false)]
+        public void Test(int n, bool excepted)
+        {
+            var solution = new Solution();
+            var result = solution.IsPowerOfTwo(n);
+            result.Should().Be(excepted);
+        }
+    }
+}
