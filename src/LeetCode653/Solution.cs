a3ba99dbe8a0dfb9f7c154003665a04eab2e06e2
h82258652 <842053625@qq.com>
2021-12-16 09:59:33 +0800

    submit 653 solution

---
+++ src/LeetCode653/Solution.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode653
+{
+    public class Solution
+    {
+        public bool FindTarget(TreeNode root, int k)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+
+            var list = new List<int>();
+            Inorder(root, list);
+
+            var left = 0;
+            var right = list.Count - 1;
+            while (left < right)
+            {
+                var leftVal = list[left];
+                var rightVal = list[right];
+                var sum = leftVal + rightVal;
+                if (sum == k)
+                {
+                    return true;
+                }
+
+                if (sum < k)
+                {
+                    left++;
+                }
+
+                if (sum > k)
+                {
+                    right--;
+                }
+            }
+
+            return false;
+        }
+
+        private void Inorder(TreeNode node, List<int> list)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            Inorder(node.left, list);
+            list.Add(node.val);
+            Inorder(node.right, list);
+        }
+    }
+}
---
+++ src/LeetCode653/TestCases.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode653
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var root = new TreeNode(5,
+                new TreeNode(3,
+                    new TreeNode(2),
+                    new TreeNode(4)),
+                new TreeNode(6,
+                    right: new TreeNode(7)));
+
+            var solution = new Solution();
+            var result = solution.FindTarget(root, 9);
+            result.Should().BeTrue();
+        }
+    }
+}
