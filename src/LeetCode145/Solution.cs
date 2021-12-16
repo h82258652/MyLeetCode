3105db68277115b606cb3740671849b59d161e9a
h82258652 <842053625@qq.com>
2021-12-16 17:19:36 +0800

    submit 145 solution

---
+++ src/LeetCode145/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode145
+{
+    public class Solution
+    {
+        public IList<int> PostorderTraversal(TreeNode root)
+        {
+            var list = new List<int>();
+            InternalPostorderTraversal(root, list);
+            return list;
+        }
+
+        private void InternalPostorderTraversal(TreeNode node, List<int> list)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            InternalPostorderTraversal(node.left, list);
+            InternalPostorderTraversal(node.right, list);
+            list.Add(node.val);
+        }
+    }
+}
---
+++ src/LeetCode145/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode145
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("[1,null,2,3]", new[] { 3, 2, 1 })]
+        public void Test(string rootJson, int[] excepted)
+        {
+            var rootArray = JsonSerializer.Deserialize<int?[]>(rootJson);
+            var root = TreeNode.Create(rootArray);
+            var solution = new Solution();
+            var result = solution.PostorderTraversal(root);
+            result.Should().Equal(excepted);
+        }
+    }
+}
