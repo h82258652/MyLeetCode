57cce3103fca1166e367e785f0f8be27e2a8d867
h82258652 <842053625@qq.com>
2021-12-16 17:43:33 +0800

    submit 342 solution

---
+++ src/LeetCode342/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode342
+{
+    public class Solution
+    {
+        public bool IsPowerOfFour(int n)
+        {
+            if (n <= 0)
+            {
+                return false;
+            }
+
+            while (n > 1)
+            {
+                if (n % 4 != 0)
+                {
+                    return false;
+                }
+
+                n >>= 2;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/LeetCode342/TestCases.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode342
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(16, true)]
+        [InlineData(5, false)]
+        [InlineData(1, true)]
+        [InlineData(2, false)]
+        [InlineData(8, false)]
+        public void Test(int n, bool expected)
+        {
+            var solution = new Solution();
+            var result = solution.IsPowerOfFour(n);
+            result.Should().Be(expected);
+        }
+    }
+}
