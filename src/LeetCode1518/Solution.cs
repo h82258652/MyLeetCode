0ac9024d4dbdb912cc82f4afafa4db126d32846a
h82258652 <842053625@qq.com>
2021-12-17 14:31:40 +0800

    submit 1518 solution

---
+++ src/LeetCode1518/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode1518
+{
+    public class Solution
+    {
+        public int NumWaterBottles(int numBottles, int numExchange)
+        {
+            return InternalNumWaterBottles(numBottles, numExchange, 0);
+        }
+
+        private int InternalNumWaterBottles(int numBottles, int numExchange, int emptyBottles)
+        {
+            numBottles += emptyBottles / numExchange;// 将空瓶子换成瓶子
+            emptyBottles %= numExchange;// 剩余不能换瓶子的空瓶子个数
+
+            if (numBottles + emptyBottles < numExchange)// 剩下的瓶子数量不能再兑换了
+            {
+                return numBottles;
+            }
+
+            return numBottles + InternalNumWaterBottles(0, numExchange, numBottles + emptyBottles);
+        }
+    }
+}
---
+++ src/LeetCode1518/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1518
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(9, 3, 13)]
+        [InlineData(15, 4, 19)]
+        [InlineData(5, 5, 6)]
+        [InlineData(2, 3, 2)]
+        public void Test(int numBottles, int numExchange, int expected)
+        {
+            var solution = new Solution();
+            var result = solution.NumWaterBottles(numBottles, numExchange);
+            result.Should().Be(expected);
+        }
+    }
+}
