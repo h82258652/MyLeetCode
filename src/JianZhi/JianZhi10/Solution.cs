cd0e421b533a85226ead410cba603f4a291369c6
h82258652 <842053625@qq.com>
2021-12-17 17:00:24 +0800

    submit JianZhi10 solution

---
+++ src/JianZhi/JianZhi10/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace JianZhi10
+{
+    public class Solution
+    {
+        /// <summary>
+        /// 时间复杂度 O(n)
+        /// 空间复杂度 O(1)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int Fib(int n)
+        {
+            if (n <= 1)// Fib(0)=0，Fib(1)=1
+            {
+                return n;
+            }
+
+            var minus2 = 0;// Fib(n-2)
+            var minus1 = 1;// Fib(n-1)
+            for (var i = 2; i <= n; i++)
+            {
+                var current = minus1 + minus2;// Fib(n)
+                current %= 1000000007;// 题目需要取模
+                minus2 = minus1;
+                minus1 = current;
+            }
+
+            return minus1;
+        }
+    }
+}
---
+++ src/JianZhi/JianZhi10/TestCases.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace JianZhi10
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            var solution = new Solution();
+
+            // Act
+            var fib = solution.Fib(2);
+
+            // Assert
+            fib.Should().Be(1);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            // Arrange
+            var solution = new Solution();
+
+            // Act
+            var fib = solution.Fib(5);
+
+            // Assert
+            fib.Should().Be(5);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            // Arrange
+            var solution = new Solution();
+
+            // Act
+            var fib = solution.Fib(45);
+
+            // Assert
+            fib.Should().Be(134903163);
+        }
+    }
+}
