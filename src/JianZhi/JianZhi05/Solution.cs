f0c53fc59b30582a2eadf37f37474a76390fba23
h82258652 <842053625@qq.com>
2021-12-17 15:18:42 +0800

    submit JianZhi05 solution

---
+++ src/JianZhi/JianZhi05/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace JianZhi05
+{
+    public class Solution
+    {
+        /// <summary>
+        /// 时间复杂度 O(n)，n 为 s 的长度
+        /// 空间复杂度 O(n)，最差的情况 s 全是空格，需要 3n 的空间，忽略常数仍为 O(n)
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public string ReplaceSpace(string s)
+        {
+            var buffer = new StringBuilder();
+            foreach (var c in s)
+            {
+                if (c == ' ')
+                {
+                    buffer.Append("%20");
+                }
+                else
+                {
+                    buffer.Append(c);
+                }
+            }
+
+            return buffer.ToString();
+        }
+    }
+}
---
+++ src/JianZhi/JianZhi05/TestCases.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace JianZhi05
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            var solution = new Solution();
+            const string s = "We are happy.";
+
+            // Act
+            var result = solution.ReplaceSpace(s);
+
+            // Assert
+            result.Should().Be("We%20are%20happy.");
+        }
+    }
+}
