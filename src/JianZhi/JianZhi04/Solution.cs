a715246a508d676eada5479457875b1637b618c3
h82258652 <842053625@qq.com>
2021-12-17 15:11:58 +0800

    submit JianZhi04 solution

---
+++ src/JianZhi/JianZhi04/Solution.cs
@@ -0,0 +1,52 @@
+﻿namespace JianZhi04
+{
+    public class Solution
+    {
+        /// <summary>
+        /// 时间复杂度 O(m+n)
+        /// 空间复杂度 O(1)
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public bool FindNumberIn2DArray(int[][] matrix, int target)
+        {
+            var m = matrix.Length;
+            if (m <= 0)// 一行都没有
+            {
+                return false;
+            }
+
+            var n = matrix[0].Length;
+            if (n <= 0)// 一列都没有
+            {
+                return false;
+            }
+
+            // 从右上角开始找
+            var i = 0;
+            var j = n - 1;
+            while (i < m && j >= 0)
+            {
+                var num = matrix[i][j];
+                if (num == target)
+                {
+                    return true;
+                }
+
+                if (num < target)
+                {
+                    // 当前数字比目标小，往下一行找
+                    i++;
+                }
+                else
+                {
+                    // 当前数字比目标大，往左一列找
+                    j--;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ src/JianZhi/JianZhi04/TestCases.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace JianZhi04
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            var solution = new Solution();
+            var matrix = new[]
+            {
+                new[] { 1, 4, 7, 11, 15 },
+                new[] { 2, 5, 8, 12, 19 },
+                new[] { 3, 6, 9, 16, 22 },
+                new[] { 10, 13, 14, 17, 24 },
+                new[] { 18, 21, 23, 26, 30 }
+            };
+
+            // Act
+            var result1 = solution.FindNumberIn2DArray(matrix, 5);
+            var result2 = solution.FindNumberIn2DArray(matrix, 20);
+
+            // Assert
+            result1.Should().BeTrue();
+            result2.Should().BeFalse();
+        }
+    }
+}
