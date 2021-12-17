2932ebe7e78af57a53ebb135fa79d7600f8bb332
h82258652 <842053625@qq.com>
2021-12-17 15:28:30 +0800

    submit JianZhi06 solution

---
+++ src/JianZhi/JianZhi06/Solution.cs
@@ -0,0 +1,35 @@
+﻿namespace JianZhi06
+{
+    public class Solution
+    {
+        /// <summary>
+        /// 时间复杂度 O(n)，第一次遍历链表获取长度 n，第二次遍历链表获取值 n，合计 2n，忽略常数为 O(n)
+        /// 空间复杂度 O(n)，为返回数组空间
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public int[] ReversePrint(ListNode head)
+        {
+            // 先获取链表的长度
+            var length = 0;
+            var node = head;
+            while (node != null)
+            {
+                length++;
+                node = node.next;
+            }
+
+            // 从后往前填充数组
+            var result = new int[length];
+            node = head;
+            while (node != null)
+            {
+                length--;
+                result[length] = node.val;
+                node = node.next;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/JianZhi/JianZhi06/TestCases.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace JianZhi06
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            var solution = new Solution();
+            var head = new ListNode(1)
+            {
+                next = new ListNode(3)
+                {
+                    next = new ListNode(2)
+                }
+            };
+
+            // Act
+            var result = solution.ReversePrint(head);
+
+            // Assert
+            result.Should().Equal(2, 3, 1);
+        }
+    }
+}
