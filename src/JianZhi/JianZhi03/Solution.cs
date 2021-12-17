6c471a337e9e835c29ddc632e8c901f5701030a4
h82258652 <842053625@qq.com>
2021-12-17 14:53:28 +0800

    submit JianZhi03 solution

---
+++ src/JianZhi/JianZhi03/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JianZhi03
+{
+    public class Solution
+    {
+        /// <summary>
+        /// 时间复杂度 O(n)
+        /// 空间复杂度 O(n)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int FindRepeatNumber(int[] nums)
+        {
+            var set = new HashSet<int>();
+            foreach (var num in nums)
+            {
+                if (!set.Add(num))
+                {
+                    return num;
+                }
+            }
+
+            throw new InvalidOperationException();
+        }
+    }
+}
---
+++ src/JianZhi/JianZhi03/TestCases.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace JianZhi03
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 2, 3, 1, 0, 2, 5, 3 }, new[] { 2, 3 })]
+        public void Test(int[] nums, int[] validValues)
+        {
+            // Arrange
+            var solution = new Solution();
+
+            // Act
+            var repeatNumber = solution.FindRepeatNumber(nums);
+
+            // Assert
+            repeatNumber.Should().BeOneOf(validValues);
+        }
+    }
+}
