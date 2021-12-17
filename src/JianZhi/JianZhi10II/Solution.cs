983f2c54c8bb44b53a38ce94d3b64153f1d55af5
h82258652 <842053625@qq.com>
2021-12-17 17:33:15 +0800

    submit JianZhi10II solution

---
+++ src/JianZhi/JianZhi10II/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace JianZhi10II
+{
+    public class Solution
+    {
+        /// <summary>
+        /// 时间复杂度 O(n)
+        /// 空间复杂度 O(1)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int NumWays(int n)
+        {
+            // n==0, 1
+            // n==1, 1
+            if (n <= 1)
+            {
+                return 1;
+            }
+
+            var minus2 = 1;// n=1时
+            var minus1 = 2;// n=2时
+            for (var i = 3; i <= n; i++)
+            {
+                var current = minus1 + minus2;
+                current %= 1000000007;// 题目需要取模
+                minus2 = minus1;
+                minus1 = current;
+            }
+
+            return minus1;
+        }
+    }
+}
---
+++ src/JianZhi/JianZhi10II/TestCases.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace JianZhi10II
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(2, 2)]
+        [InlineData(7, 21)]
+        [InlineData(0, 1)]
+        public void Test(int n, int excepted)
+        {
+            // Arrange
+            var solution = new Solution();
+
+            // Act
+            var result = solution.NumWays(n);
+
+            // Assert
+            result.Should().Be(excepted);
+        }
+    }
+}
