27cb32e7b7f811783f5d5e4a7218ac246204730c
h82258652 <842053625@qq.com>
2021-12-18 15:19:30 +0800

    submit 419 solution

---
+++ src/LeetCode419/Solution.cs
@@ -0,0 +1,64 @@
+﻿namespace LeetCode419
+{
+    public class Solution
+    {
+        public int CountBattleships(char[][] board)
+        {
+            var m = board.Length;
+            var n = board[0].Length;
+
+            var visited = new bool[m][];
+
+            for (var i = 0; i < m; i++)
+            {
+                visited[i] = new bool[n];
+            }
+
+            var result = 0;
+            for (var i = 0; i < m; i++)
+            {
+                for (var j = 0; j < n; j++)
+                {
+                    if (visited[i][j])
+                    {
+                        continue;
+                    }
+
+                    if (board[i][j] == '.')
+                    {
+                        continue;
+                    }
+
+                    result++;
+                    Dfs(board, i, j, m, n, visited);
+                }
+            }
+
+            return result;
+        }
+
+        private void Dfs(char[][] board, int i, int j, int m, int n, bool[][] visited)
+        {
+            if (i < 0 || j < 0 || i >= m || j >= n)
+            {
+                return;
+            }
+
+            if (visited[i][j])
+            {
+                return;
+            }
+
+            visited[i][j] = true;
+            if (board[i][j] == '.')
+            {
+                return;
+            }
+
+            Dfs(board, i - 1, j, m, n, visited);
+            Dfs(board, i + 1, j, m, n, visited);
+            Dfs(board, i, j - 1, m, n, visited);
+            Dfs(board, i, j + 1, m, n, visited);
+        }
+    }
+}
---
+++ src/LeetCode419/TestCases.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode419
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(@"[[""X"",""."",""."",""X""],
+                       [""."",""."",""."",""X""],
+                       [""."",""."",""."",""X""]]", 2)]
+        [InlineData(@"[["".""]]", 0)]
+        public void Test(string boardJson, int excepted)
+        {
+            var board = JsonSerializer.Deserialize<char[][]>(boardJson);
+            var solution = new Solution();
+            var result = solution.CountBattleships(board);
+            result.Should().Be(excepted);
+        }
+    }
+}
