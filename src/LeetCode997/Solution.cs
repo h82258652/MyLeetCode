680c387c95338e77be2265683cef4f01f26b5e83
h82258652 <842053625@qq.com>
2021-12-19 14:10:34 +0800

    submit 997 solution

---
+++ src/LeetCode997/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode997
+{
+    public class Solution
+    {
+        public int FindJudge(int n, int[][] trust)
+        {
+            var judge = new int[n + 1];
+
+            foreach (var temp in trust)
+            {
+                var a = temp[0];
+                var b = temp[1];
+                judge[a] = -1;
+                if (judge[b] != -1)
+                {
+                    judge[b]++;
+                }
+            }
+
+            var judgeList = new List<int>();
+            for (var i = 1; i <= n; i++)
+            {
+                if (judge[i] == n - 1)
+                {
+                    judgeList.Add(i);
+                }
+            }
+
+            if (judgeList.Count == 1)
+            {
+                return judgeList[0];
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ src/LeetCode997/TestCases.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode997
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(2, "[[1,2]]", 2)]
+        [InlineData(3, "[[1,3],[2,3]]", 3)]
+        [InlineData(3, "[[1,3],[2,3],[3,1]]", -1)]
+        [InlineData(3, "[[1,2],[2,3]]", -1)]
+        [InlineData(4, "[[1,3],[1,4],[2,3],[2,4],[4,3]]", 3)]
+        public void Test(int n, string trustJson, int excepted)
+        {
+            var trust = JsonSerializer.Deserialize<int[][]>(trustJson);
+            var solution = new Solution();
+            var result = solution.FindJudge(n, trust);
+            result.Should().Be(excepted);
+        }
+    }
+}
