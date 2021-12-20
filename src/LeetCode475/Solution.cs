7ed7ee2d7c09ad72c40ef3a405bf9b139858ea1c
h82258652 <842053625@qq.com>
2021-12-20 09:28:39 +0800

    submit 475 solution

---
+++ src/LeetCode475/Solution.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode475
+{
+    public class Solution
+    {
+        public int FindRadius(int[] houses, int[] heaters)
+        {
+            Array.Sort(houses);
+            Array.Sort(heaters);
+
+            var houseCount = houses.Length;
+            var heaterCount = heaters.Length;
+
+            // 从左往右，房子离供暖器最近的距离
+            var left = new int[houseCount];
+            Array.Fill(left, int.MaxValue);
+
+            var heaterIndex = 0;
+            var houseIndex = 0;
+            while (heaterIndex < heaterCount && houseIndex < houseCount)
+            {
+                var heater = heaters[heaterIndex];
+                var house = houses[houseIndex];
+                if (house <= heater)
+                {
+                    left[houseIndex] = heater - house;
+                    houseIndex++;
+                }
+                else
+                {
+                    heaterIndex++;
+                }
+            }
+
+            // 从右往左，房子离供暖器最近的距离
+            var right = new int[houseCount];
+            Array.Fill(right, int.MaxValue);
+
+            heaterIndex = heaterCount - 1;
+            houseIndex = houseCount - 1;
+            while (heaterIndex >= 0 && houseIndex >= 0)
+            {
+                var heater = heaters[heaterIndex];
+                var house = houses[houseIndex];
+                if (house >= heater)
+                {
+                    right[houseIndex] = house - heater;
+                    houseIndex--;
+                }
+                else
+                {
+                    heaterIndex--;
+                }
+            }
+
+            // 每个房间离供暖器最近的距离
+            var nearest = new int[houseCount];
+            for (var i = 0; i < houseCount; i++)
+            {
+                nearest[i] = Math.Min(left[i], right[i]);
+            }
+
+            // 要使供暖器覆盖到每个房子，选最大值
+            return nearest.Max();
+        }
+    }
+}
---
+++ src/LeetCode475/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode475
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 3 }, new[] { 2 }, 1)]
+        [InlineData(new[] { 1, 2, 3, 4 }, new[] { 1, 4 }, 1)]
+        [InlineData(new[] { 1, 5 }, new[] { 2 }, 3)]
+        [InlineData(new[] { 282475249, 622650073, 984943658, 144108930, 470211272, 101027544, 457850878, 458777923 }, new[] { 823564440, 115438165, 784484492, 74243042, 114807987, 137522503, 441282327, 16531729, 823378840, 143542612 }, 161834419)]
+        public void Test(int[] houses, int[] heaters, int expected)
+        {
+            var solution = new Solution();
+            var result = solution.FindRadius(houses, heaters);
+            result.Should().Be(expected);
+        }
+    }
+}
