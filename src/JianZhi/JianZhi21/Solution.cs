03725c35b7c96b0195478d52d8b402e3a67b97b8
h82258652 <842053625@qq.com>
2021-12-20 13:50:45 +0800

    submit JianZhi21 solution

---
+++ src/JianZhi/JianZhi21/Solution.cs
@@ -0,0 +1,40 @@
+﻿namespace JianZhi21
+{
+    public class Solution
+    {
+        /// <summary>
+        /// 时间复杂度 O(n)
+        /// 空间复杂度 O(1)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int[] Exchange(int[] nums)
+        {
+            var left = 0;
+            var right = nums.Length - 1;
+            while (left < right)
+            {
+                var leftVal = nums[left];
+                if ((leftVal & 1) == 1)// leftVal%2==1
+                {
+                    left++;
+                    continue;
+                }
+
+                var rightVal = nums[right];
+                if ((rightVal & 1) == 0)// rightVal%2==0
+                {
+                    right--;
+                    continue;
+                }
+
+                nums[left] = rightVal;
+                nums[right] = leftVal;
+                left++;
+                right--;
+            }
+
+            return nums;
+        }
+    }
+}
---
+++ src/JianZhi/JianZhi21/TestCases.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace JianZhi21
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 4 })]
+        public void Test(int[] nums)
+        {
+            // Arrange
+            var solution = new Solution();
+
+            // Act
+            var result = solution.Exchange(nums);
+
+            // Assert
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (i < nums.Length / 2)
+                {
+                    result[i].Should().Match(temp => temp % 2 == 1);
+                }
+                else if (i > nums.Length / 2)
+                {
+                    result[i].Should().Match(temp => temp % 2 == 0);
+                }
+            }
+        }
+    }
+}
