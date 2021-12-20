0c33c9d622450e3f20568a0e20cc44593b4035d5
h82258652 <842053625@qq.com>
2021-12-20 11:30:29 +0800

    submit JianZhi17 solution

---
+++ src/JianZhi/JianZhi17/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+namespace JianZhi17
+{
+    public class Solution
+    {
+        /// <summary>
+        /// 时间复杂度 O(10^n)
+        /// 空间复杂度 O(1)，返回结果不算入空间复杂度
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int[] PrintNumbers(int n)
+        {
+            var count = Pow(10, n) - 1;
+            return Enumerable.Range(1, count).ToArray();
+        }
+
+        private int Pow(int x, int y)
+        {
+            var result = 1;
+            while (y > 0)
+            {
+                result *= x;
+                y--;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/JianZhi/JianZhi17/TestCases.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace JianZhi17
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(1, new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 })]
+        public void Test(int n, int[] expected)
+        {
+            // Arrange
+            var solution = new Solution();
+
+            // Act
+            var numbers = solution.PrintNumbers(n);
+
+            // Assert
+            numbers.Should().Equal(expected);
+        }
+    }
+}
