4704c13c497c72ce0b6addf975d9109139a28690
h82258652 <842053625@qq.com>
2021-12-20 13:31:32 +0800

    submit JianZhi18 solution

---
+++ src/JianZhi/JianZhi18/Solution.cs
@@ -0,0 +1,42 @@
+﻿namespace JianZhi18
+{
+    public class Solution
+    {
+        /// <summary>
+        /// 时间复杂度 O(n)，n为链表长度
+        /// 空间复杂度 O(1)
+        /// </summary>
+        /// <param name="head"></param>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        public ListNode DeleteNode(ListNode head, int val)
+        {
+            if (head == null)
+            {
+                return null;
+            }
+
+            // 如果第一个节点是目标，则直接返回next
+            if (head.val == val)
+            {
+                return head.next;
+            }
+
+            var previous = head;
+            var node = head;
+            while (node != null)
+            {
+                if (node.val == val)
+                {
+                    previous.next = node.next;
+                    break;
+                }
+
+                previous = node;
+                node = node.next;
+            }
+
+            return head;
+        }
+    }
+}
---
+++ src/JianZhi/JianZhi18/TestCases.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace JianZhi18
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            var solution = new Solution();
+            var head = new ListNode(4)
+            {
+                next = new ListNode(5)
+                {
+                    next = new ListNode(1)
+                    {
+                        next = new ListNode(9)
+                    }
+                }
+            };
+
+            // Act
+            var newHead = solution.DeleteNode(head, 5);
+
+            // Assert
+            newHead.val.Should().Be(4);
+            newHead.next.val.Should().Be(1);
+            newHead.next.next.val.Should().Be(9);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            // Arrange
+            var solution = new Solution();
+            var head = new ListNode(4)
+            {
+                next = new ListNode(5)
+                {
+                    next = new ListNode(1)
+                    {
+                        next = new ListNode(9)
+                    }
+                }
+            };
+
+            // Act
+            var newHead = solution.DeleteNode(head, 1);
+
+            // Assert
+            newHead.val.Should().Be(4);
+            newHead.next.val.Should().Be(5);
+            newHead.next.next.val.Should().Be(9);
+        }
+    }
+}
