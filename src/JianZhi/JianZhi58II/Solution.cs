bbd7b2e2ae6371486c1c43077c58728e0f3d35e4
h82258652 <842053625@qq.com>
2021-12-20 16:39:25 +0800

    submit JianZhi58II solution

---
+++ src/JianZhi/JianZhi58II/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace JianZhi58II
+{
+    public class Solution
+    {
+        public string ReverseLeftWords(string s, int n)
+        {
+            var length = s.Length;
+            n %= length;
+            if (n <= 0)
+            {
+                return s;
+            }
+
+            var chars = s.ToCharArray();
+            Array.Reverse(chars);
+            Array.Reverse(chars, 0, length - n);
+            Array.Reverse(chars, length - n, n);
+            return new string(chars);
+        }
+    }
+}
---
+++ src/JianZhi/JianZhi58II/TestCases.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace JianZhi58II
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("abcdefg", 2, "cdefgab")]
+        [InlineData("lrloseumgh", 6, "umghlrlose")]
+        public void Test(string s, int n, string excepted)
+        {
+            // Arrange
+            var solution = new Solution();
+
+            // Act
+            var result = solution.ReverseLeftWords(s, n);
+
+            // Assert
+            result.Should().Be(excepted);
+        }
+    }
+}
