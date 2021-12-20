a281c0bcad0b62e87e53caf4fddd3e8e8a268c0c
h82258652 <842053625@qq.com>
2021-12-20 17:25:52 +0800

    submit JianZhi57 solution

---
+++ src/JianZhi/JianZhi57/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace JianZhi57
+{
+    public class Solution
+    {
+        /// <summary>
+        /// 时间复杂度 O(n)
+        /// 空间复杂度 O(1)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var left = 0;
+            var right = nums.Length - 1;
+            while (left < right)
+            {
+                var leftVal = nums[left];
+                var rightVal = nums[right];
+                var sum = leftVal + rightVal;
+                if (sum == target)
+                {
+                    return new[] { leftVal, rightVal };
+                }
+
+                if (sum < target)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+
+            throw new InvalidOperationException();
+        }
+    }
+}
---
+++ src/JianZhi/JianZhi57/TestCases.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace JianZhi57
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 2, 7, 11, 15 }, 9, new[] { 2, 7 })]
+        [InlineData(new[] { 10, 26, 30, 31, 47, 60 }, 40, new[] { 10, 30 })]
+        public void Test(int[] nums, int target, int[] expectation)
+        {
+            // Arrange
+            var solution = new Solution();
+
+            // Act
+            var result = solution.TwoSum(nums, target);
+
+            // Assert
+            result.Should().BeEquivalentTo(expectation);
+        }
+    }
+}
