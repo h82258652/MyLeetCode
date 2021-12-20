747a0f4130e8a6b9d7f1db520e9eeba88bf5a338
h82258652 <842053625@qq.com>
2021-12-20 16:17:22 +0800

    submit JianZhi58I solution

---
+++ src/JianZhi/JianZhi58I/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace JianZhi58I
+{
+    public class Solution
+    {
+        /// <summary>
+        /// 时间复杂度 O(n)，
+        /// 空间复杂度 O(n)
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public string ReverseWords(string s)
+        {
+            var words = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Array.Reverse(words);
+            return string.Join(' ', words);
+        }
+    }
+}
---
+++ src/JianZhi/JianZhi58I/TestCases.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace JianZhi58I
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("the sky is blue", "blue is sky the")]
+        [InlineData("  hello world!  ", "world! hello")]
+        [InlineData("a good   example", "example good a")]
+        public void Test(string s, string expected)
+        {
+            // Arrange
+            var solution = new Solution();
+
+            // Act
+            var result = solution.ReverseWords(s);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+    }
+}
