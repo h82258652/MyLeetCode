e7a6326729d6177ed7a20f5a226536e99fed625b
h82258652 <842053625@qq.com>
2021-12-20 11:18:28 +0800

    submit JianZhi15 solution

---
+++ src/JianZhi/JianZhi15/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace JianZhi15
+{
+    public class Solution
+    {
+        /// <summary>
+        /// 时间复杂度 O(n)，n为输入的二进制的位数
+        /// 空间复杂度 O(1)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int HammingWeight(uint n)
+        {
+            var result = 0;
+            while (n > 0)
+            {
+                if ((n & 1) == 1)// n%2==1
+                {
+                    result++;
+                }
+
+                n >>= 1;// n=n/2
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/JianZhi/JianZhi15/TestCases.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace JianZhi15
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(0b00000000000000000000000000001011u, 3)]
+        [InlineData(0b00000000000000000000000010000000u, 1)]
+        [InlineData(0b11111111111111111111111111111101u, 31)]
+        public void Test(uint n, int expected)
+        {
+            // Arrange
+            var solution = new Solution();
+
+            // Act
+            var result = solution.HammingWeight(n);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+    }
+}
