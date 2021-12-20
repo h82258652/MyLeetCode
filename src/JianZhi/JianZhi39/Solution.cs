56cf6806efe233b98de1e67d7ac5b750e4d7c6da
h82258652 <842053625@qq.com>
2021-12-20 15:48:43 +0800

    submit JianZhi39 solution

---
+++ src/JianZhi/JianZhi39/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JianZhi39
+{
+    public class Solution
+    {
+        public int MajorityElement(int[] nums)
+        {
+            var length = nums.Length;
+            var numCount = new Dictionary<int, int>();
+
+            for (var i = 0; i < length; i++)
+            {
+                var num = nums[i];
+                numCount.TryGetValue(num, out var count);
+                count++;
+                if (count == (length + 1) / 2)
+                {
+                    return num;
+                }
+
+                numCount[num] = count;
+            }
+
+            throw new InvalidOperationException();
+        }
+    }
+}
---
+++ src/JianZhi/JianZhi39/TestCases.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace JianZhi39
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 2, 2, 2, 5, 4, 2 }, 2)]
+        public void Test(int[] nums, int excepted)
+        {
+            // Solution
+            var solution = new Solution();
+
+            // Act
+            var majority = solution.MajorityElement(nums);
+
+            // Assert
+            majority.Should().Be(excepted);
+        }
+    }
+}
