57485df6c8b200e2df83b1a5f170c978d4e0ba5a
h82258652 <842053625@qq.com>
2021-12-20 14:14:47 +0800

    submit JianZhi27 solution

---
+++ src/JianZhi/JianZhi27/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace JianZhi27
+{
+    public class Solution
+    {
+        /// <summary>
+        /// 时间复杂度 O(n)，n为节点个数
+        /// 空间复杂度 O(logn)，为递归堆栈空间
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public TreeNode MirrorTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+
+            (root.left, root.right) = (root.right, root.left);
+            MirrorTree(root.left);
+            MirrorTree(root.right);
+            return root;
+        }
+    }
+}
---
+++ src/JianZhi/JianZhi27/TestCases.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace JianZhi27
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            var solution = new Solution();
+            var root = new TreeNode(4)
+            {
+                left = new TreeNode(2)
+                {
+                    left = new TreeNode(1),
+                    right = new TreeNode(3)
+                },
+                right = new TreeNode(7)
+                {
+                    left = new TreeNode(6),
+                    right = new TreeNode(9)
+                }
+            };
+
+            // Act
+            var mirrorRoot = solution.MirrorTree(root);
+
+            // Assert
+            mirrorRoot.val.Should().Be(4);
+            mirrorRoot.left.val.Should().Be(7);
+            mirrorRoot.right.val.Should().Be(2);
+            mirrorRoot.left.left.val.Should().Be(9);
+            mirrorRoot.left.right.val.Should().Be(6);
+            mirrorRoot.right.left.val.Should().Be(3);
+            mirrorRoot.right.right.val.Should().Be(1);
+        }
+    }
+}
