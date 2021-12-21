df520fc098434c803e90f1da030aa7ad1fe1f513
h82258652 <842053625@qq.com>
2021-12-21 10:08:03 +0800

    submit 008 solution

---
+++ src/LeetCode008/Solution.cs
@@ -0,0 +1,108 @@
+﻿namespace LeetCode008
+{
+    public class Solution
+    {
+        public int MyAtoi(string s)
+        {
+            var result = 0;
+
+            var sign = 0;// 正负，1代表>=0，0代表>=0，-1代表<0
+            var started = false;// 是否已经开始计算，用于忽略前导零
+            var digitStarted = false;
+            var length = s.Length;
+            for (var i = 0; i < length; i++)
+            {
+                var c = s[i];
+                if (c == ' ')
+                {
+                    if (!started)
+                    {
+                        continue;
+                    }
+
+                    break;
+                }
+
+                started = true;
+                if (c == '+')
+                {
+                    if (digitStarted)
+                    {
+                        break;
+                    }
+
+                    if (sign != 0)
+                    {
+                        break;
+                    }
+
+                    sign = 1;
+                }
+                else if (c == '-')
+                {
+                    if (digitStarted)
+                    {
+                        break;
+                    }
+
+                    if (sign != 0)
+                    {
+                        break;
+                    }
+
+                    sign = -1;
+                }
+                else if (char.IsDigit(c))
+                {
+                    digitStarted = true;
+
+                    // check overflow
+                    if (sign == 0 || sign == 1)
+                    {
+                        if (result > int.MaxValue / 10)
+                        {
+                            return int.MaxValue;
+                        }
+                    }
+                    else
+                    {
+                        if (result < int.MinValue / 10)
+                        {
+                            return int.MinValue;
+                        }
+                    }
+
+                    result *= 10;
+
+                    var plus = c - '0';
+
+                    // check overflow
+                    if (sign == 0 || sign == 1)
+                    {
+                        if (result > int.MaxValue - plus)
+                        {
+                            return int.MaxValue;
+                        }
+
+                        result += plus;
+                    }
+                    else
+                    {
+                        if (result < int.MinValue + plus)
+                        {
+                            return int.MinValue;
+                        }
+
+                        result -= plus;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode008/TestCases.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode008
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("42", 42)]
+        [InlineData("   -42", -42)]
+        [InlineData("4193 with words", 4193)]
+        [InlineData("words and 987", 0)]
+        [InlineData("-91283472332", -2147483648)]
+        [InlineData("+1", 1)]
+        [InlineData("00000-42a1234", 0)]
+        public void Test(string s, int expected)
+        {
+            var solution = new Solution();
+            var result = solution.MyAtoi(s);
+            result.Should().Be(expected);
+        }
+    }
+}
