d5e763e776b31315953f77c5d9bc4736dd47a980
h82258652 <842053625@qq.com>
2021-12-21 22:58:25 +0800

    submit JianZhi50 solution

---
+++ src/JianZhi/JianZhi50/Solution.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace JianZhi50
+{
+    public class Solution
+    {
+        /// <summary>
+        /// 时间复杂度 O(n)，n为s的长度
+        /// 空间复杂度 O(1)
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public char FirstUniqChar(string s)
+        {
+            var length = s.Length;
+            if (length <= 0)
+            {
+                return ' ';
+            }
+
+            var firstIndex = new int[26];// 每个字母第一次出现的索引
+            Array.Fill(firstIndex, -1);// 负1代表未查找
+            for (var i = 0; i < length; i++)
+            {
+                var c = s[i];
+                var delta = c - 'a';
+                if (firstIndex[delta] == -1)
+                {
+                    firstIndex[delta] = i;
+                }
+                else
+                {
+                    firstIndex[delta] = -2;// 用负2代表重复
+                }
+            }
+
+            var result = ' ';
+            var minIndex = int.MaxValue;
+            for (var i = 0; i < 26; i++)
+            {
+                var index = firstIndex[i];
+                if (index < 0)
+                {
+                    continue;
+                }
+
+                if (index < minIndex)
+                {
+                    result = (char)('a' + i);
+                    minIndex = firstIndex[i];
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/JianZhi/JianZhi50/TestCases.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace JianZhi50
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("abaccdeff", 'b')]
+        [InlineData("", ' ')]
+        public void Test(string s, char excepted)
+        {
+            // Arrange
+            var solution = new Solution();
+
+            // Act
+            var uniqChar = solution.FirstUniqChar(s);
+
+            // Assert
+            uniqChar.Should().Be(excepted);
+        }
+    }
+}
