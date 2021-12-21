132428d22dd87618effb8e61e172c60ad42f05a8
h82258652 <842053625@qq.com>
2021-12-21 23:10:53 +0800

    submit JianZhi55I solution

---
+++ src/JianZhi/JianZhi55I/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace JianZhi55I
+{
+    public class Solution
+    {
+        /// <summary>
+        /// 时间复杂度 O(n)，n为树的节点个数
+        /// 空间复杂度 O(n)，用于递归的栈
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            return Math.Max(MaxDepth(root.left), MaxDepth(root.right)) + 1;
+        }
+    }
+}
---
+++ src/JianZhi/JianZhi55I/TestCases.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace JianZhi55I
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            var solution = new Solution();
+            var root = new TreeNode(3)
+            {
+                left = new TreeNode(9),
+                right = new TreeNode(20)
+                {
+                    left = new TreeNode(15),
+                    right = new TreeNode(7)
+                }
+            };
+
+            // Act
+            var maxDepth = solution.MaxDepth(root);
+
+            // Assert
+            maxDepth.Should().Be(3);
+        }
+    }
+}
