8ceb9f1b5a6421b7e7548ed6c73c55cb0a7edacd
h82258652 <842053625@qq.com>
2021-12-21 16:49:01 +0800

    submit 2000 solution

---
+++ src/LeetCode2000/Solution.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode2000
+{
+    public class Solution
+    {
+        public string ReversePrefix(string word, char ch)
+        {
+            var chIndex = 0;
+            while (chIndex < word.Length)
+            {
+                if (word[chIndex] == ch)
+                {
+                    break;
+                }
+
+                chIndex++;
+            }
+
+            if (chIndex >= word.Length)
+            {
+                return word;
+            }
+
+            var wordArray = word.ToCharArray();
+
+            var left = 0;
+            while (left < chIndex)
+            {
+                (wordArray[left], wordArray[chIndex]) = (wordArray[chIndex], wordArray[left]);
+
+                left++;
+                chIndex--;
+            }
+
+            return new string(wordArray);
+        }
+    }
+}
---
+++ src/LeetCode2000/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2000
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("abcdefd", 'd', "dcbaefd")]
+        [InlineData("xyxzxe", 'z', "zxyxxe")]
+        [InlineData("abcd", 'z', "abcd")]
+        public void Test(string word, char ch, string expected)
+        {
+            var solution = new Solution();
+            var result = solution.ReversePrefix(word, ch);
+            result.Should().Be(expected);
+        }
+    }
+}
