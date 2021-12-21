e2396ee53e6d2b6a47018a6bf6bb12808e0a954a
h82258652 <842053625@qq.com>
2021-12-21 17:11:18 +0800

    submit 821 solution

---
+++ src/LeetCode821/Solution.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace LeetCode821
+{
+    public class Solution
+    {
+        public int[] ShortestToChar(string s, char c)
+        {
+            var length = s.Length;
+
+            var leftResult = new int[length];
+            var distance = int.MaxValue;
+            for (var i = 0; i < length; i++)
+            {
+                if (s[i] == c)
+                {
+                    distance = 0;
+                }
+                else
+                {
+                    if (distance < int.MaxValue)
+                    {
+                        distance++;
+                    }
+                }
+
+                leftResult[i] = distance;
+            }
+
+            var rightResult = new int[length];
+            distance = int.MaxValue;
+            for (var i = length - 1; i >= 0; i--)
+            {
+                if (s[i] == c)
+                {
+                    distance = 0;
+                }
+                else
+                {
+                    if (distance < int.MaxValue)
+                    {
+                        distance++;
+                    }
+                }
+
+                rightResult[i] = distance;
+            }
+
+            var result = new int[length];
+            for (var i = 0; i < length; i++)
+            {
+                result[i] = Math.Min(leftResult[i], rightResult[i]);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode821/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode821
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("loveleetcode", 'e', new[] { 3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0 })]
+        [InlineData("aaab", 'b', new[] { 3, 2, 1, 0 })]
+        public void Test(string s, char c, int[] expected)
+        {
+            Solution solution = new Solution();
+            var result = solution.ShortestToChar(s, c);
+            result.Should().Equal(expected);
+        }
+    }
+}
