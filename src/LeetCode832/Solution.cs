14c6c7a6b527a46a1961d1eb514eac4733b02671
h82258652 <842053625@qq.com>
2021-12-21 14:04:38 +0800

    submit 832 solution

---
+++ src/LeetCode832/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LeetCode832
+{
+    public class Solution
+    {
+        public int[][] FlipAndInvertImage(int[][] image)
+        {
+            for (var i = 0; i < image.Length; i++)
+            {
+                Array.Reverse(image[i]);
+            }
+
+            for (var i = 0; i < image.Length; i++)
+            {
+                for (var j = 0; j < image[i].Length; j++)
+                {
+                    image[i][j] = 1 - image[i][j];
+                }
+            }
+
+            return image;
+        }
+    }
+}
---
+++ src/LeetCode832/TestCases.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode832
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var image = new[]
+            {
+                new[] { 1, 1, 0 },
+                new[] { 1, 0, 1 },
+                new[] { 0, 0, 0 }
+            };
+            var result = solution.FlipAndInvertImage(image);
+            var expectation = new[]
+            {
+                new[] { 1, 0, 0 },
+                new[] { 0, 1, 0 },
+                new[] { 1, 1, 1 }
+            };
+            result.Should().BeEquivalentTo(expectation);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var image = new[]
+            {
+                new[] { 1, 1, 0, 0 },
+                new[] { 1, 0, 0, 1 },
+                new[] { 0, 1, 1, 1 },
+                new[] { 1, 0, 1, 0 }
+            };
+            var result = solution.FlipAndInvertImage(image);
+            var expectation = new[]
+            {
+                new[] { 1, 1, 0, 0 },
+                new[] { 0, 1, 1, 0 },
+                new[] { 0, 0, 0, 1 },
+                new[] { 1, 0, 1, 0 }
+            };
+            result.Should().BeEquivalentTo(expectation);
+        }
+    }
+}
