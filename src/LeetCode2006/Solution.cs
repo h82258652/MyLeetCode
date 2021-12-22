d324241fb54793ca710cd3bbc28eb4860faef210
h82258652 <842053625@qq.com>
2021-12-22 19:35:27 +0800

    submit 2006 solution

---
+++ src/LeetCode2006/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LeetCode2006
+{
+    public class Solution
+    {
+        public int CountKDifference(int[] nums, int k)
+        {
+            var result = 0;
+
+            var length = nums.Length;
+            for (var i = 0; i < length; i++)
+            {
+                for (var j = i + 1; j < length; j++)
+                {
+                    if (Math.Abs(nums[i] - nums[j]) == k)
+                    {
+                        result++;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode2006/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2006
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 2, 1 }, 1, 4)]
+        [InlineData(new[] { 1, 3 }, 3, 0)]
+        [InlineData(new[] { 3, 2, 1, 5, 4 }, 2, 3)]
+        public void Test(int[] nums, int k, int expected)
+        {
+            var solution = new Solution();
+            var result = solution.CountKDifference(nums, k);
+            result.Should().Be(expected);
+        }
+    }
+}
