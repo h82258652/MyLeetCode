0d73cd43104790aa8e3eaa60abc28bc111c0a818
h82258652 <842053625@qq.com>
2021-12-22 15:41:12 +0800

    submit JianZhi22 solution

---
+++ src/JianZhi/JianZhi22/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace JianZhi22
+{
+    public class Solution
+    {
+        /// <summary>
+        /// 时间复杂度 O(n)，n为链表长度
+        /// 空间复杂度 O(1)
+        /// </summary>
+        /// <param name="head"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public ListNode GetKthFromEnd(ListNode head, int k)
+        {
+            var length = 0;
+            var node = head;
+            while (node != null)
+            {
+                length++;
+                node = node.next;
+            }
+
+            length -= k;
+            node = head;
+            while (length > 0)
+            {
+                node = node.next;
+                length--;
+            }
+
+            return node;
+        }
+    }
+}
---
+++ src/JianZhi/JianZhi22/TestCases.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace JianZhi22
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            var solution = new Solution();
+            var head = new ListNode(1)
+            {
+                next = new ListNode(2)
+                {
+                    next = new ListNode(3)
+                    {
+                        next = new ListNode(4)
+                        {
+                            next = new ListNode(5)
+                        }
+                    }
+                }
+            };
+
+            // Act
+            var result = solution.GetKthFromEnd(head, 2);
+
+            // Assert
+            result.val.Should().Be(4);
+            result.next.val.Should().Be(5);
+        }
+    }
+}
