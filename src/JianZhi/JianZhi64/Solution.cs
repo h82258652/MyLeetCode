7fb65c0dcae8703115ec25d2effb5e6086cddffb
h82258652 <842053625@qq.com>
2021-12-22 16:57:28 +0800

    submit JianZhi64 solution

---
+++ src/JianZhi/JianZhi64/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace JianZhi64
+{
+    public class Solution
+    {
+        /// <summary>
+        /// 时间复杂度 O(1)
+        /// 空间复杂度 O(1)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int SumNums(int n)
+        {
+            // sum = n * (n + 1) / 2
+            // sum = (n * n + n) / 2
+            // sum = (n * n + n) >> 1
+            return ((int)Math.Pow(n, 2) + n) >> 1;
+        }
+    }
+}
---
+++ src/JianZhi/JianZhi64/TestCases.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace JianZhi64
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(3, 6)]
+        [InlineData(9, 45)]
+        public void Test(int n, int expected)
+        {
+            // Arrange
+            var solution = new Solution();
+
+            // Act
+            var sum = solution.SumNums(n);
+
+            // Assert
+            sum.Should().Be(expected);
+        }
+    }
+}
