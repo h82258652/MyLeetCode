8f8af5ab7726119b4d44b39c25d34ce95ab649c4
h82258652 <842053625@qq.com>
2021-12-22 09:19:53 +0800

    submit 686 solution

---
+++ src/LeetCode686/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode686
+{
+    public class Solution
+    {
+        public int RepeatedStringMatch(string a, string b)
+        {
+            var repeatCount = (int)Math.Ceiling(b.Length * 1d / a.Length);
+            var builder = new StringBuilder();
+            for (var i = 0; i < repeatCount; i++)
+            {
+                builder.Append(a);
+            }
+
+            if (builder.ToString().Contains(b))
+            {
+                return repeatCount;
+            }
+
+            builder.Append(a);
+
+            if (builder.ToString().Contains(b))
+            {
+                return repeatCount + 1;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ src/LeetCode686/TestCases.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode686
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("abcd", "cdabcdab", 3)]
+        [InlineData("a", "aa", 2)]
+        [InlineData("a", "a", 1)]
+        [InlineData("abc", "wxyz", -1)]
+        [InlineData("abc", "cabcabca", 4)]
+        public void Test(string a, string b, int expected)
+        {
+            var solution = new Solution();
+            var result = solution.RepeatedStringMatch(a, b);
+            result.Should().Be(expected);
+        }
+    }
+}
