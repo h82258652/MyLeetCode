111c09a07852e66b08086f39d167bc6e5dd6bcb2
h82258652 <842053625@qq.com>
2021-12-22 14:45:25 +0800

    submit JianZhi16 solution

---
+++ src/JianZhi/JianZhi16/Solution.cs
@@ -0,0 +1,63 @@
+﻿namespace JianZhi16
+{
+    public class Solution
+    {
+        /// <summary>
+        /// 时间复杂度 O(logn)
+        /// 空间复杂度 O(logn)，递归堆栈
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public double MyPow(double x, int n)
+        {
+            if (x == 0)
+            {
+                return 0;
+            }
+
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            if (n == 1)
+            {
+                return x;
+            }
+
+            if (n == -1)
+            {
+                return 1 / x;
+            }
+
+            if (n > 0)
+            {
+                if ((n & 1) == 0)
+                {
+                    return MyPow(x * x, n >> 1);// 如果 n 是偶数，x^n=(x*x)^(n/2)
+                }
+                else
+                {
+                    return MyPow(x * x, n >> 1) * x;// 如果 n 是奇数，x^n=(x*x)^(n/2)*x
+                }
+            }
+            else
+            {
+                // 注意负数除2和右移1效果不一样
+
+                if ((n & 1) == 0)
+                {
+                    return MyPow(x * x, n / 2);// 如果 n 是偶数，x^(-n)=(x*x)^((-n)/2)
+                }
+                else
+                {
+                    // 如果 n 是奇数，x^(-n)=(x*x)^((-n)/2-1)*x;
+                    // 例如 x=2，n=-3
+                    // 2^(-3)=4^(-2)*2
+                    return MyPow(x * x, n / 2 - 1) * x;
+                }
+            }
+        }
+    }
+}
---
+++ src/JianZhi/JianZhi16/TestCases.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace JianZhi16
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(2.00000, 10, 1024.00000)]
+        [InlineData(2.10000, 3, 9.26100)]
+        [InlineData(2.00000, -2, 0.25000)]
+        [InlineData(2.00000, -2147483648, 0.0)]
+        [InlineData(1.00000, -2147483648, 1.0)]
+        [InlineData(-1.00000, -2147483648, 1.0)]
+        [InlineData(34.00515, -3, 0)]
+        public void Test(double x, int n, double expected)
+        {
+            // Arrange
+            var solution = new Solution();
+
+            // Act
+            var pow = solution.MyPow(x, n);
+
+            // Assert
+            pow.Should().BeApproximately(expected, 0.0001);
+        }
+    }
+}
