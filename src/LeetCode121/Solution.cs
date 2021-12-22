96f7f252e38506450620190f1ca9e5967d9d5f89
h82258652 <842053625@qq.com>
2021-12-22 20:39:47 +0800

    submit 121 solution

---
+++ src/LeetCode121/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeetCode121
+{
+    public class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            // 最大利润
+            var maxProfit = 0;
+
+            // 最小买入
+            var minBuy = prices[0];
+            for (var i = 1; i < prices.Length; i++)
+            {
+                // 今天卖出的价钱
+                var sell = prices[i];
+                if (sell - minBuy > 0)
+                {
+                    maxProfit = Math.Max(maxProfit, sell - minBuy);
+                }
+
+                // 如果今天买入比历史买入更低，那就更新为今天的价格
+                minBuy = Math.Min(minBuy, prices[i]);
+            }
+
+            return maxProfit;
+        }
+    }
+}
---
+++ src/LeetCode121/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode121
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 7, 1, 5, 3, 6, 4 }, 5)]
+        [InlineData(new[] { 7, 6, 4, 3, 1 }, 0)]
+        public void Test(int[] prices, int excepted)
+        {
+            var solution = new Solution();
+            var result = solution.MaxProfit(prices);
+            result.Should().Be(excepted);
+        }
+    }
+}
