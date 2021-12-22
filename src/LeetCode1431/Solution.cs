7208981c7c4c0d0b80d8e7a3a4761159c1ed2b5d
h82258652 <842053625@qq.com>
2021-12-22 19:47:55 +0800

    submit 1431 solution

---
+++ src/LeetCode1431/Solution.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode1431
+{
+    public class Solution
+    {
+        public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+            var max = candies.Max();
+            return candies.Select(candy => candy + extraCandies >= max).ToList();
+        }
+    }
+}
---
+++ src/LeetCode1431/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1431
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 2, 3, 5, 1, 3 }, 3, new[] { true, true, true, false, true })]
+        [InlineData(new[] { 4, 2, 1, 1, 2 }, 1, new[] { true, false, false, false, false })]
+        [InlineData(new[] { 12, 1, 12 }, 10, new[] { true, false, true })]
+        public void Test(int[] candies, int extraCandies, bool[] excepted)
+        {
+            var solution = new Solution();
+            var result = solution.KidsWithCandies(candies, extraCandies);
+            result.Should().Equal(excepted);
+        }
+    }
+}
