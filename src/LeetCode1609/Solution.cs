372b3d566e83207f06591471ec3818e2c9f4e2a2
h82258652 <842053625@qq.com>
2021-12-25 11:30:13 +0800

    submit 1609 solution

---
+++ src/LeetCode1609/Solution.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode1609
+{
+    public class Solution
+    {
+        public bool IsEvenOddTree(TreeNode root)
+        {
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            var isOdd = true;
+            var previousValue = int.MinValue;
+            while (queue.Count > 0)
+            {
+                var count = queue.Count;
+                for (var i = 0; i < count; i++)
+                {
+                    var node = queue.Dequeue();
+
+                    var val = node.val;
+                    if (isOdd)
+                    {
+                        if (val <= previousValue || (val & 1) == 0)
+                        {
+                            return false;
+                        }
+
+                        previousValue = val;
+                    }
+                    else
+                    {
+                        if (val >= previousValue || (val & 1) == 1)
+                        {
+                            return false;
+                        }
+
+                        previousValue = val;
+                    }
+
+                    var left = node.left;
+                    if (left != null)
+                    {
+                        queue.Enqueue(left);
+                    }
+
+                    var right = node.right;
+                    if (right != null)
+                    {
+                        queue.Enqueue(right);
+                    }
+                }
+
+                if (isOdd)
+                {
+                    isOdd = false;
+                    previousValue = int.MaxValue;
+                }
+                else
+                {
+                    isOdd = true;
+                    previousValue = int.MinValue;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/LeetCode1609/TestCases.cs
@@ -0,0 +1,127 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1609
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var root = new TreeNode(1)
+            {
+                left = new TreeNode(10)
+                {
+                    left = new TreeNode(3)
+                    {
+                        left = new TreeNode(12),
+                        right = new TreeNode(8)
+                    }
+                },
+                right = new TreeNode(4)
+                {
+                    left = new TreeNode(7)
+                    {
+                        left = new TreeNode(6)
+                    },
+                    right = new TreeNode(9)
+                    {
+                        right = new TreeNode(2)
+                    }
+                }
+            };
+            var result = solution.IsEvenOddTree(root);
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var root = new TreeNode(5)
+            {
+                left = new TreeNode(4)
+                {
+                    left = new TreeNode(3),
+                    right = new TreeNode(3)
+                },
+                right = new TreeNode(2)
+                {
+                    left = new TreeNode(7)
+                }
+            };
+            var result = solution.IsEvenOddTree(root);
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var solution = new Solution();
+            var root = new TreeNode(5)
+            {
+                left = new TreeNode(9)
+                {
+                    left = new TreeNode(3),
+                    right = new TreeNode(5)
+                },
+                right = new TreeNode(1)
+                {
+                    left = new TreeNode(7)
+                }
+            };
+            var result = solution.IsEvenOddTree(root);
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            var solution = new Solution();
+            var root = new TreeNode(1);
+            var result = solution.IsEvenOddTree(root);
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            var solution = new Solution();
+            var root = new TreeNode(11)
+            {
+                left = new TreeNode(8)
+                {
+                    left = new TreeNode(1)
+                    {
+                        left = new TreeNode(30)
+                        {
+                            left = new TreeNode(17)
+                        },
+                        right = new TreeNode(20)
+                    },
+                    right = new TreeNode(3)
+                    {
+                        left = new TreeNode(18),
+                        right = new TreeNode(16)
+                    }
+                },
+                right = new TreeNode(6)
+                {
+                    left = new TreeNode(9)
+                    {
+                        left = new TreeNode(12),
+                        right = new TreeNode(10)
+                    },
+                    right = new TreeNode(11)
+                    {
+                        left = new TreeNode(4),
+                        right = new TreeNode(2)
+                    }
+                }
+            };
+            var result = solution.IsEvenOddTree(root);
+            result.Should().BeTrue();
+        }
+    }
+}
