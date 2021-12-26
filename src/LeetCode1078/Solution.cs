1a8cbb45308912bca794a9a07a82c4243fea8ef5
h82258652 <842053625@qq.com>
2021-12-26 11:55:51 +0800

    submit 1078 solution

---
+++ src/LeetCode1078/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode1078
+{
+    public class Solution
+    {
+        public string[] FindOcurrences(string text, string first, string second)
+        {
+            var words = text.Split(' ');
+            var result = new List<string>();
+
+            for (var i = 0; i < words.Length; i++)
+            {
+                var word = words[i];
+                if (word != first)
+                {
+                    continue;
+                }
+
+                if (i + 1 >= words.Length || words[i + 1] != second)
+                {
+                    continue;
+                }
+
+                if (i + 2 < words.Length)
+                {
+                    result.Add(words[i + 2]);
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ src/LeetCode1078/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1078
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("alice is a good girl she is a good student", "a", "good", new[] { "girl", "student" })]
+        [InlineData("we will we will rock you", "we", "will", new[] { "we", "rock" })]
+        public void Test(string text, string first, string second, string[] expected)
+        {
+            var solution = new Solution();
+            var result = solution.FindOcurrences(text, first, second);
+            result.Should().Equal(expected);
+        }
+    }
+}
