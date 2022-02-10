4acbe4f57934d3ce604e52c499d27f261f4cd8dd
h82258652 <842053625@qq.com>
2022-02-10 09:16:20 +0800

    submit 1447 solution

---
+++ src/LeetCode1447/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode1447
+{
+    public class Solution
+    {
+        public IList<string> SimplifiedFractions(int n)
+        {
+            if (n == 1)
+            {
+                return new List<string>();
+            }
+
+            var result = new List<string>();
+
+            for (var j = 2; j <= n; j++)
+            {
+                for (var i = 1; i < j; i++)
+                {
+                    var gcd = Gcd(i, j);
+                    if (gcd == 1)
+                    {
+                        result.Add($"{i}/{j}");
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private static int Gcd(int a, int b)
+        {
+            return b == 0 ? a : Gcd(b, a % b);
+        }
+    }
+}
---
+++ src/LeetCode1447/TestCases.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1447
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var result = solution.SimplifiedFractions(2);
+            result.Should().BeEquivalentTo("1/2");
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var result = solution.SimplifiedFractions(3);
+            result.Should().BeEquivalentTo("1/2", "1/3", "2/3");
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var solution = new Solution();
+            var result = solution.SimplifiedFractions(4);
+            result.Should().BeEquivalentTo("1/2", "1/3", "1/4", "2/3", "3/4");
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            var solution = new Solution();
+            var result = solution.SimplifiedFractions(1);
+            result.Should().BeEquivalentTo(Array.Empty<string>());
+        }
+    }
+}
