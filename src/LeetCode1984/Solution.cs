5b31f657caaad8f2a99d25c5849b9a79638e1171
h82258652 <842053625@qq.com>
2022-02-11 09:31:09 +0800

    submit 1984 solution

---
+++ src/LeetCode1984/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LeetCode1984
+{
+    public class Solution
+    {
+        public int MinimumDifference(int[] nums, int k)
+        {
+            if (k == 1)
+            {
+                return 0;
+            }
+
+            Array.Sort(nums);
+            var length = nums.Length;
+            var min = int.MaxValue;
+            for (var i = 0; i + k - 1 < length; i++)
+            {
+                var low = nums[i];
+                var high = nums[i + k - 1];
+                min = Math.Min(min, high - low);
+            }
+
+            return min;
+        }
+    }
+}
---
+++ src/LeetCode1984/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1984
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 90 }, 1, 0)]
+        [InlineData(new[] { 9, 4, 1, 7 }, 2, 2)]
+        public void Test(int[] nums, int k, int expected)
+        {
+            var solution = new Solution();
+            var result = solution.MinimumDifference(nums, k);
+            result.Should().Be(expected);
+        }
+    }
+}
