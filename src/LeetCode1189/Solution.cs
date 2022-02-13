22caf5327b7dc719363b6683dfe9665760ee77c4
h82258652 <842053625@qq.com>
2022-02-13 13:36:48 +0800

    submit 1189 solution

---
+++ src/LeetCode1189/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LeetCode1189
+{
+    public class Solution
+    {
+        public int MaxNumberOfBalloons(string text)
+        {
+            int[] charCount = new int[26];
+            foreach (var c in text)
+            {
+                charCount[c - 'a']++;
+            }
+
+            int result = int.MaxValue;
+            result = Math.Min(result, charCount[1]);// b
+            result = Math.Min(result, charCount[0]);// a
+            result = Math.Min(result, charCount[11] / 2);// l
+            result = Math.Min(result, charCount[14] / 2);// o
+            result = Math.Min(result, charCount[13]);// n
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode1189/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1189
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("nlaebolko", 1)]
+        [InlineData("loonbalxballpoon", 2)]
+        [InlineData("leetcode", 0)]
+        public void Test(string text, int expected)
+        {
+            var solution = new Solution();
+            var result = solution.MaxNumberOfBalloons(text);
+            result.Should().Be(expected);
+        }
+    }
+}
