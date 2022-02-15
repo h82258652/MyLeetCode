a0e2740e1901e14992990a7a1c9b8a1dec74d055
h82258652 <842053625@qq.com>
2022-02-15 08:59:28 +0800

    submit 1380 solution

---
+++ src/LeetCode1380/Solution.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode1380
+{
+    public class Solution
+    {
+        public IList<int> LuckyNumbers(int[][] matrix)
+        {
+            var result = new List<int>();
+
+            var m = matrix.Length;
+            var n = matrix[0].Length;
+
+            var min = new int[m];
+            Array.Fill(min, int.MaxValue);
+            var max = new int[n];
+            Array.Fill(max, int.MinValue);
+
+            for (var i = 0; i < m; i++)
+            {
+                for (var j = 0; j < n; j++)
+                {
+                    var value = matrix[i][j];
+                    min[i] = Math.Min(min[i], value);
+                    max[j] = Math.Max(max[j], value);
+                }
+            }
+
+            for (var i = 0; i < m; i++)
+            {
+                for (var j = 0; j < n; j++)
+                {
+                    if (min[i] == max[j])
+                    {
+                        result.Add(min[i]);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode1380/TestCases.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1380
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var result = solution.LuckyNumbers(new[]
+            {
+                new[] { 3, 7, 8 },
+                new[] { 9, 11, 13 },
+                new[] { 15, 16, 17 }
+            });
+            result.Should().Equal(15);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var result = solution.LuckyNumbers(new[]
+            {
+                new[] { 1, 10, 4, 2 },
+                new[] { 9, 3, 8, 7 },
+                new[] { 15, 16, 17, 12 }
+            });
+            result.Should().Equal(12);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var solution = new Solution();
+            var result = solution.LuckyNumbers(new[]
+            {
+                new[] { 7, 8 },
+                new[] { 1, 2 }
+            });
+            result.Should().Equal(7);
+        }
+    }
+}
