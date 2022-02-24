e2f38e58e742745e4177d27f1679acd20958d862
h82258652 <842053625@qq.com>
2022-02-24 09:56:57 +0800

    submit 1706 solution

---
+++ src/LeetCode1706/Solution.cs
@@ -0,0 +1,82 @@
+﻿namespace LeetCode1706
+{
+    public class Solution
+    {
+        public int[] FindBall(int[][] grid)
+        {
+            var m = grid.Length;
+            var n = grid[0].Length;
+
+            var balls = new int[n];
+            for (var i = 0; i < n; i++)
+            {
+                balls[i] = i;
+            }
+
+            for (var rowIndex = 0; rowIndex < m; rowIndex++)
+            {
+                var row = grid[rowIndex];
+                for (var columnIndex = 0; columnIndex < n; columnIndex++)
+                {
+                    var ball = balls[columnIndex];
+                    if (ball == -1)
+                    {
+                        continue;
+                    }
+
+                    if (ball == 0)
+                    {
+                        if (row[0] == -1 || (n > 1 && row[1] == -1) || n == 1)
+                        {
+                            balls[columnIndex] = -1;
+                        }
+                        else
+                        {
+                            balls[columnIndex] = 1;
+                        }
+                    }
+                    else if (ball == n - 1)
+                    {
+                        if (row[n - 1] == 1 || (n - 2 >= 0 && row[n - 2] == 1) || n == 1)
+                        {
+                            balls[columnIndex] = -1;
+                        }
+                        else
+                        {
+                            balls[columnIndex] = n - 2;
+                        }
+                    }
+                    else
+                    {
+                        if (row[ball] == 1)
+                        {
+                            // 往右
+                            if (row[ball + 1] == 1)
+                            {
+                                balls[columnIndex] = ball + 1;
+                            }
+                            else
+                            {
+                                balls[columnIndex] = -1;
+                            }
+                        }
+                        else
+                        {
+                            // 往左
+                            if (row[ball - 1] == -1)
+                            {
+                                balls[columnIndex] = ball - 1;
+                            }
+                            else
+                            {
+                                balls[columnIndex] = -1;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return balls;
+        }
+    }
+}
---
+++ src/LeetCode1706/TestCases.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1706
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var grid = new[]
+            {
+                new[]{ 1, 1, 1,-1,-1},
+                new[]{ 1, 1, 1,-1,-1},
+                new[]{-1,-1,-1, 1, 1},
+                new[]{ 1, 1, 1, 1,-1},
+                new[]{-1,-1,-1,-1,-1}
+            };
+            var result = solution.FindBall(grid);
+            result.Should().Equal(1, -1, -1, -1, -1);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var grid = new[]
+            {
+                new[]{-1}
+            };
+            var result = solution.FindBall(grid);
+            result.Should().Equal(-1);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var solution = new Solution();
+            var grid = new[]
+            {
+                new[]{ 1, 1, 1, 1, 1, 1},
+                new[]{-1,-1,-1,-1,-1,-1},
+                new[]{ 1, 1, 1, 1, 1, 1},
+                new[]{-1,-1,-1,-1,-1,-1}
+            };
+            var result = solution.FindBall(grid);
+            result.Should().Equal(0, 1, 2, 3, 4, -1);
+        }
+    }
+}
