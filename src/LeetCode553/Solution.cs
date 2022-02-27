e91de1cbe9f32a634e1cf026ba886eb87292a141
h82258652 <842053625@qq.com>
2022-02-27 14:33:32 +0800

    submit 553 solution

---
+++ src/LeetCode553/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace LeetCode553
+{
+    public class Solution
+    {
+        public string OptimalDivision(int[] nums)
+        {
+            var length = nums.Length;
+            if (length == 1)
+            {
+                return nums[0].ToString();
+            }
+
+            if (length == 2)
+            {
+                return nums[0] + "/" + nums[1];
+            }
+
+            var builder = new StringBuilder();
+            builder.Append(nums[0]);
+            builder.Append("/(");
+            for (var i = 1; i < length; i++)
+            {
+                builder.Append(nums[i]);
+                if (i != length - 1)
+                {
+                    builder.Append('/');
+                }
+            }
+
+            builder.Append(')');
+            return builder.ToString();
+        }
+    }
+}
---
+++ src/LeetCode553/TestCases.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode553
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var result = solution.OptimalDivision(new[] { 1000, 100, 10, 2 });
+            result.Should().Be("1000/(100/10/2)");
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var result = solution.OptimalDivision(new[] { 2, 3, 4 });
+            result.Should().Be("2/(3/4)");
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var solution = new Solution();
+            var result = solution.OptimalDivision(new[] { 2 });
+            result.Should().Be("2");
+        }
+    }
+}
