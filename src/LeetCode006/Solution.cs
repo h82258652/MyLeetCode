a3a5be90d131e810c4a3082b06f269c602b9b7c9
h82258652 <842053625@qq.com>
2022-03-01 08:25:45 +0800

    submit 006 solution

---
+++ src/LeetCode006/Solution.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode006
+{
+    public class Solution
+    {
+        public enum Direction
+        {
+            Down,
+            Up
+        }
+
+        public string Convert(string s, int numRows)
+        {
+            if (numRows == 1)
+            {
+                return s;
+            }
+
+            var rows = new List<char>[numRows];
+            for (var i = 0; i < numRows; i++)
+            {
+                rows[i] = new List<char>();
+            }
+
+            var direction = Direction.Down;
+            var length = s.Length;
+            var row = 0;
+            for (var i = 0; i < length; i++)
+            {
+                var c = s[i];
+                rows[row].Add(c);
+
+                if (direction == Direction.Down)
+                {
+                    if (row == numRows - 1)
+                    {
+                        row--;
+                        direction = Direction.Up;
+                    }
+                    else
+                    {
+                        row++;
+                    }
+                }
+                else
+                {
+                    if (row == 0)
+                    {
+                        row++;
+                        direction = Direction.Down;
+                    }
+                    else
+                    {
+                        row--;
+                    }
+                }
+            }
+
+            var result = new StringBuilder();
+            foreach (var chars in rows)
+            {
+                foreach (var c in chars)
+                {
+                    result.Append(c);
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ src/LeetCode006/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode006
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("PAYPALISHIRING", 3, "PAHNAPLSIIGYIR")]
+        [InlineData("PAYPALISHIRING", 4, "PINALSIGYAHRPI")]
+        [InlineData("A", 1, "A")]
+        [InlineData("AB", 1, "AB")]
+        public void Test(string s, int numRows, string excepted)
+        {
+            var solution = new Solution();
+            var result = solution.Convert(s, numRows);
+            result.Should().Be(excepted);
+        }
+    }
+}
