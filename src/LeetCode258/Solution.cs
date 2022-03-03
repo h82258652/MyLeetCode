b4f21eec43faa425cf977501383a635ecbad2427
h82258652 <842053625@qq.com>
2022-03-03 10:17:44 +0800

    submit 258 solution

---
+++ src/LeetCode258/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode258
+{
+    public class Solution
+    {
+        public int AddDigits(int num)
+        {
+            if (num < 10)
+            {
+                return num;
+            }
+
+            var r = 0;
+            while (num > 0)
+            {
+                r += num % 10;
+                num /= 10;
+            }
+
+            return AddDigits(r);
+        }
+    }
+}
---
+++ src/LeetCode258/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode258
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(38, 2)]
+        [InlineData(0, 0)]
+        public void Test(int num, int expected)
+        {
+            var solution = new Solution();
+            var result = solution.AddDigits(num);
+            result.Should().Be(expected);
+        }
+    }
+}
