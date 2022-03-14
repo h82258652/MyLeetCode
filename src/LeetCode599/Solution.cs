705299812c8865dfd390bdedb0a3225571a3abd3
h82258652 <842053625@qq.com>
2022-03-14 09:01:40 +0800

    submit 599 solution

---
+++ src/LeetCode599/Solution.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode599
+{
+    public class Solution
+    {
+        public string[] FindRestaurant(string[] list1, string[] list2)
+        {
+            var temp = new HashSet<string>();
+            var both = new List<string>();
+            var index1 = new Dictionary<string, int>();
+            var index2 = new Dictionary<string, int>();
+            for (var i = 0; i < list1.Length; i++)
+            {
+                var s = list1[i];
+                temp.Add(s);
+                index1[s] = i;
+            }
+
+            for (var i = 0; i < list2.Length; i++)
+            {
+                var s = list2[i];
+                if (temp.Contains(s))
+                {
+                    both.Add(s);
+                    index2[s] = i;
+                }
+            }
+
+            var result = new List<string>();
+            var minIndex = int.MaxValue;
+            foreach (var s in both)
+            {
+                var i = index1[s] + index2[s];
+
+                if (i == minIndex)
+                {
+                    result.Add(s);
+                }
+                else if (i < minIndex)
+                {
+                    result.Clear();
+                    result.Add(s);
+                    minIndex = i;
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ src/LeetCode599/TestCases.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode599
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var list1 = new[] { "Shogun", "Tapioca Express", "Burger King", "KFC" };
+            var list2 = new[] { "Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun" };
+            var result = solution.FindRestaurant(list1, list2);
+            result.Should().Equal("Shogun");
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var list1 = new[] { "Shogun", "Tapioca Express", "Burger King", "KFC" };
+            var list2 = new[] { "KFC", "Shogun", "Burger King" };
+            var result = solution.FindRestaurant(list1, list2);
+            result.Should().Equal("Shogun");
+        }
+    }
+}
