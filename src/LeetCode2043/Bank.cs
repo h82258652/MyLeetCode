27ff2967485021fcddfca051c5705d5fd76cdd0a
h82258652 <842053625@qq.com>
2022-03-18 09:25:08 +0800

    submit 2043 solution

---
+++ src/LeetCode2043/Bank.cs
@@ -0,0 +1,59 @@
+﻿namespace LeetCode2043
+{
+    public class Bank
+    {
+        private readonly long[] _balance;
+
+        public Bank(long[] balance)
+        {
+            _balance = balance;
+        }
+
+        public bool Transfer(int account1, int account2, long money)
+        {
+            if (account1 <= 0 || account1 > _balance.Length ||
+                account2 <= 0 || account2 > _balance.Length)
+            {
+                return false;
+            }
+
+            var account1CurrentMoney = _balance[account1 - 1];
+            if (account1CurrentMoney < money)
+            {
+                return false;
+            }
+
+            _balance[account1 - 1] = account1CurrentMoney - money;
+            _balance[account2 - 1] += money;
+            return true;
+        }
+
+        public bool Deposit(int account, long money)
+        {
+            if (account <= 0 || account > _balance.Length)
+            {
+                return false;
+            }
+
+            _balance[account - 1] += money;
+            return true;
+        }
+
+        public bool Withdraw(int account, long money)
+        {
+            if (account <= 0 || account > _balance.Length)
+            {
+                return false;
+            }
+
+            var currentMoney = _balance[account - 1];
+            if (currentMoney < money)
+            {
+                return false;
+            }
+
+            _balance[account - 1] = currentMoney - money;
+            return true;
+        }
+    }
+}
---
+++ src/LeetCode2043/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2043
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var bank = new Bank(new long[] { 10, 100, 20, 50, 30 });
+            bank.Withdraw(3, 10).Should().BeTrue();
+            bank.Transfer(5, 1, 20).Should().BeTrue();
+            bank.Deposit(5, 20).Should().BeTrue();
+            bank.Transfer(3, 4, 15).Should().BeFalse();
+            bank.Withdraw(10, 50).Should().BeFalse();
+        }
+    }
+}
