80f95c36a25ae436997a57f0db77fc31fdcc661e
h82258652 <842053625@qq.com>
2022-03-19 10:51:32 +0800

    submit 606 solution

---
+++ src/LeetCode606/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace LeetCode606
+{
+    public class Solution
+    {
+        public string Tree2str(TreeNode root)
+        {
+            var result = new StringBuilder();
+            InternalTree2str(root, result);
+            return result.ToString();
+        }
+
+        private void InternalTree2str(TreeNode node, StringBuilder builder)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            builder.Append(node.val);
+            if (node.left == null && node.right == null)
+            {
+                return;
+            }
+
+            if (node.left != null && node.right == null)
+            {
+                builder.Append('(');
+                InternalTree2str(node.left, builder);
+                builder.Append(')');
+                return;
+            }
+
+            builder.Append('(');
+            InternalTree2str(node.left, builder);
+            builder.Append(")(");
+            InternalTree2str(node.right, builder);
+            builder.Append(')');
+        }
+    }
+}
---
+++ src/LeetCode606/TestCases.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode606
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var root = new TreeNode(1)
+            {
+                left = new TreeNode(2)
+                {
+                    left = new TreeNode(4)
+                },
+                right = new TreeNode(3)
+            };
+            var result = solution.Tree2str(root);
+            result.Should().Be("1(2(4))(3)");
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var root = new TreeNode(1)
+            {
+                left = new TreeNode(2)
+                {
+                    right = new TreeNode(4)
+                },
+                right = new TreeNode(3)
+            };
+            var result = solution.Tree2str(root);
+            result.Should().Be("1(2()(4))(3)");
+        }
+    }
+}
