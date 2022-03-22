243992cdae2a52a8f9f97c88796cf418557dd299
h82258652 <842053625@qq.com>
2022-03-22 08:25:48 +0800

    submit 2038 solution

---
+++ src/LeetCode2038/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode2038
+{
+    public class Solution
+    {
+        public bool WinnerOfGame(string colors)
+        {
+            var length = colors.Length;
+            if (length < 3)
+            {
+                return false;
+            }
+
+            var aCount = 0;
+            var bCount = 0;
+            for (var i = 2; i < length; i++)
+            {
+                var c1 = colors[i - 2];
+                var c2 = colors[i - 1];
+                var c3 = colors[i];
+                if (c1 == 'A' && c2 == 'A' && c3 == 'A')
+                {
+                    aCount++;
+                }
+                else if (c1 == 'B' && c2 == 'B' && c3 == 'B')
+                {
+                    bCount++;
+                }
+            }
+
+            return aCount > bCount;
+        }
+    }
+}
---
+++ src/LeetCode2038/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2038
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("AAABABB", true)]
+        [InlineData("AA", false)]
+        [InlineData("ABBBBBBBAAA", false)]
+        public void Test(string colors, bool expected)
+        {
+            var solution = new Solution();
+            var result = solution.WinnerOfGame(colors);
+            result.Should().Be(expected);
+        }
+    }
+}
