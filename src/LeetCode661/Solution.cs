38d26a90ad6a6ed46d5607009e192a992f328fa0
h82258652 <842053625@qq.com>
2022-03-24 09:08:08 +0800

    submit 661 solution

---
+++ src/LeetCode661/Solution.cs
@@ -0,0 +1,82 @@
+﻿namespace LeetCode661
+{
+    public class Solution
+    {
+        public int[][] ImageSmoother(int[][] img)
+        {
+            var m = img.Length;
+            var n = img[0].Length;
+
+            var result = new int[m][];
+            for (var i = 0; i < m; i++)
+            {
+                result[i] = new int[n];
+            }
+
+            for (var i = 0; i < m; i++)
+            {
+                for (var j = 0; j < n; j++)
+                {
+                    result[i][j] = Calc(img, i, j, m, n);
+                }
+            }
+
+            return result;
+        }
+
+        private int Calc(int[][] img, int i, int j, int m, int n)
+        {
+            var sum = img[i][j];
+            var count = 1;
+            if (i - 1 >= 0 && j - 1 >= 0)
+            {
+                sum += img[i - 1][j - 1];
+                count++;
+            }
+
+            if (j - 1 >= 0)
+            {
+                sum += img[i][j - 1];
+                count++;
+            }
+
+            if (i + 1 < m && j - 1 >= 0)
+            {
+                sum += img[i + 1][j - 1];
+                count++;
+            }
+
+            if (i - 1 >= 0)
+            {
+                sum += img[i - 1][j];
+                count++;
+            }
+
+            if (i + 1 < m)
+            {
+                sum += img[i + 1][j];
+                count++;
+            }
+
+            if (i - 1 >= 0 && j + 1 < n)
+            {
+                sum += img[i - 1][j + 1];
+                count++;
+            }
+
+            if (j + 1 < n)
+            {
+                sum += img[i][j + 1];
+                count++;
+            }
+
+            if (i + 1 < m && j + 1 < n)
+            {
+                sum += img[i + 1][j + 1];
+                count++;
+            }
+
+            return sum / count;
+        }
+    }
+}
---
+++ src/LeetCode661/TestCases.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode661
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var img = new[]
+            {
+                new[] { 1, 1, 1 },
+                new[] { 1, 0, 1 },
+                new[] { 1, 1, 1 }
+            };
+            var result = solution.ImageSmoother(img);
+            result.Should().BeEquivalentTo(new[]
+            {
+                new[] { 0, 0, 0 },
+                new[] { 0, 0, 0 },
+                new[] { 0, 0, 0 }
+            }, options => options.WithStrictOrdering());
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var img = new[]
+            {
+                new[] { 100, 200, 100 },
+                new[] { 200, 50, 200 },
+                new[] { 100, 200, 100 }
+            };
+            var result = solution.ImageSmoother(img);
+            result.Should().BeEquivalentTo(new[]
+            {
+                new[] { 137, 141, 137 },
+                new[] { 141, 138, 141 },
+                new[] { 137, 141, 137 }
+            }, options => options.WithStrictOrdering());
+        }
+    }
+}
