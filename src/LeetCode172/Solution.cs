9a6b5b96870b2866b7e609bf4033414398f7e606
h82258652 <842053625@qq.com>
2022-03-25 11:10:45 +0800

    submit 172 solution

---
+++ src/LeetCode172/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode172
+{
+    public class Solution
+    {
+        public int TrailingZeroes(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+
+            var count = 0;
+            for (var i = 5; i <= n; i += 5)
+            {
+                var temp = i;
+                while (temp > 0)
+                {
+                    if (temp % 5 == 0)
+                    {
+                        count++;
+                        temp /= 5;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ src/LeetCode172/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode172
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(3, 0)]
+        [InlineData(5, 1)]
+        [InlineData(0, 0)]
+        public void Test(int n, int expected)
+        {
+            var solution = new Solution();
+            var result = solution.TrailingZeroes(n);
+            result.Should().Be(expected);
+        }
+    }
+}
