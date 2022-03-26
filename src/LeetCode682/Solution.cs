b85893c6ddbb9567db31be517f91fc79b0ca6ba3
h82258652 <842053625@qq.com>
2022-03-26 10:44:35 +0800

    submit 682 solution

---
+++ src/LeetCode682/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode682
+{
+    public class Solution
+    {
+        public int CalPoints(string[] ops)
+        {
+            var points = new List<int>();
+            foreach (var op in ops)
+            {
+                switch (op)
+                {
+                    case "+":
+                        points.Add(points[points.Count - 1] + points[points.Count - 2]);
+                        break;
+                    case "D":
+                        points.Add(points[points.Count - 1] * 2);
+                        break;
+                    case "C":
+                        points.RemoveAt(points.Count - 1);
+                        break;
+                    default:
+                        points.Add(int.Parse(op));
+                        break;
+                }
+            }
+
+            return points.Sum();
+        }
+    }
+}
---
+++ src/LeetCode682/TestCases.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode682
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var result = solution.CalPoints(new[] { "5", "2", "C", "D", "+" });
+            result.Should().Be(30);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var result = solution.CalPoints(new[] { "5", "-2", "4", "C", "D", "9", "+", "+" });
+            result.Should().Be(27);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var solution = new Solution();
+            var result = solution.CalPoints(new[] { "1" });
+            result.Should().Be(1);
+        }
+    }
+}
