f32bea58864d5bae9d35b3d7cec5c0819d074bb6
h82258652 <842053625@qq.com>
2022-03-27 16:06:53 +0800

    submit 2028 solution

---
+++ src/LeetCode2028/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode2028
+{
+    public class Solution
+    {
+        public int[] MissingRolls(int[] rolls, int mean, int n)
+        {
+            var m = rolls.Length;
+            var totalLength = m + n;
+            var total = mean * totalLength;
+            var sum = rolls.Sum();
+            var remain = total - sum;
+            if (remain * 1d / n > 6d || remain * 1d / n < 1d)
+            {
+                return Array.Empty<int>();
+            }
+            var result = new int[n];
+            var avg = remain / n;
+            var mod = remain % n;
+            for (var i = 0; i < n; i++)
+            {
+                var temp = avg;
+                if (mod > 0)
+                {
+                    temp++;
+                    mod--;
+                }
+
+                result[i] = temp;
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ src/LeetCode2028/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2028
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 3, 2, 4, 3 }, 4, 2, new[] { 6, 6 })]
+        [InlineData(new[] { 1, 5, 6 }, 3, 4, new[] { 2, 3, 2, 2 })]
+        [InlineData(new[] { 1, 2, 3, 4 }, 6, 4, new int[] { })]
+        [InlineData(new[] { 1 }, 3, 1, new[] { 5 })]
+        public void Test(int[] rolls, int mean, int n, int[] expectation)
+        {
+            var solution = new Solution();
+            var result = solution.MissingRolls(rolls, mean, n);
+            result.Should().BeEquivalentTo(expectation, options => options.WithoutStrictOrdering());
+        }
+    }
+}
