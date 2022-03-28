0969506915961a50c4db53281f4ecbd662f06db3
h82258652 <842053625@qq.com>
2022-03-28 17:26:26 +0800

    submit 1827 solution

---
+++ src/LeetCode1827/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode1827
+{
+    public class Solution
+    {
+        public int MinOperations(int[] nums)
+        {
+            var result = 0;
+            for (var i = 1; i < nums.Length; i++)
+            {
+                var previous = nums[i - 1];
+                var current = nums[i];
+                if (current > previous)
+                {
+                    continue;
+                }
+
+                result += previous + 1 - current;
+
+                nums[i] = previous + 1;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode1827/TestCases.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1827
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var nums = new[] { 1, 1, 1 };
+            var result = solution.MinOperations(nums);
+            result.Should().Be(3);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var nums = new[] { 1, 5, 2, 4, 1 };
+            var result = solution.MinOperations(nums);
+            result.Should().Be(14);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var solution = new Solution();
+            var nums = new[] { 8 };
+            var result = solution.MinOperations(nums);
+            result.Should().Be(0);
+        }
+    }
+}
