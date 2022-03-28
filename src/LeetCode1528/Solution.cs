65d678d7b13a9d3ef949c903b9e528a6423f8ae6
h82258652 <842053625@qq.com>
2022-03-28 17:45:23 +0800

    submit 1528 solution

---
+++ src/LeetCode1528/Solution.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode1528
+{
+    public class Solution
+    {
+        public string RestoreString(string s, int[] indices)
+        {
+            var chars = new char[indices.Length];
+            for (var i = 0; i < s.Length; i++)
+            {
+                chars[indices[i]] = s[i];
+            }
+
+            return new string(chars);
+        }
+    }
+}
---
+++ src/LeetCode1528/TestCases.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1528
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var result = solution.RestoreString("codeleet", new[] { 4, 5, 6, 7, 0, 2, 1, 3 });
+            result.Should().Be("leetcode");
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var result = solution.RestoreString("abc", new[] { 0, 1, 2 });
+            result.Should().Be("abc");
+        }
+    }
+}
