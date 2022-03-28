7d7a1b7f45a47fd35d2affe56a2c2fdcb441881d
h82258652 <842053625@qq.com>
2022-03-28 10:20:45 +0800

    submit 965 solution

---
+++ src/LeetCode965/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode965
+{
+    public class Solution
+    {
+        public bool IsUnivalTree(TreeNode root)
+        {
+            int val = root.val;
+            return PreOrder(root.left, val) && PreOrder(root.right, val);
+        }
+
+        private bool PreOrder(TreeNode node, int val)
+        {
+            if (node == null)
+            {
+                return true;
+            }
+
+            if (node.val != val)
+            {
+                return false;
+            }
+
+            return PreOrder(node.left, val) && PreOrder(node.right, val);
+        }
+    }
+}
---
+++ src/LeetCode965/TestCases.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode965
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var root = new TreeNode(1, new TreeNode(1, new TreeNode(1), new TreeNode(1)), new TreeNode(1, right: new TreeNode(1)));
+            var result = solution.IsUnivalTree(root);
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var root = new TreeNode(2, new TreeNode(2, new TreeNode(5), new TreeNode(2)), new TreeNode(2));
+            var result = solution.IsUnivalTree(root);
+            result.Should().BeFalse();
+        }
+    }
+}
