aac49840e6448a151eee3780fb4cb1182ac70fe1
h82258652 <842053625@qq.com>
2022-03-28 09:35:37 +0800

    submit 643 solution

---
+++ src/LeetCode643/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LeetCode643
+{
+    public class Solution
+    {
+        public double FindMaxAverage(int[] nums, int k)
+        {
+            var sum = 0d;
+            var i = 0;
+            for (; i < k; i++)
+            {
+                sum += nums[i];
+            }
+
+            var avg = sum / k;
+
+            for (; i < nums.Length; i++)
+            {
+                sum -= nums[i - k];
+                sum += nums[i];
+                avg = Math.Max(avg, sum / k);
+            }
+
+            return avg;
+        }
+    }
+}
---
+++ src/LeetCode643/TestCases.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode643
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var nums = new[] { 1, 12, -5, -6, 50, 3 };
+            var k = 4;
+            var result = solution.FindMaxAverage(nums, k);
+            result.Should().Be(12.75);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var nums = new[] { 5 };
+            var k = 1;
+            var result = solution.FindMaxAverage(nums, k);
+            result.Should().Be(5);
+        }
+    }
+}
