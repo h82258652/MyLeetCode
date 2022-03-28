f2d3f7d56c3db8048661a0f2c65d5a2450226066
h82258652 <842053625@qq.com>
2022-03-28 12:46:57 +0800

    submit 1022 solution

---
+++ src/LeetCode1022/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode1022
+{
+    public class Solution
+    {
+        public int SumRootToLeaf(TreeNode root)
+        {
+            var sum = 0;
+            PreOrder(root, 0, ref sum);
+            return sum;
+        }
+
+        private void PreOrder(TreeNode node, int current, ref int sum)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            if (node.left == null && node.right == null)
+            {
+                sum += (current << 1) + node.val;
+            }
+            else
+            {
+                var pathValue = (current << 1) + node.val;
+                PreOrder(node.left, pathValue, ref sum);
+                PreOrder(node.right, pathValue, ref sum);
+            }
+        }
+    }
+}
---
+++ src/LeetCode1022/TestCases.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1022
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var root = new TreeNode(1, new TreeNode(0, new TreeNode(0), new TreeNode(1)), new TreeNode(1, new TreeNode(0), new TreeNode(1)));
+            var result = solution.SumRootToLeaf(root);
+            result.Should().Be(22);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var root = new TreeNode(0);
+            var result = solution.SumRootToLeaf(root);
+            result.Should().Be(0);
+        }
+    }
+}
