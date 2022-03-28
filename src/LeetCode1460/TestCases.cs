c362de951cc759eeb0159aa081f28d57d83484eb
h82258652 <842053625@qq.com>
2022-03-28 17:51:04 +0800

    submit 1460 solution

---
+++ src/LeetCode1460/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1460
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 4 }, new[] { 2, 4, 1, 3 }, true)]
+        [InlineData(new[] { 7 }, new[] { 7 }, true)]
+        [InlineData(new[] { 3, 7, 9 }, new[] { 3, 7, 11 }, false)]
+        public void Test(int[] target, int[] arr, bool expected)
+        {
+            var solution = new Solution();
+            var result = solution.CanBeEqual(target, arr);
+            result.Should().Be(expected);
+        }
+    }
+}
