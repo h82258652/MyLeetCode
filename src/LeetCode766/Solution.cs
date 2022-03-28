eef059b4934ba4a278ef4048c91cd1f8dc41f146
h82258652 <842053625@qq.com>
2022-03-28 10:13:46 +0800

    submit 766 solution

---
+++ src/LeetCode766/Solution.cs
@@ -0,0 +1,52 @@
+﻿namespace LeetCode766
+{
+    public class Solution
+    {
+        public bool IsToeplitzMatrix(int[][] matrix)
+        {
+            var m = matrix.Length;
+            var n = matrix[0].Length;
+
+            for (var i = 0; i < m; i++)
+            {
+                if (!IsMatch(matrix, i, 0, m, n))
+                {
+                    return false;
+                }
+            }
+
+            for (var j = 1; j < n; j++)
+            {
+                if (!IsMatch(matrix, 0, j, m, n))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool IsMatch(int[][] matrix, int i, int j, int m, int n)
+        {
+            var element = matrix[i][j];
+            while (true)
+            {
+                i++;
+                j++;
+                if (i >= m || j >= n)
+                {
+                    break;
+                }
+
+                if (matrix[i][j] == element)
+                {
+                    continue;
+                }
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/LeetCode766/TestCases.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode766
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var result = solution.IsToeplitzMatrix(new[]
+            {
+                new[] { 1, 2, 3, 4 },
+                new[] { 5, 1, 2, 3 },
+                new[] { 9, 5, 1, 2 }
+            });
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var result = solution.IsToeplitzMatrix(new[]
+            {
+                new[] { 1, 2 },
+                new[] { 2, 2 }
+            });
+            result.Should().BeFalse();
+        }
+    }
+}
