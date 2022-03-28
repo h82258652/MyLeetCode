e93edf715c693c3828fcbb1fdd54d030a3c2fe45
h82258652 <842053625@qq.com>
2022-03-28 09:53:28 +0800

    submit 680 solution

---
+++ src/LeetCode680/Solution.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode680
+{
+    public class Solution
+    {
+        public bool ValidPalindrome(string s)
+        {
+            var left = 0;
+            var right = s.Length - 1;
+            while (left < right)
+            {
+                if (s[left] == s[right])
+                {
+                    left++;
+                    right--;
+                    continue;
+                }
+
+                return IsPalindrome(s, left + 1, right) || IsPalindrome(s, left, right - 1);
+            }
+
+            return true;
+        }
+
+        private bool IsPalindrome(string s, int left, int right)
+        {
+            while (left < right)
+            {
+                if (s[left] == s[right])
+                {
+                    left++;
+                    right--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/LeetCode680/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode680
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("aba", true)]
+        [InlineData("abca", true)]
+        [InlineData("abc", false)]
+        public void Test(string s, bool expected)
+        {
+            var solution = new Solution();
+            var result = solution.ValidPalindrome(s);
+            result.Should().Be(expected);
+        }
+    }
+}
