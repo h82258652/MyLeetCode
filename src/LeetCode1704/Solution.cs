cf7f8cd0fc78313adfbab3096963a6bf94990cba
h82258652 <842053625@qq.com>
2022-03-28 13:43:47 +0800

    submit 1704 solution

---
+++ src/LeetCode1704/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode1704
+{
+    public class Solution
+    {
+        private static readonly HashSet<char> Vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+
+        public bool HalvesAreAlike(string s)
+        {
+            var count = 0;
+            var length = s.Length;
+            var halfLength = length >> 1;
+            for (var i = 0; i < halfLength; i++)
+            {
+                if (Vowels.Contains(s[i]))
+                {
+                    count++;
+                }
+            }
+
+            for (var i = halfLength; i < length; i++)
+            {
+                if (Vowels.Contains(s[i]))
+                {
+                    count--;
+                }
+            }
+
+            return count == 0;
+        }
+    }
+}
---
+++ src/LeetCode1704/TestCases.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1704
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var result = solution.HalvesAreAlike("book");
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var result = solution.HalvesAreAlike("textbook");
+            result.Should().BeFalse();
+        }
+    }
+}
