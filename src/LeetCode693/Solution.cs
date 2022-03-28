489ee0410acf8e14254170e41385269243b8ce75
h82258652 <842053625@qq.com>
2022-03-28 08:41:36 +0800

    submit 693 solution

---
+++ src/LeetCode693/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace LeetCode693
+{
+    public class Solution
+    {
+        public bool HasAlternatingBits(int n)
+        {
+            var twoBit = Cast(n);
+            for (var i = 0; i < twoBit.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    if (twoBit[i] == '0')
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (twoBit[i] == '1')
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        private string Cast(int n)
+        {
+            var builder = new StringBuilder();
+            while (n > 0)
+            {
+                builder.Insert(0, n % 2);
+                n /= 2;
+            }
+            return builder.ToString();
+        }
+    }
+}
---
+++ src/LeetCode693/TestCases.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode693
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(5, true)]
+        [InlineData(7, false)]
+        [InlineData(11, false)]
+        [InlineData(10, true)]
+        [InlineData(3, false)]
+        [InlineData(4, false)]
+        public void Test(int n, bool excepted)
+        {
+            Solution solution = new Solution();
+            var result = solution.HasAlternatingBits(n);
+            result.Should().Be(excepted);
+        }
+    }
+}
