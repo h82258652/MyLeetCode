c8c881481b3534b90f7a5d67a4f1ade7bcf29dea
h82258652 <842053625@qq.com>
2022-03-28 10:29:19 +0800

    submit 1037 solution

---
+++ src/LeetCode1037/Solution.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode1037
+{
+    public class Solution
+    {
+        public bool IsBoomerang(int[][] points)
+        {
+            var p1 = points[0];
+            var p2 = points[1];
+            var p3 = points[2];
+
+            return ((p3[1] - p2[1]) * (p2[0] - p1[0])) != ((p2[1] - p1[1]) * (p3[0] - p2[0]));
+        }
+    }
+}
---
+++ src/LeetCode1037/TestCases.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1037
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var result = solution.IsBoomerang(new[]
+            {
+                new[] { 1, 1 },
+                new[] { 2, 3 },
+                new[] { 3, 2 }
+            });
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var result = solution.IsBoomerang(new[]
+            {
+                new[] { 1, 1 },
+                new[] { 2, 2 },
+                new[] { 3, 3 }
+            });
+            result.Should().BeFalse();
+        }
+    }
+}
