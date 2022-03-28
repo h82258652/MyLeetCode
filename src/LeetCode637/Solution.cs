e9b2ae183732918163542ff0e9c0b833d0b5e7f2
h82258652 <842053625@qq.com>
2022-03-28 09:23:29 +0800

    submit 637 solution

---
+++ src/LeetCode637/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode637
+{
+    public class Solution
+    {
+        public IList<double> AverageOfLevels(TreeNode root)
+        {
+            var result = new List<double>();
+            var nodes = new Queue<TreeNode>();
+            nodes.Enqueue(root);
+            while (nodes.Count > 0)
+            {
+                var sum = 0d;
+                var count = nodes.Count;
+                for (var i = 0; i < count; i++)
+                {
+                    var node = nodes.Dequeue();
+                    sum += node.val;
+
+                    if (node.left != null)
+                    {
+                        nodes.Enqueue(node.left);
+                    }
+
+                    if (node.right != null)
+                    {
+                        nodes.Enqueue(node.right);
+                    }
+                }
+
+                result.Add(sum / count);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode637/TestCases.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode637
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var root = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+            var result = solution.AverageOfLevels(root);
+            result.Should().Equal(3d, 14.5, 11d);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var root = new TreeNode(3, new TreeNode(9, new TreeNode(15), new TreeNode(7)), new TreeNode(20));
+            var result = solution.AverageOfLevels(root);
+            result.Should().Equal(3d, 14.5, 11d);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var solution = new Solution();
+            var root = new TreeNode(2147483647, new TreeNode(2147483647), new TreeNode(2147483647));
+            var result = solution.AverageOfLevels(root);
+            result.Should().Equal(2147483647.0, 2147483647.0);
+        }
+    }
+}
