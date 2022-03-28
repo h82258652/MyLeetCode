256e1f75a13a1b7cc215fa45c3e4a215df57ddda
h82258652 <842053625@qq.com>
2022-03-28 17:35:04 +0800

    submit 1299 solution

---
+++ src/LeetCode1299/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LeetCode1299
+{
+    public class Solution
+    {
+        public int[] ReplaceElements(int[] arr)
+        {
+            var max = 0;
+            for (var i = arr.Length - 1; i >= 0; i--)
+            {
+                if (i == arr.Length - 1)
+                {
+                    max = arr[i];
+                    arr[i] = -1;
+                }
+                else
+                {
+                    var current = arr[i];
+                    arr[i] = max;
+                    max = Math.Max(max, current);
+                }
+            }
+
+            return arr;
+        }
+    }
+}
---
+++ src/LeetCode1299/TestCases.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1299
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var arr = new[] { 17, 18, 5, 4, 6, 1 };
+            var result = solution.ReplaceElements(arr);
+            result.Should().Equal(18, 6, 6, 6, 1, -1);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var arr = new[] { 400 };
+            var result = solution.ReplaceElements(arr);
+            result.Should().Equal(-1);
+        }
+    }
+}
