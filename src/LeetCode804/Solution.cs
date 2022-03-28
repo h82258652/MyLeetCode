955559d27fdda0ebb9ba8b62de6d05f46120d77f
h82258652 <842053625@qq.com>
2022-03-28 15:04:42 +0800

    submit 804 solution

---
+++ src/LeetCode804/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode804
+{
+    public class Solution
+    {
+        private static readonly string[] MorseMaps = new[]
+        {
+            ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---",
+            ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."
+        };
+
+        public int UniqueMorseRepresentations(string[] words)
+        {
+            var set = new HashSet<string>();
+            foreach (var word in words)
+            {
+                set.Add(Parse(word));
+            }
+
+            return set.Count;
+        }
+
+        private string Parse(string word)
+        {
+            var builder = new StringBuilder();
+            foreach (var c in word)
+            {
+                builder.Append(MorseMaps[c - 'a']);
+            }
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ src/LeetCode804/TestCases.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode804
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var words = new[] { "gin", "zen", "gig", "msg" };
+            var result = solution.UniqueMorseRepresentations(words);
+            result.Should().Be(2);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var words = new[] { "a" };
+            var result = solution.UniqueMorseRepresentations(words);
+            result.Should().Be(1);
+        }
+    }
+}
