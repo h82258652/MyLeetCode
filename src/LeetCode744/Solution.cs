6cc08edd6be540442309a6481213df5e93b16675
h82258652 <842053625@qq.com>
2022-03-28 10:01:09 +0800

    submit 744 solution

---
+++ src/LeetCode744/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode744
+{
+    public class Solution
+    {
+        public char NextGreatestLetter(char[] letters, char target)
+        {
+            foreach (var c in letters)
+            {
+                if (c > target)
+                {
+                    return c;
+                }
+            }
+
+            return letters[0];
+        }
+    }
+}
---
+++ src/LeetCode744/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode744
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { 'c', 'f', 'j' }, 'a', 'c')]
+        [InlineData(new[] { 'c', 'f', 'j' }, 'c', 'f')]
+        [InlineData(new[] { 'c', 'f', 'j' }, 'd', 'f')]
+        public void Test(char[] letters, char target, char expected)
+        {
+            var solution = new Solution();
+            var result = solution.NextGreatestLetter(letters, target);
+            result.Should().Be(expected);
+        }
+    }
+}
