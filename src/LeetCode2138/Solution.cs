216f9c6fbb8d7d6c44d5489f59b2e80e02358200
h82258652 <842053625@qq.com>
2022-03-30 17:36:09 +0800

    submit 2138 solution

---
+++ src/LeetCode2138/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode2138
+{
+    public class Solution
+    {
+        public string[] DivideString(string s, int k, char fill)
+        {
+            var chars = s.ToCharArray();
+            var result = new List<string>();
+            for (var i = 0; i < chars.Length; i += k)
+            {
+                var temp = new List<char>(chars.Skip(i).Take(k));
+                while (temp.Count < k)
+                {
+                    temp.Add(fill);
+                }
+                result.Add(new string(temp.ToArray()));
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ src/LeetCode2138/TestCases.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2138
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var result = solution.DivideString("abcdefghi", 3, 'x');
+            result.Should().Equal("abc", "def", "ghi");
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var result = solution.DivideString("abcdefghij", 3, 'x');
+            result.Should().Equal("abc", "def", "ghi", "jxx");
+        }
+    }
+}
