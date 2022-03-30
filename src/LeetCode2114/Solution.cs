e4fde539643e476fc422ed1951ed54f731e5ce51
h82258652 <842053625@qq.com>
2022-03-30 15:09:38 +0800

    submit 2114 solution

---
+++ src/LeetCode2114/Solution.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace LeetCode2114
+{
+    public class Solution
+    {
+        public int MostWordsFound(string[] sentences)
+        {
+            return sentences.Select(temp => temp.Count(c => c == ' ')).Max() + 1;
+        }
+    }
+}
---
+++ src/LeetCode2114/TestCases.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2114
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var result = solution.MostWordsFound(new[]
+            {
+                "alice and bob love leetcode",
+                "i think so too",
+                "this is great thanks very much"
+            });
+            result.Should().Be(6);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var result = solution.MostWordsFound(new[]
+            {
+                "please wait",
+                "continue to fight",
+                "continue to win"
+            });
+            result.Should().Be(3);
+        }
+    }
+}
