e166bed1585de5b05ba7ab84bced8ce3175df946
h82258652 <842053625@qq.com>
2022-03-30 16:53:57 +0800

    submit 1812 solution

---
+++ src/LeetCode1812/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode1812
+{
+    public class Solution
+    {
+        public bool SquareIsWhite(string coordinates)
+        {
+            return ((coordinates[0] - 'a') + (coordinates[1] - '1')) % 2 == 1;
+        }
+    }
+}
---
+++ src/LeetCode1812/TestCases.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1812
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var result = solution.SquareIsWhite("a1");
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var result = solution.SquareIsWhite("h3");
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var solution = new Solution();
+            var result = solution.SquareIsWhite("c7");
+            result.Should().BeFalse();
+        }
+    }
+}
