4e5ae690d490bf92c33467a27b21ad902d3305a4
h82258652 <842053625@qq.com>
2022-03-30 15:53:09 +0800

    submit 1678 solution

---
+++ src/LeetCode1678/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace LeetCode1678
+{
+    public class Solution
+    {
+        public string Interpret(string command)
+        {
+            var builder = new StringBuilder();
+
+            for (var i = 0; i < command.Length; i++)
+            {
+                var c = command[i];
+                if (c == 'G')
+                {
+                    builder.Append('G');
+                }
+                else
+                {
+                    if (command[i + 1] == ')')
+                    {
+                        builder.Append('o');
+                        i++;
+                    }
+                    else
+                    {
+                        builder.Append("al");
+                        i += 3;
+                    }
+                }
+            }
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ src/LeetCode1678/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1678
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("G()(al)", "Goal")]
+        [InlineData("G()()()()(al)", "Gooooal")]
+        [InlineData("(al)G(al)()()G", "alGalooG")]
+        public void Test(string command, string expected)
+        {
+            var solution = new Solution();
+            var result = solution.Interpret(command);
+            result.Should().Be(expected);
+        }
+    }
+}
