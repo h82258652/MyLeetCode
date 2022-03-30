eae233f088a858db8639442b6e05f0e6ec45cb2e
h82258652 <842053625@qq.com>
2022-03-30 15:21:10 +0800

    submit 2160 solution

---
+++ src/LeetCode2160/Solution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LeetCode2160
+{
+    public class Solution
+    {
+        public int MinimumSum(int num)
+        {
+            var ge = num % 10;
+            var shi = (num / 10) % 10;
+            var bai = (num / 10 / 10) % 10;
+            var qian = (num / 10 / 10 / 10) % 10;
+            var bits = new[] { ge, shi, bai, qian };
+            Array.Sort(bits);
+
+            return bits[0] * 10 + bits[1] * 10 + bits[2] + bits[3];
+        }
+    }
+}
---
+++ src/LeetCode2160/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2160
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(2932, 52)]
+        [InlineData(4009, 13)]
+        public void Test(int num, int excepted)
+        {
+            var solution = new Solution();
+            var result = solution.MinimumSum(num);
+            result.Should().Be(excepted);
+        }
+    }
+}
