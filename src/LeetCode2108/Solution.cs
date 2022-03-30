10665c5a2b94dc684929f243c70693cab3b6b43a
h82258652 <842053625@qq.com>
2022-03-30 16:27:24 +0800

    submit 2108 solution

---
+++ src/LeetCode2108/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode2108
+{
+    public class Solution
+    {
+        public string FirstPalindrome(string[] words)
+        {
+            foreach (var word in words)
+            {
+                if (IsPalindrome(word))
+                {
+                    return word;
+                }
+            }
+
+            return string.Empty;
+        }
+
+        private bool IsPalindrome(string word)
+        {
+            for (int i = 0, j = word.Length - 1; i < j; i++, j--)
+            {
+                if (word[i] != word[j])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/LeetCode2108/TestCases.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2108
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var words = new[]
+            {
+                "abc",
+                "car",
+                "ada",
+                "racecar",
+                "cool"
+            };
+            var result = solution.FirstPalindrome(words);
+            result.Should().Be("ada");
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var words = new[]
+            {
+                "notapalindrome",
+                "racecar"
+            };
+            var result = solution.FirstPalindrome(words);
+            result.Should().Be("racecar");
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var solution = new Solution();
+            var words = new[]
+            {
+                "def",
+                "ghi"
+            };
+            var result = solution.FirstPalindrome(words);
+            result.Should().Be("");
+        }
+    }
+}
