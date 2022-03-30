c0a3ab80d66c90021b1450836d3e764f361885e3
h82258652 <842053625@qq.com>
2022-03-30 16:42:05 +0800

    submit 2185 solution

---
+++ src/LeetCode2185/Solution.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace LeetCode2185
+{
+    public class Solution
+    {
+        public int PrefixCount(string[] words, string pref)
+        {
+            return words.Count(temp => temp.StartsWith(pref));
+        }
+    }
+}
---
+++ src/LeetCode2185/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2185
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(new[] { "pay", "attention", "practice", "attend" }, "at", 2)]
+        [InlineData(new[] { "leetcode", "win", "loops", "success" }, "code", 0)]
+        public void Test(string[] words, string pref, int expected)
+        {
+            var solution = new Solution();
+            var result = solution.PrefixCount(words, pref);
+            result.Should().Be(expected);
+        }
+    }
+}
