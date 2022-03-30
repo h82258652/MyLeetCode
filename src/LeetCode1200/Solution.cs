22d50e0a26af2c1da55234385867c1a041c4d3df
h82258652 <842053625@qq.com>
2022-03-30 17:07:45 +0800

    submit 1200 solution

---
+++ src/LeetCode1200/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode1200
+{
+    public class Solution
+    {
+        public IList<IList<int>> MinimumAbsDifference(int[] arr)
+        {
+            Array.Sort(arr);
+
+            var div = int.MaxValue;
+            var length = arr.Length;
+            for (var i = 1; i < length; i++)
+            {
+                div = Math.Min(div, arr[i] - arr[i - 1]);
+            }
+
+            var result = new List<IList<int>>();
+
+            var set = new HashSet<int>(arr);
+            foreach (var i in arr)
+            {
+                if (set.Contains(i + div))
+                {
+                    result.Add(new List<int> { i, i + div });
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode1200/TestCases.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1200
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var arr = new[] { 4, 2, 1, 3 };
+            var result = solution.MinimumAbsDifference(arr);
+            result.Should().BeEquivalentTo(new List<IList<int>>
+            {
+                new List<int> { 1, 2 },
+                new List<int> { 2, 3 },
+                new List<int> { 3, 4 }
+            }, options => options.WithStrictOrdering());
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var arr = new[] { 1, 3, 6, 10, 15 };
+            var result = solution.MinimumAbsDifference(arr);
+            result.Should().BeEquivalentTo(new List<IList<int>>
+            {
+                new List<int> { 1, 3 }
+            }, options => options.WithStrictOrdering());
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var solution = new Solution();
+            var arr = new[] { 3, 8, -10, 23, 19, -4, -14, 27 };
+            var result = solution.MinimumAbsDifference(arr);
+            result.Should().BeEquivalentTo(new List<IList<int>>
+            {
+                new List<int> { -14, -10 },
+                new List<int> { 19, 23 },
+                new List<int> { 23, 27 }
+            }, options => options.WithStrictOrdering());
+        }
+    }
+}
