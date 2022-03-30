828530a754eb508e0d905b734ad4f9fce80c061c
h82258652 <842053625@qq.com>
2022-03-30 15:39:15 +0800

    submit 1725 solution

---
+++ src/LeetCode1725/Solution.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace LeetCode1725
+{
+    public class Solution
+    {
+        public int CountGoodRectangles(int[][] rectangles)
+        {
+            var size = rectangles.Select(rectangle => rectangle.Min()).ToArray();
+            var maxSize = size.Max();
+            return size.Count(temp => temp == maxSize);
+        }
+    }
+}
---
+++ src/LeetCode1725/TestCases.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1725
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var rectangles = new[]
+            {
+                new[] { 5, 8 },
+                new[] { 3, 9 },
+                new[] { 5, 12 },
+                new[] { 16, 5 }
+            };
+            var result = solution.CountGoodRectangles(rectangles);
+            result.Should().Be(3);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var rectangles = new[]
+            {
+                new[] { 2, 3 },
+                new[] { 3, 7 },
+                new[] { 4, 3 },
+                new[] { 3, 7 }
+            };
+            var result = solution.CountGoodRectangles(rectangles);
+            result.Should().Be(3);
+        }
+    }
+}
