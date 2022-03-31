1ab0d1453037970032ca9fb543d87ffb96411e27
h82258652 <842053625@qq.com>
2022-03-31 14:54:25 +0800

    submit 2139 solution

---
+++ src/LeetCode2139/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode2139
+{
+    public class Solution
+    {
+        public int MinMoves(int target, int maxDoubles)
+        {
+            var count = 0;
+            while (target > 1)
+            {
+                if (maxDoubles > 0)
+                {
+                    if ((target & 1) == 0)
+                    {
+                        target >>= 1;
+                        maxDoubles--;
+                    }
+                    else
+                    {
+                        target--;
+                    }
+
+                    count++;
+                }
+                else
+                {
+                    return target + count - 1;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ src/LeetCode2139/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2139
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(5, 0, 4)]
+        [InlineData(19, 2, 7)]
+        [InlineData(10, 4, 4)]
+        [InlineData(999999997, 0, 999999996)]
+        public void Test(int target, int maxDoubles, int excepted)
+        {
+            var solution = new Solution();
+            var result = solution.MinMoves(target, maxDoubles);
+            result.Should().Be(excepted);
+        }
+    }
+}
