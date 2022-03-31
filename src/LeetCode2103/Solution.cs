4249874d7fcd302c54a66af52777d2950e1540f3
h82258652 <842053625@qq.com>
2022-03-31 15:42:09 +0800

    submit 2103 solution

---
+++ src/LeetCode2103/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+
+namespace LeetCode2103
+{
+    public class Solution
+    {
+        public int CountPoints(string rings)
+        {
+            var array = new int[10];
+
+            for (var i = 0; i < rings.Length; i += 2)
+            {
+                var color = rings[i];
+                var position = rings[i + 1];
+
+                switch (color)
+                {
+                    case 'R':
+                        array[position - '0'] |= 1;
+                        break;
+
+                    case 'G':
+                        array[position - '0'] |= 2;
+                        break;
+
+                    case 'B':
+                        array[position - '0'] |= 4;
+                        break;
+                }
+            }
+
+            return array.Count(temp => temp == 7);
+        }
+    }
+}
---
+++ src/LeetCode2103/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2103
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("B0B6G0R6R0R6G9", 1)]
+        [InlineData("B0R0G0R9R0B0G0", 1)]
+        [InlineData("G4", 0)]
+        public void Test(string rings, int expected)
+        {
+            var solution = new Solution();
+            var result = solution.CountPoints(rings);
+            result.Should().Be(expected);
+        }
+    }
+}
