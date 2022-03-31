229c1e5acb629a587a609333ac55ead73335896f
h82258652 <842053625@qq.com>
2022-03-31 15:21:58 +0800

    submit 2124 solution

---
+++ src/LeetCode2124/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode2124
+{
+    public class Solution
+    {
+        public bool CheckString(string s)
+        {
+            var aIndex = s.LastIndexOf('a');
+            if (aIndex == -1)
+            {
+                return true;
+            }
+
+            var bIndex = s.IndexOf('b');
+            if (bIndex == -1)
+            {
+                return true;
+            }
+
+            return aIndex < bIndex;
+        }
+    }
+}
---
+++ src/LeetCode2124/TestCases.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2124
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var result = solution.CheckString("aaabbb");
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var result = solution.CheckString("abab");
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var solution = new Solution();
+            var result = solution.CheckString("bbb");
+            result.Should().BeTrue();
+        }
+    }
+}
