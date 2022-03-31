18f32bcbd5ffd6c6fbbe0d7ca4ffefdbc293b702
h82258652 <842053625@qq.com>
2022-03-31 09:55:57 +0800

    submit 2169 solution

---
+++ src/LeetCode2169/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode2169
+{
+    public class Solution
+    {
+        public int CountOperations(int num1, int num2)
+        {
+            var count = 0;
+
+            while (true)
+            {
+                if (num1 == 0 || num2 == 0)
+                {
+                    break;
+                }
+
+                if (num1 >= num2)
+                {
+                    num1 -= num2;
+                }
+                else
+                {
+                    num2 -= num1;
+                }
+
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ src/LeetCode2169/TestCases.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2169
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var result = solution.CountOperations(2, 3);
+            result.Should().Be(3);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var result = solution.CountOperations(10, 10);
+            result.Should().Be(1);
+        }
+    }
+}
