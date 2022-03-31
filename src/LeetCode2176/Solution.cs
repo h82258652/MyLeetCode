ea794b2a1a24abff31abf46b8d9f354d71b928b2
h82258652 <842053625@qq.com>
2022-03-31 09:38:19 +0800

    submit 2176 solution

---
+++ src/LeetCode2176/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode2176
+{
+    public class Solution
+    {
+        public int CountPairs(int[] nums, int k)
+        {
+            var count = 0;
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                for (var j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j] && (i * j) % k == 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ src/LeetCode2176/TestCases.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2176
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var nums = new[] { 3, 1, 2, 2, 2, 1, 3 };
+            var k = 2;
+            var result = solution.CountPairs(nums, k);
+            result.Should().Be(4);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var nums = new[] { 1, 2, 3, 4 };
+            var k = 1;
+            var result = solution.CountPairs(nums, k);
+            result.Should().Be(0);
+        }
+    }
+}
