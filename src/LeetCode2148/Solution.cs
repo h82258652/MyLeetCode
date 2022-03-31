972aed62c05862feda5ace9c2985a78951e6a810
h82258652 <842053625@qq.com>
2022-03-31 13:57:24 +0800

    submit 2148 solution

---
+++ src/LeetCode2148/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LeetCode2148
+{
+    public class Solution
+    {
+        public int CountElements(int[] nums)
+        {
+            Array.Sort(nums);
+            int left = 0;
+            int min = nums[left];
+            int right = nums.Length - 1;
+            int max = nums[right];
+            while (left < nums.Length && min == nums[left])
+            {
+                left++;
+            }
+
+            while (right >= 0 && max == nums[right])
+            {
+                right--;
+            }
+
+            return Math.Max(0, right - left + 1);
+        }
+    }
+}
---
+++ src/LeetCode2148/TestCases.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2148
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var nums = new[] { 11, 7, 2, 15 };
+            var result = solution.CountElements(nums);
+            result.Should().Be(2);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var nums = new[] { -3, 3, 3, 90 };
+            var result = solution.CountElements(nums);
+            result.Should().Be(2);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var solution = new Solution();
+            var nums = new[] { 0 };
+            var result = solution.CountElements(nums);
+            result.Should().Be(0);
+        }
+    }
+}
