3585c098d5efc41878e77cf16c5b3ca9074af212
h82258652 <842053625@qq.com>
2022-03-31 16:23:05 +0800

    submit 654 solution

---
+++ src/LeetCode654/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace LeetCode654
+{
+    public class Solution
+    {
+        public TreeNode ConstructMaximumBinaryTree(int[] nums)
+        {
+            return InternalConstructMaximumBinaryTree(nums);
+        }
+
+        private TreeNode InternalConstructMaximumBinaryTree(Span<int> nums)
+        {
+            var max = int.MinValue;
+            var maxIndex = -1;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > max)
+                {
+                    max = nums[i];
+                    maxIndex = i;
+                }
+            }
+
+            var root = new TreeNode(max);
+
+            if (maxIndex > 0)
+            {
+                var left = InternalConstructMaximumBinaryTree(nums[..maxIndex]);
+                root.left = left;
+            }
+
+            if (maxIndex < nums.Length - 1)
+            {
+                var right = InternalConstructMaximumBinaryTree(nums[(maxIndex + 1)..]);
+                root.right = right;
+            }
+
+            return root;
+        }
+    }
+}
---
+++ src/LeetCode654/TestCases.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode654
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var nums = new[] { 3, 2, 1, 6, 0, 5 };
+            var result = solution.ConstructMaximumBinaryTree(nums);
+            result.val.Should().Be(6);
+            result.left.val.Should().Be(3);
+            result.left.right.val.Should().Be(2);
+            result.left.right.right.val.Should().Be(1);
+            result.right.val.Should().Be(5);
+            result.right.left.val.Should().Be(0);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var nums = new[] { 3, 2, 1 };
+            var result = solution.ConstructMaximumBinaryTree(nums);
+            result.val.Should().Be(3);
+            result.right.val.Should().Be(2);
+            result.right.right.val.Should().Be(1);
+        }
+    }
+}
