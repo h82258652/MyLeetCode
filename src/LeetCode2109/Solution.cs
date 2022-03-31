9d1fe9d78c8d5e896a933bd5c74b8a0d60af0f54
h82258652 <842053625@qq.com>
2022-03-31 15:27:57 +0800

    submit 2109 solution

---
+++ src/LeetCode2109/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode2109
+{
+    public class Solution
+    {
+        public string AddSpaces(string s, int[] spaces)
+        {
+            var builder = new StringBuilder();
+            var sp = new HashSet<int>(spaces);
+            for (var i = 0; i < s.Length; i++)
+            {
+                if (sp.Contains(i))
+                {
+                    builder.Append(' ');
+                }
+
+                builder.Append(s[i]);
+            }
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ src/LeetCode2109/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2109
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("LeetcodeHelpsMeLearn", new int[] { 8, 13, 15 }, "Leetcode Helps Me Learn")]
+        [InlineData("icodeinpython", new int[] { 1, 5, 7, 9 }, "i code in py thon")]
+        [InlineData("spacing", new int[] { 0, 1, 2, 3, 4, 5, 6 }, " s p a c i n g")]
+        public void Test(string s, int[] spaces, string expected)
+        {
+            var solution = new Solution();
+            var result = solution.AddSpaces(s, spaces);
+            result.Should().Be(expected);
+        }
+    }
+}
