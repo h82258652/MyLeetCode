5d15bf8f5a62915ddae11a9d95e0e5f619a7b2dd
h82258652 <842053625@qq.com>
2022-03-31 17:07:02 +0800

    submit 897 solution

---
+++ src/LeetCode897/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode897
+{
+    public class Solution
+    {
+        public TreeNode IncreasingBST(TreeNode root)
+        {
+            var nodes = new List<TreeNode>();
+            Inorder(root, nodes);
+
+            var newRoot = nodes[0];
+
+            for (var i = 1; i < nodes.Count; i++)
+            {
+                var parent = nodes[i - 1];
+                parent.left = null;
+                parent.right = nodes[i];
+
+                if (i == nodes.Count - 1)
+                {
+                    nodes[i].left = null;
+                    nodes[i].right = null;
+                }
+            }
+
+            return newRoot;
+        }
+
+        private void Inorder(TreeNode node, List<TreeNode> nodes)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            Inorder(node.left, nodes);
+            nodes.Add(node);
+            Inorder(node.right, nodes);
+        }
+    }
+}
---
+++ src/LeetCode897/TestCases.cs
@@ -0,0 +1,70 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode897
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var root = new TreeNode(5)
+            {
+                left = new TreeNode(3)
+                {
+                    left = new TreeNode(2)
+                    {
+                        left = new TreeNode(1)
+                    },
+                    right = new TreeNode(4)
+                },
+                right = new TreeNode(6)
+                {
+                    right = new TreeNode(8)
+                    {
+                        left = new TreeNode(7),
+                        right = new TreeNode(9)
+                    }
+                }
+            };
+            var result = solution.IncreasingBST(root);
+            result.val.Should().Be(1);
+            result.left.Should().BeNull();
+            result.right.val.Should().Be(2);
+            result.right.left.Should().BeNull();
+            result.right.right.val.Should().Be(3);
+            result.right.right.left.Should().BeNull();
+            result.right.right.right.val.Should().Be(4);
+            result.right.right.right.left.Should().BeNull();
+            result.right.right.right.right.val.Should().Be(5);
+            result.right.right.right.right.left.Should().BeNull();
+            result.right.right.right.right.right.val.Should().Be(6);
+            result.right.right.right.right.right.left.Should().BeNull();
+            result.right.right.right.right.right.right.val.Should().Be(7);
+            result.right.right.right.right.right.right.left.Should().BeNull();
+            result.right.right.right.right.right.right.right.val.Should().Be(8);
+            result.right.right.right.right.right.right.right.left.Should().BeNull();
+            result.right.right.right.right.right.right.right.right.val.Should().Be(9);
+            result.right.right.right.right.right.right.right.right.left.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var root = new TreeNode(5)
+            {
+                left = new TreeNode(1),
+                right = new TreeNode(7)
+            };
+            var result = solution.IncreasingBST(root);
+            result.val.Should().Be(1);
+            result.left.Should().BeNull();
+            result.right.val.Should().Be(5);
+            result.right.left.Should().BeNull();
+            result.right.right.val.Should().Be(7);
+            result.right.right.left.Should().BeNull();
+        }
+    }
+}
