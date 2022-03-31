10958d235b5dd7efe0d63062611953102c243991
h82258652 <842053625@qq.com>
2022-03-31 13:21:33 +0800

    submit 2196 solution

---
+++ src/LeetCode2196/Solution.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode2196
+{
+    public class Solution
+    {
+        public TreeNode CreateBinaryTree(int[][] descriptions)
+        {
+            var nodes = new Dictionary<int, TreeNode>();
+            var parents = new HashSet<TreeNode>();
+
+            foreach (var description in descriptions)
+            {
+                var parentValue = description[0];
+                var childValue = description[1];
+                var isLeft = description[2] == 1;
+
+                if (!nodes.TryGetValue(parentValue, out var parent))
+                {
+                    parent = new TreeNode(parentValue);
+                    nodes[parentValue] = parent;
+                    parents.Add(parent);
+                }
+
+                if (!nodes.TryGetValue(childValue, out var child))
+                {
+                    child = new TreeNode(childValue);
+                    nodes[childValue] = child;
+                }
+
+                parents.Remove(child);
+
+                if (isLeft)
+                {
+                    parent.left = child;
+                }
+                else
+                {
+                    parent.right = child;
+                }
+            }
+
+            return parents.Single();
+        }
+    }
+}
---
+++ src/LeetCode2196/TestCases.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2196
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var descriptions = new[]
+            {
+                new[] { 20, 15, 1 },
+                new[] { 20, 17, 0 },
+                new[] { 50, 20, 1 },
+                new[] { 50, 80, 0 },
+                new[] { 80, 19, 1 }
+            };
+            var result = solution.CreateBinaryTree(descriptions);
+            result.val.Should().Be(50);
+            result.left.val.Should().Be(20);
+            result.left.left.val.Should().Be(15);
+            result.left.right.val.Should().Be(17);
+            result.right.val.Should().Be(80);
+            result.right.left.val.Should().Be(19);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var descriptions = new[]
+            {
+                new[] { 1, 2, 1 },
+                new[] { 2, 3, 0 },
+                new[] { 3, 4, 1 }
+            };
+            var result = solution.CreateBinaryTree(descriptions);
+            result.val.Should().Be(1);
+            result.left.val.Should().Be(2);
+            result.left.right.val.Should().Be(3);
+            result.left.right.left.val.Should().Be(4);
+        }
+    }
+}
