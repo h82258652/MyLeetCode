de056bc6b1c2bdd9a20dd7b0c7d7313999791a21
h82258652 <842053625@qq.com>
2022-03-31 14:37:55 +0800

    submit 2133 solution

---
+++ src/LeetCode2133/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode2133
+{
+    public class Solution
+    {
+        public bool CheckValid(int[][] matrix)
+        {
+            var n = matrix.Length;
+            for (var i = 0; i < n; i++)
+            {
+                var set = new HashSet<int>();
+                for (var j = 0; j < n; j++)
+                {
+                    var num = matrix[i][j];
+                    if (set.Contains(num))
+                    {
+                        return false;
+                    }
+
+                    set.Add(num);
+                }
+            }
+
+            for (var j = 0; j < n; j++)
+            {
+                var set = new HashSet<int>();
+                for (var i = 0; i < n; i++)
+                {
+                    var num = matrix[i][j];
+                    if (set.Contains(num))
+                    {
+                        return false;
+                    }
+
+                    set.Add(num);
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/LeetCode2133/TestCases.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2133
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var matrix = new[]
+            {
+                new[] { 1, 2, 3 },
+                new[] { 3, 1, 2 },
+                new[] { 2, 3, 1 }
+            };
+            var result = solution.CheckValid(matrix);
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var matrix = new[]
+            {
+                new[] { 1, 1, 1 },
+                new[] { 1, 2, 3 },
+                new[] { 1, 2, 3 }
+            };
+            var result = solution.CheckValid(matrix);
+            result.Should().BeFalse();
+        }
+    }
+}
