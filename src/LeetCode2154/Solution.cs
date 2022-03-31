83dbf8a3b847a2819a17efaac0126e20c349f635
h82258652 <842053625@qq.com>
2022-03-31 12:58:28 +0800

    submit 2154 solution

---
+++ src/LeetCode2154/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LeetCode2154
+{
+    public class Solution
+    {
+        public int FindFinalValue(int[] nums, int original)
+        {
+            Array.Sort(nums);
+            foreach (var num in nums)
+            {
+                if (num == original)
+                {
+                    original <<= 1;
+                }
+            }
+
+            return original;
+        }
+    }
+}
---
+++ src/LeetCode2154/TestCases.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2154
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var nums = new[] { 5, 3, 6, 1, 12 };
+            var original = 3;
+            var result = solution.FindFinalValue(nums, original);
+            result.Should().Be(24);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var nums = new[] { 2, 7, 9 };
+            var original = 4;
+            var result = solution.FindFinalValue(nums, original);
+            result.Should().Be(4);
+        }
+    }
+}
