0c811a50a9a0cac28cea46146814f7b8f2d56c5a
h82258652 <842053625@qq.com>
2022-03-31 09:28:17 +0800

    submit 2186 solution

---
+++ src/LeetCode2186/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeetCode2186
+{
+    public class Solution
+    {
+        public int MinSteps(string s, string t)
+        {
+            var sCount = new int[26];
+            var tCount = new int[26];
+            foreach (var c in s)
+            {
+                sCount[c - 'a']++;
+            }
+
+            foreach (var c in t)
+            {
+                tCount[c - 'a']++;
+            }
+
+            var totalCount = 0;
+            for (var i = 0; i < 26; i++)
+            {
+                totalCount += Math.Abs(sCount[i] - tCount[i]);
+            }
+
+            return totalCount;
+        }
+    }
+}
---
+++ src/LeetCode2186/TestCases.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2186
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var result = solution.MinSteps("leetcode", "coats");
+            result.Should().Be(7);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var result = solution.MinSteps("night", "thing");
+            result.Should().Be(0);
+        }
+    }
+}
