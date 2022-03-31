6930e0b50b188b2efdfcab764e111888b519d88e
h82258652 <842053625@qq.com>
2022-03-31 15:12:05 +0800

    submit 2129 solution

---
+++ src/LeetCode2129/Solution.cs
@@ -0,0 +1,63 @@
+﻿namespace LeetCode2129
+{
+    public class Solution
+    {
+        public string CapitalizeTitle(string title)
+        {
+            var chars = title.ToCharArray();
+            var left = 0;
+            for (var i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] != ' ')
+                {
+                    continue;
+                }
+
+                switch (i - left)
+                {
+                    case 1:
+                        chars[left] = char.ToLowerInvariant(chars[left]);
+                        break;
+
+                    case 2:
+                        chars[left] = char.ToLowerInvariant(chars[left]);
+                        chars[left + 1] = char.ToLowerInvariant(chars[left + 1]);
+                        break;
+
+                    default:
+                        ToTitle(chars, left, i);
+                        break;
+                }
+
+                left = i + 1;
+            }
+
+            switch (chars.Length - left)
+            {
+                case 1:
+                    chars[left] = char.ToLowerInvariant(chars[left]);
+                    break;
+
+                case 2:
+                    chars[left] = char.ToLowerInvariant(chars[left]);
+                    chars[left + 1] = char.ToLowerInvariant(chars[left + 1]);
+                    break;
+
+                default:
+                    ToTitle(chars, left, chars.Length);
+                    break;
+            }
+
+            return new string(chars);
+        }
+
+        private void ToTitle(char[] chars, int left, int right)
+        {
+            chars[left] = char.ToUpperInvariant(chars[left]);
+            for (var i = left + 1; i < right; i++)
+            {
+                chars[i] = char.ToLowerInvariant(chars[i]);
+            }
+        }
+    }
+}
---
+++ src/LeetCode2129/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2129
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("capiTalIze tHe title", "Capitalize The Title")]
+        [InlineData("First leTTeR of EACH Word", "First Letter of Each Word")]
+        [InlineData("i lOve leetcode", "i Love Leetcode")]
+        public void Test(string title, string expected)
+        {
+            var solution = new Solution();
+            var result = solution.CapitalizeTitle(title);
+            result.Should().Be(expected);
+        }
+    }
+}
