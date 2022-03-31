d88a4420aba88df4bbd9d51f375cb680c4a97d27
h82258652 <842053625@qq.com>
2022-03-31 17:17:08 +0800

    submit 513 solution

---
+++ src/LeetCode513/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode513
+{
+    public class Solution
+    {
+        public int FindBottomLeftValue(TreeNode root)
+        {
+            var nodes = new Queue<TreeNode>();
+            nodes.Enqueue(root);
+
+            var result = 0;
+            while (nodes.Count > 0)
+            {
+                var levelCount = nodes.Count;
+                result = nodes.Peek().val;
+
+                for (var i = 0; i < levelCount; i++)
+                {
+                    var node = nodes.Dequeue();
+                    if (node.left != null)
+                    {
+                        nodes.Enqueue(node.left);
+                    }
+
+                    if (node.right != null)
+                    {
+                        nodes.Enqueue(node.right);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode513/TestCases.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode513
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var root = new TreeNode(2)
+            {
+                left = new TreeNode(1),
+                right = new TreeNode(3)
+            };
+            var result = solution.FindBottomLeftValue(root);
+            result.Should().Be(1);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var root = new TreeNode(1)
+            {
+                left = new TreeNode(2)
+                {
+                    left = new TreeNode(4)
+                },
+                right = new TreeNode(3)
+                {
+                    left = new TreeNode(5)
+                    {
+                        left = new TreeNode(7)
+                    },
+                    right = new TreeNode(6)
+                }
+            };
+            var result = solution.FindBottomLeftValue(root);
+            result.Should().Be(7);
+        }
+    }
+}
