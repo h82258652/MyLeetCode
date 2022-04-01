8b428dcc4bc57ee4f81bf96bce646fddd9e63d3f
h82258652 <842053625@qq.com>
2022-04-01 11:08:57 +0800

    submit 954 solution

---
+++ src/LeetCode954/Solution.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode954
+{
+    public class Solution
+    {
+        public bool CanReorderDoubled(int[] arr)
+        {
+            Array.Sort(arr);
+            var positives = new List<int>();
+            var zeros = new List<int>();
+            var minus = new List<int>();
+
+            foreach (var i in arr)
+            {
+                if (i > 0)
+                {
+                    positives.Add(i);
+                }
+                else if (i == 0)
+                {
+                    zeros.Add(i);
+                }
+                else
+                {
+                    minus.Add(i);
+                }
+            }
+
+            if (positives.Count % 2 == 1 ||
+                zeros.Count % 2 == 1 ||
+                minus.Count % 2 == 1)
+            {
+                return false;
+            }
+
+            while (positives.Count > 0)
+            {
+                var last = positives[^1];
+                if (last % 2 == 1)
+                {
+                    return false;
+                }
+
+                var half = last / 2;
+                if (positives.Remove(half))
+                {
+                    positives.Remove(last);
+                    continue;
+                }
+
+                return false;
+            }
+
+            while (minus.Count > 0)
+            {
+                var first = minus[0];
+                if (first % 2 == -1)
+                {
+                    return false;
+                }
+
+                var half = first / 2;
+                if (minus.Remove(half))
+                {
+                    minus.Remove(first);
+                    continue;
+                }
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/LeetCode954/TestCases.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode954
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var result = solution.CanReorderDoubled(new[] { 3, 1, 3, 6 });
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var result = solution.CanReorderDoubled(new[] { 2, 1, 2, 6 });
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var solution = new Solution();
+            var result = solution.CanReorderDoubled(new[] { 4, -2, 2, -4 });
+            result.Should().BeTrue();
+        }
+    }
+}
