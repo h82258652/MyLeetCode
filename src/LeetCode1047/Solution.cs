c4c12c1446a92ce34c104fbbd81be810b11a0530
h82258652 <h82258652@hotmail.com>
2022-04-02 22:43:32 +0800

    submit 1047 solution

---
+++ src/LeetCode1047/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode1047
+{
+    public class Solution
+    {
+        public string RemoveDuplicates(string s)
+        {
+            var stack = new Stack<char>();
+            foreach (var c in s)
+            {
+                if (stack.Count > 0)
+                {
+                    if (stack.Peek() == c)
+                    {
+                        stack.Pop();
+                        continue;
+                    }
+                }
+
+                stack.Push(c);
+            }
+
+            return new string(stack.Reverse().ToArray());
+        }
+    }
+}
---
+++ src/LeetCode1047/TestCases.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1047
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var result = solution.RemoveDuplicates("abbaca");
+            result.Should().Be("ca");
+        }
+    }
+}
