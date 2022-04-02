2360bba23581e8fafcd37815fbc02b2292e01ffa
h82258652 <h82258652@hotmail.com>
2022-04-03 00:03:41 +0800

    submit 1046 solution

---
+++ src/LeetCode1046/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode1046
+{
+    public class Solution
+    {
+        public int LastStoneWeight(int[] stones)
+        {
+            var queue = new PriorityQueue<int, int>();
+            foreach (var stone in stones)
+            {
+                queue.Enqueue(stone, -stone);
+            }
+
+            while (queue.Count > 1)
+            {
+                var y = queue.Dequeue();
+                var x = queue.Dequeue();
+                if (y - x != 0)
+                {
+                    queue.Enqueue(y - x, -(y - x));
+                }
+            }
+
+            if (queue.Count == 0)
+            {
+                return 0;
+            }
+
+            return queue.Peek();
+        }
+    }
+}
---
+++ src/LeetCode1046/TestCases.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1046
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var stones = new[] { 2, 7, 4, 1, 8, 1 };
+            var result = solution.LastStoneWeight(stones);
+            result.Should().Be(1);
+        }
+    }
+}
