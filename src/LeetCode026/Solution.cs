a94b0a7c3a239da47f864b43ca0848ac76aa3850
h82258652 <h82258652@hotmail.com>
2022-04-03 10:28:43 +0800

    submit 026 solution

---
+++ src/LeetCode026/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode026
+{
+    public class Solution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            var previous = int.MinValue;
+            var k = 0;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var num = nums[i];
+                if (num == previous)
+                {
+                    continue;
+                }
+
+                nums[k] = num;
+                k++;
+                previous = num;
+            }
+
+            return k;
+        }
+    }
+}
---
+++ src/LeetCode026/TestCases.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode026
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var nums = new int[] { 1, 1, 2 };
+            var result = solution.RemoveDuplicates(nums);
+            result.Should().Be(2);
+            nums.Take(result).Should().Equal(1, 2);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var nums = new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+            var result = solution.RemoveDuplicates(nums);
+            result.Should().Be(5);
+            nums.Take(result).Should().Equal(0, 1, 2, 3, 4);
+        }
+    }
+}
