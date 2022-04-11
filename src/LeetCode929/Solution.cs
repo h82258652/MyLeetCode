3919d006a5d664029ce69d8e799b4064fbd9c974
h82258652 <h82258652@hotmail.com>
2022-04-11 19:41:11 +0800

    submit 929 solution

---
+++ src/LeetCode929/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode929
+{
+    public class Solution
+    {
+        public int NumUniqueEmails(string[] emails)
+        {
+            var processedEmails = new HashSet<string>();
+            foreach (var email in emails)
+            {
+                processedEmails.Add(Process(email));
+            }
+
+            return processedEmails.Count;
+        }
+
+        private string Process(string email)
+        {
+            var temp = email.Split('@');
+            var local = temp[0];
+            var domain = temp[1];
+            var builder = new StringBuilder();
+            foreach (var c in local)
+            {
+                if (c == '+')
+                {
+                    break;
+                }
+
+                if (c == '.')
+                {
+                    continue;
+                }
+
+                builder.Append(c);
+            }
+
+            return builder.ToString() + "@" + domain;
+        }
+    }
+}
---
+++ src/LeetCode929/TestCases.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode929
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var emails = new[] { "test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com" };
+            var result = solution.NumUniqueEmails(emails);
+            result.Should().Be(2);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var emails = new[] { "a@leetcode.com", "b@leetcode.com", "c@leetcode.com" };
+            var result = solution.NumUniqueEmails(emails);
+            result.Should().Be(3);
+        }
+    }
+}
