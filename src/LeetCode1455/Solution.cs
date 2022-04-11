a78778d8a7f2a01d177b3090246e978c5b7a1508
h82258652 <h82258652@hotmail.com>
2022-04-11 19:31:12 +0800

    submit 1455 solution

---
+++ src/LeetCode1455/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode1455
+{
+    public class Solution
+    {
+        public int IsPrefixOfWord(string sentence, string searchWord)
+        {
+            var words = sentence.Split(' ');
+            for (var i = 0; i < words.Length; i++)
+            {
+                var word = words[i];
+                if (word.StartsWith(searchWord))
+                {
+                    return i + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ src/LeetCode1455/TestCases.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1455
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var result = solution.IsPrefixOfWord("i love eating burger", "burg");
+            result.Should().Be(4);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var result = solution.IsPrefixOfWord("this problem is an easy problem", "pro");
+            result.Should().Be(2);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var solution = new Solution();
+            var result = solution.IsPrefixOfWord("i am tired", "you");
+            result.Should().Be(-1);
+        }
+    }
+}
