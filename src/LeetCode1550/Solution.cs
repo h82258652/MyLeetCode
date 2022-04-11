6fcfcccd831974d836534b3d5edae815b4265037
h82258652 <h82258652@hotmail.com>
2022-04-11 21:28:49 +0800

    submit 1550 solution

---
+++ src/LeetCode1550/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode1550
+{
+    public class Solution
+    {
+        public bool ThreeConsecutiveOdds(int[] arr)
+        {
+            for (var i = 0; i < arr.Length - 2; i++)
+            {
+                if (arr[i] % 2 == 1 && arr[i + 1] % 2 == 1 && arr[i + 2] % 2 == 1)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ src/LeetCode1550/TestCases.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1550
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var arr = new int[] { 2, 6, 4, 1 };
+            var result = solution.ThreeConsecutiveOdds(arr);
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var arr = new int[] { 1, 2, 34, 3, 4, 5, 7, 23, 12 };
+            var result = solution.ThreeConsecutiveOdds(arr);
+            result.Should().BeTrue();
+        }
+    }
+}
