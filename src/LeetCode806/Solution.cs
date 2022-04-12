d1e7bb85ab2cd3f9a0a12d2c71f7f55f534f95cd
h82258652 <842053625@qq.com>
2022-04-12 10:12:06 +0800

    submit 806 solution

---
+++ src/LeetCode806/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode806
+{
+    public class Solution
+    {
+        public int[] NumberOfLines(int[] widths, string s)
+        {
+            var lineCount = 0;
+            var lastLineWidth = 0;
+            foreach (var c in s)
+            {
+                var width = widths[c - 'a'];
+                if (lastLineWidth + width > 100)
+                {
+                    lineCount++;
+                    lastLineWidth = width;
+                }
+                else
+                {
+                    lastLineWidth += width;
+                }
+            }
+
+            return new[] { lineCount + 1, lastLineWidth };
+        }
+    }
+}
