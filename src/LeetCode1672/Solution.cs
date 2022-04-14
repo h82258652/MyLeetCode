1e17457c5ea2914ecbb2c8f244b13a97fd2c72f7
h82258652 <842053625@qq.com>
2022-04-14 09:31:38 +0800

    submit 1672 solution

---
+++ src/LeetCode1672/Solution.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace LeetCode1672
+{
+    public class Solution
+    {
+        public int MaximumWealth(int[][] accounts)
+        {
+            return accounts.Max(temp => temp.Sum());
+        }
+    }
+}
---
+++ src/LeetCode1672/TestCases.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1672
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var accounts = new[]
+            {
+                new[] { 1, 2, 3 },
+                new[] { 3, 2, 1 }
+            };
+            var result = solution.MaximumWealth(accounts);
+            result.Should().Be(6);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var accounts = new[]
+            {
+                new[] { 1, 5 },
+                new[] { 7, 3 },
+                new[] { 3, 5 }
+            };
+            var result = solution.MaximumWealth(accounts);
+            result.Should().Be(10);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var solution = new Solution();
+            var accounts = new[]
+            {
+                new[] { 2, 8, 7 },
+                new[] { 7, 1, 3 },
+                new[] { 1, 9, 5 }
+            };
+            var result = solution.MaximumWealth(accounts);
+            result.Should().Be(17);
+        }
+    }
+}
