eda87be987b34b506bd2cee6e24a97a4774f325c
h82258652 <h82258652@hotmail.com>
2022-04-17 14:47:56 +0800

    submit 819 solution

---
+++ src/LeetCode819/Solution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode819
+{
+    public class Solution
+    {
+        public string MostCommonWord(string paragraph, string[] banned)
+        {
+            return paragraph
+                .Split(new char[] { ' ', '!', '?', '\'', ',', ';', '.' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(temp => temp.ToLowerInvariant())
+                .Where(temp => !banned.Contains(temp))
+                .GroupBy(temp => temp)
+                .OrderByDescending(temp => temp.Count())
+                .First().Key;
+        }
+    }
+}
