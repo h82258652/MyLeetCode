ccf4707c15ffc87a68303960082cac6fc8e14f3d
h82258652 <h82258652@hotmail.com>
2022-04-20 20:08:03 +0800

    submit 2239 solution

---
+++ src/LeetCode2239/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LeetCode2239
+{
+    public class Solution
+    {
+        public int FindClosestNumber(int[] nums)
+        {
+            Array.Sort(nums);
+            var minDistance = int.MaxValue;
+            var result = int.MaxValue;
+            for (var i = nums.Length - 1; i >= 0; i--)
+            {
+                var distance = Math.Abs(nums[i]);
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    result = nums[i];
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode2239/TestCases.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2239
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var result = solution.FindClosestNumber(new int[] { -4, -2, 1, 4, 8 });
+            result.Should().Be(1);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var result = solution.FindClosestNumber(new int[] { 2, -1, 1 });
+            result.Should().Be(1);
+        }
+    }
+}
