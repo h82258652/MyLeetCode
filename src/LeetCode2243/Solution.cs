96cb859b61a2246921ca64f5bb758a18dd19ba7d
h82258652 <h82258652@hotmail.com>
2022-04-20 19:59:37 +0800

    submit 2243 solution

---
+++ src/LeetCode2243/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace LeetCode2243
+{
+    public class Solution
+    {
+        public string DigitSum(string s, int k)
+        {
+            if (s.Length <= k)
+            {
+                return s;
+            }
+
+            var builder = new StringBuilder();
+            for (var i = 0; i < s.Length; i += k)
+            {
+                var sum = 0;
+                for (var j = i; j < i + k && j < s.Length; j++)
+                {
+                    sum += s[j] - '0';
+                }
+
+                builder.Append(sum);
+            }
+
+            return DigitSum(builder.ToString(), k);
+        }
+    }
+}
---
+++ src/LeetCode2243/TestCases.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2243
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var result = solution.DigitSum("11111222223", 3);
+            result.Should().Be("135");
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var result = solution.DigitSum("00000000", 3);
+            result.Should().Be("000");
+        }
+    }
+}
