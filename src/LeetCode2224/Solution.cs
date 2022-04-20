be06013d06e3d4daa1a5284510eccc05d333907e
h82258652 <h82258652@hotmail.com>
2022-04-20 20:25:12 +0800

    submit 2224 solution

---
+++ src/LeetCode2224/Solution.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode2224
+{
+    public class Solution
+    {
+        public int ConvertTime(string current, string correct)
+        {
+            var currentTime = ToMinutes(current);
+            var correctTime = ToMinutes(correct);
+            var step = 0;
+            while (correctTime > currentTime)
+            {
+                if (correctTime - 60 >= currentTime)
+                {
+                    correctTime -= 60;
+                }
+                else if (correctTime - 15 >= currentTime)
+                {
+                    correctTime -= 15;
+                }
+                else if (correctTime - 5 >= currentTime)
+                {
+                    correctTime -= 5;
+                }
+                else
+                {
+                    correctTime--;
+                }
+
+                step++;
+            }
+            return step;
+        }
+
+        private int ToMinutes(string time)
+        {
+            var hour = int.Parse(time.Substring(0, 2));
+            var minute = int.Parse(time.Substring(3, 2));
+            return hour * 60 + minute;
+        }
+    }
+}
---
+++ src/LeetCode2224/TestCases.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2224
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var result = solution.ConvertTime("02:30", "04:35");
+            result.Should().Be(3);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var result = solution.ConvertTime("11:00", "11:01");
+            result.Should().Be(1);
+        }
+    }
+}
