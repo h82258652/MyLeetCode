5bc22f8e7ce8e027dfec02e1dadcc42398e7efe0
h82258652 <842053625@qq.com>
2022-04-20 10:32:43 +0800

    submit 388 solution

---
+++ src/LeetCode388/Solution.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode388
+{
+    public class Solution
+    {
+        public int GetLongestLength(Node node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+
+            if (node.IsFile)
+            {
+                return node.Length;
+            }
+
+            var length = 0;
+            foreach (var child in node.Children)
+            {
+                var childLongestLength = GetLongestLength(child);
+                if (childLongestLength <= 0)
+                {
+                    continue;
+                }
+
+                length = node.Length == 0 ?
+                    Math.Max(length, childLongestLength) :
+                    Math.Max(length, node.Length + 1 + childLongestLength);
+            }
+
+            return length;
+        }
+
+        public int LengthLongestPath(string input)
+        {
+            if (input.Length <= 0)
+            {
+                return 0;
+            }
+
+            var root = new Node();
+            var lines = input.Split('\n');
+            foreach (var line in lines)
+            {
+                var tabLength = GetTabLength(line);
+
+                var newNode = new Node
+                {
+                    IsFile = line.Contains('.'),
+                    Length = line.Length - tabLength
+                };
+
+                var node = root;
+                while (tabLength > 0)
+                {
+                    node = node.Children.Last(temp => !temp.IsFile);
+                    tabLength--;
+                }
+
+                node.Children.Add(newNode);
+            }
+
+            return GetLongestLength(root);
+        }
+
+        private static int GetTabLength(string line)
+        {
+            var tabLength = 0;
+            for (var i = 0; i < line.Length; i++)
+            {
+                var c = line[i];
+                if (c != '\t')
+                {
+                    break;
+                }
+
+                tabLength++;
+            }
+
+            return tabLength;
+        }
+
+        public class Node
+        {
+            public List<Node> Children { get; set; } = new List<Node>();
+
+            public bool IsFile { get; set; }
+
+            public int Length { get; set; }
+        }
+    }
+}
---
+++ src/LeetCode388/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode388
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext", 20)]
+        [InlineData("dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext", 32)]
+        [InlineData("a", 0)]
+        [InlineData("file1.txt\nfile2.txt\nlongfile.txt", 12)]
+        public void Test(string input, int expected)
+        {
+            var solution = new Solution();
+            var result = solution.LengthLongestPath(input);
+            result.Should().Be(expected);
+        }
+    }
+}
