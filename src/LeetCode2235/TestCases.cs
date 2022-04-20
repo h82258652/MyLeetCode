9493a42663d48b436db4c4d0ba28d242fa656854
h82258652 <h82258652@hotmail.com>
2022-04-20 20:11:24 +0800

    submit 2235 solution

---
+++ src/LeetCode2235/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2235
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(12, 5, 17)]
+        [InlineData(-10, 4, -6)]
+        public void Test(int num1, int num2, int excepted)
+        {
+            var solution = new Solution();
+            var result = solution.Sum(num1, num2);
+            result.Should().Be(excepted);
+        }
+    }
+}
