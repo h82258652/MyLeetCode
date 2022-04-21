fdfd882f79f0023ae50e94c4725902899ff95cab
h82258652 <842053625@qq.com>
2022-04-21 09:31:37 +0800

    submit 824 solution

---
+++ src/LeetCode824/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode824
+{
+    public class Solution
+    {
+        public string ToGoatLatin(string sentence)
+        {
+            var words = sentence.Split(' ');
+            var builder = new StringBuilder();
+            for (var i = 0; i < words.Length; i++)
+            {
+                var newWord = ChangeWord(words[i], i);
+                builder.Append(newWord);
+                if (i != words.Length - 1)
+                {
+                    builder.Append(' ');
+                }
+            }
+
+            return builder.ToString();
+        }
+
+        private string ChangeWord(string word, int index)
+        {
+            if (word.StartsWith("a", StringComparison.OrdinalIgnoreCase) ||
+                word.StartsWith("e", StringComparison.OrdinalIgnoreCase) ||
+                word.StartsWith("i", StringComparison.OrdinalIgnoreCase) ||
+                word.StartsWith("o", StringComparison.OrdinalIgnoreCase) ||
+                word.StartsWith("u", StringComparison.OrdinalIgnoreCase))
+            {
+                return word + "ma" + new string('a', index + 1);
+            }
+            else
+            {
+                var firstChar = word.Substring(0, 1);
+                var remain = word.Substring(1);
+                return remain + firstChar + "ma" + new string('a', index + 1);
+            }
+        }
+    }
+}
---
+++ src/LeetCode824/TestCases.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode824
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var result = solution.ToGoatLatin("I speak Goat Latin");
+            result.Should().Be("Imaa peaksmaaa oatGmaaaa atinLmaaaaa");
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var result = solution.ToGoatLatin("The quick brown fox jumped over the lazy dog");
+            result.Should().Be("heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa");
+        }
+    }
+}
