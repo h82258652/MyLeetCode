c7c434f8d74519c2702020120b186dee137381d8
h82258652 <842053625@qq.com>
2022-04-24 09:40:03 +0800

    submit 868 solution

---
+++ src/LeetCode868/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LeetCode868
+{
+    public class Solution
+    {
+        public int BinaryGap(int n)
+        {
+            int? firstIndex = null;
+
+            var result = 0;
+
+            var index = 0;
+            while (n > 0)
+            {
+                if (n % 2 == 1)
+                {
+                    if (!firstIndex.HasValue)
+                    {
+                        firstIndex = index;
+                    }
+                    else
+                    {
+                        result = Math.Max(result, index - firstIndex.Value);
+                        firstIndex = index;
+                    }
+                }
+
+                n /= 2;
+                index++;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode868/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode868
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData(22, 2)]
+        [InlineData(8, 0)]
+        [InlineData(5, 2)]
+        public void Test(int n, int expected)
+        {
+            var solution = new Solution();
+            var result = solution.BinaryGap(n);
+            result.Should().Be(expected);
+        }
+    }
+}
