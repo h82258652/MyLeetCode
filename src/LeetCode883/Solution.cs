fab59667dd7d52b18da7dd87a724a62eeeb6bfa8
h82258652 <842053625@qq.com>
2022-04-26 10:56:44 +0800

    submit 883 solution

---
+++ src/LeetCode883/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LeetCode883
+{
+    public class Solution
+    {
+        public int ProjectionArea(int[][] grid)
+        {
+            var n = grid.Length;
+
+            var xy = 0;
+            var yz = 0;
+            var zx = 0;
+            for (var i = 0; i < n; i++)
+            {
+                var x = 0;
+                var y = 0;
+                for (var j = 0; j < n; j++)
+                {
+                    x = Math.Max(x, grid[j][i]);
+                    y = Math.Max(y, grid[i][j]);
+                    if (grid[i][j] > 0)
+                    {
+                        xy++;
+                    }
+                }
+
+                yz += x;
+                zx += y;
+            }
+
+            return xy + yz + zx;
+        }
+    }
+}
---
+++ src/LeetCode883/TestCases.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode883
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var grid = new[]
+            {
+                new[] { 1, 2 },
+                new[] { 3, 4 }
+            };
+            var result = solution.ProjectionArea(grid);
+            result.Should().Be(17);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var grid = new[]
+            {
+                new[] { 2 }
+            };
+            var result = solution.ProjectionArea(grid);
+            result.Should().Be(17);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var solution = new Solution();
+            var grid = new[]
+            {
+                new[] { 1, 0 },
+                new[] { 0, 2 }
+            };
+            var result = solution.ProjectionArea(grid);
+            result.Should().Be(17);
+        }
+    }
+}
