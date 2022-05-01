9b1df6cfe1b6e09be437c52eeb621c9f419b434f
h82258652 <h82258652@hotmail.com>
2022-05-01 13:15:59 +0800

    submit 1305 solution

---
+++ src/LeetCode1305/Solution.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode1305
+{
+    public class Solution
+    {
+        public IList<int> GetAllElements(TreeNode root1, TreeNode root2)
+        {
+            var list1 = new List<int>();
+            var list2 = new List<int>();
+            Inorder(root1, list1);
+            Inorder(root2, list2);
+            return Merge(list1, list2);
+        }
+
+        private void Inorder(TreeNode node, List<int> list)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            Inorder(node.left, list);
+            list.Add(node.val);
+            Inorder(node.right, list);
+        }
+
+        private List<int> Merge(List<int> list1, List<int> list2)
+        {
+            var list = new List<int>();
+            var i1 = 0;
+            var i2 = 0;
+            while (i1 < list1.Count || i2 < list2.Count)
+            {
+                if (i1 == list1.Count)
+                {
+                    list.Add(list2[i2]);
+                    i2++;
+                }
+                else if (i2 == list2.Count)
+                {
+                    list.Add(list1[i1]);
+                    i1++;
+                }
+                else
+                {
+                    if (list1[i1] < list2[i2])
+                    {
+                        list.Add(list1[i1]);
+                        i1++;
+                    }
+                    else
+                    {
+                        list.Add(list2[i2]);
+                        i2++;
+                    }
+                }
+            }
+
+            return list;
+        }
+    }
+}
---
+++ src/LeetCode1305/TestCases.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1305
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var root1 = new TreeNode(2)
+            {
+                left = new TreeNode(1),
+                right = new TreeNode(4)
+            };
+            var root2 = new TreeNode(1)
+            {
+                left = new TreeNode(0),
+                right = new TreeNode(3)
+            };
+            var result = solution.GetAllElements(root1, root2);
+            result.Should().Equal(0, 1, 1, 2, 3, 4);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var root1 = new TreeNode(1)
+            {
+                right = new TreeNode(8)
+            };
+            var root2 = new TreeNode(8)
+            {
+                left = new TreeNode(1)
+            };
+            var result = solution.GetAllElements(root1, root2);
+            result.Should().Equal(1, 1, 8, 8);
+        }
+    }
+}
