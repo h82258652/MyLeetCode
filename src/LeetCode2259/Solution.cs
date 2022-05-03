c43eea21c116699618c7d335ad9e2e5fe258743f
h82258652 <h82258652@hotmail.com>
2022-05-03 15:58:51 +0800

    submit 2259 solution

---
+++ src/LeetCode2259/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+
+namespace LeetCode2259
+{
+    public class Solution
+    {
+        public string RemoveDigit(string number, char digit)
+        {
+            var max = BigInteger.Zero;
+
+            for (var i = 0; i < number.Length; i++)
+            {
+                if (number[i] == digit)
+                {
+                    var value = BigInteger.Parse(number.Substring(0, i) + number.Substring(i + 1));
+                    max = BigInteger.Max(max, value);
+                }
+            }
+
+            return max.ToString();
+        }
+    }
+}
---
+++ src/LeetCode2259/TestCases.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2259
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var result = solution.RemoveDigit("123", '3');
+            result.Should().Be("12");
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var result = solution.RemoveDigit("1231", '1');
+            result.Should().Be("231");
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var solution = new Solution();
+            var result = solution.RemoveDigit("551", '5');
+            result.Should().Be("51");
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            var solution = new Solution();
+            var result = solution.RemoveDigit("2998589353917872714814599237991174513476623756395992135212546127959342974628712329595771672911914471", '3');
+            result.Should().Be("299858953917872714814599237991174513476623756395992135212546127959342974628712329595771672911914471");
+        }
+    }
+}
