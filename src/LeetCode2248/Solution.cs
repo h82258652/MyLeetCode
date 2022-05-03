198f73a00b1dd84f9f0bac4ebd5e58ba184fd7b6
h82258652 <h82258652@hotmail.com>
2022-05-03 15:39:45 +0800

    submit 2248 solution

---
+++ src/LeetCode2248/Solution.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode2248
+{
+    public class Solution
+    {
+        public IList<int> Intersection(int[][] nums)
+        {
+            IEnumerable<int> array = nums[0];
+            for (var i = 1; i < nums.Length; i++)
+            {
+                array = array.Intersect(nums[i]);
+            }
+
+            return array.OrderBy(temp => temp).ToList();
+        }
+    }
+}
---
+++ src/LeetCode2248/TestCases.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2248
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var nums = new int[][]
+            {
+                new int[] { 3, 1, 2, 4, 5 },
+                new int[] { 1, 2, 3, 4 },
+                new int[] { 3, 4, 5, 6 }
+            };
+            var result = solution.Intersection(nums);
+            result.Should().Equal(3, 4);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var nums = new int[][]
+            {
+                new int[] { 1, 2, 3 },
+                new int[] { 4, 5, 6 }
+            };
+            var result = solution.Intersection(nums);
+            result.Should().Equal(Array.Empty<int>());
+        }
+    }
+}
