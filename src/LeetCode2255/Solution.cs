44384c21018abc09b4ffdfd0defc40dac588f250
h82258652 <h82258652@hotmail.com>
2022-05-03 16:08:23 +0800

    submit 2255 solution

---
+++ src/LeetCode2255/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode2255
+{
+    public class Solution
+    {
+        public int CountPrefixes(string[] words, string s)
+        {
+            var prefixes = new HashSet<string>();
+            var buffer = new StringBuilder();
+            foreach (var c in s)
+            {
+                buffer.Append(c);
+                prefixes.Add(buffer.ToString());
+            }
+
+            var count = 0;
+            foreach (var word in words)
+            {
+                if (prefixes.Contains(word))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ src/LeetCode2255/TestCases.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2255
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var words = new string[] { "a", "b", "c", "ab", "bc", "abc" };
+            var s = "abc";
+            var result = solution.CountPrefixes(words, s);
+            result.Should().Be(3);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var words = new string[] { "a", "a" };
+            var s = "aa";
+            var result = solution.CountPrefixes(words, s);
+            result.Should().Be(2);
+        }
+    }
+}
