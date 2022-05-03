a935e91ac26d8f1b2fb01063764815e16cb70876
h82258652 <h82258652@hotmail.com>
2022-05-03 16:20:48 +0800

    submit 2053 solution

---
+++ src/LeetCode2053/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode2053
+{
+    public class Solution
+    {
+        public string KthDistinct(string[] arr, int k)
+        {
+            var counts = new Dictionary<string, int>();
+            foreach (var s in arr)
+            {
+                counts.TryGetValue(s, out var count);
+                count++;
+                counts[s] = count;
+            }
+
+            foreach (var s in arr)
+            {
+                if (counts[s] > 1)
+                {
+                    continue;
+                }
+
+                if (k == 1)
+                {
+                    return s;
+                }
+
+                k--;
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ src/LeetCode2053/TestCases.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2053
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var arr = new string[] { "d", "b", "c", "b", "c", "a" };
+            var k = 2;
+            var result = solution.KthDistinct(arr, k);
+            result.Should().Be("a");
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var arr = new string[] { "aaa", "aa", "a" };
+            var k = 1;
+            var result = solution.KthDistinct(arr, k);
+            result.Should().Be("aaa");
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var solution = new Solution();
+            var arr = new string[] { "a", "b", "a" };
+            var k = 3;
+            var result = solution.KthDistinct(arr, k);
+            result.Should().Be("");
+        }
+    }
+}
