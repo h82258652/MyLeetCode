d63f935bed539cfe1184bf7cbd56889cd393e154
h82258652 <h82258652@hotmail.com>
2022-05-03 15:23:53 +0800

    submit 937 solution

---
+++ src/LeetCode937/Solution.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode937
+{
+    public class Solution
+    {
+        public string[] ReorderLogFiles(string[] logs)
+        {
+            var result = new List<string>(logs.Length);
+
+            var letterLogs = new List<LetterLog>();
+            var digitLogs = new List<string>();
+
+            foreach (var log in logs)
+            {
+                var spaceIndex = log.IndexOf(' ');
+                if (!char.IsDigit(log[spaceIndex + 1]))
+                {
+                    var letterLog = new LetterLog(log, spaceIndex);
+                    letterLogs.Add(letterLog);
+                }
+                else
+                {
+                    digitLogs.Add(log);
+                }
+            }
+
+            foreach (var letterLog in letterLogs.OrderBy(temp => temp.Content).ThenBy(temp => temp.Identity))
+            {
+                result.Add(letterLog.Raw);
+            }
+
+            foreach (var digitLog in digitLogs)
+            {
+                result.Add(digitLog);
+            }
+
+            return result.ToArray();
+        }
+
+        public class LetterLog
+        {
+            public LetterLog(string raw, int spaceIndex)
+            {
+                Raw = raw;
+                Identity = raw.Substring(0, spaceIndex);
+                Content = raw.Substring(spaceIndex + 1);
+            }
+
+            public string Identity { get; set; }
+
+            public string Raw { get; set; }
+
+            public string Content { get; set; }
+        }
+    }
+}
---
+++ src/LeetCode937/TestCases.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode937
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var logs = new string[] { "dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero" };
+            var result = solution.ReorderLogFiles(logs);
+            result.Should().Equal("let1 art can", "let3 art zero", "let2 own kit dig", "dig1 8 1 5 1", "dig2 3 6");
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var logs = new string[] { "a1 9 2 3 1", "g1 act car", "zo4 4 7", "ab1 off key dog", "a8 act zoo" };
+            var result = solution.ReorderLogFiles(logs);
+            result.Should().Equal("g1 act car", "a8 act zoo", "ab1 off key dog", "a1 9 2 3 1", "zo4 4 7");
+        }
+    }
+}
