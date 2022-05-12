8391364849a1269b8ab021ade995c1d68b0d7c8a
h82258652 <842053625@qq.com>
2022-05-12 13:00:18 +0800

    submit 944 solution

---
+++ src/LeetCode944/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode944
+{
+    public class Solution
+    {
+        public int MinDeletionSize(string[] strs)
+        {
+            var n = strs.Length;
+            if (n == 1)
+            {
+                return 0;
+            }
+
+            var strLength = strs[0].Length;
+            var result = 0;
+            for (var i = 0; i < strLength; i++)
+            {
+                for (var j = 1; j < n; j++)
+                {
+                    if (strs[j][i] < strs[j - 1][i])
+                    {
+                        result++;
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
