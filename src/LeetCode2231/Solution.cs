243c88700d98cadbbe9065ec810463cf0e615ed3
h82258652 <h82258652@hotmail.com>
2022-05-15 18:11:46 +0800

    submit 2231 solution

---
+++ src/LeetCode2231/Solution.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode2231
+{
+    public class Solution
+    {
+        public int LargestInteger(int num)
+        {
+            var chars = num.ToString().ToCharArray();
+            var oddIndex = new List<int>();
+            var evenIndex = new List<int>();
+            for (var i = 0; i < chars.Length; i++)
+            {
+                if ((chars[i] - '0') % 2 == 1)
+                {
+                    oddIndex.Add(i);
+                }
+                else
+                {
+                    evenIndex.Add(i);
+                }
+            }
+
+            for (var i = 0; i < oddIndex.Count; i++)
+            {
+                for (var j = i + 1; j < oddIndex.Count; j++)
+                {
+                    var leftIndex = oddIndex[i];
+                    var rightIndex = oddIndex[j];
+                    if (chars[rightIndex] > chars[leftIndex])
+                    {
+                        (chars[rightIndex], chars[leftIndex]) = (chars[leftIndex], chars[rightIndex]);
+                    }
+                }
+            }
+            for (var i = 0; i < evenIndex.Count; i++)
+            {
+                for (var j = i + 1; j < evenIndex.Count; j++)
+                {
+                    var leftIndex = evenIndex[i];
+                    var rightIndex = evenIndex[j];
+                    if (chars[rightIndex] > chars[leftIndex])
+                    {
+                        (chars[rightIndex], chars[leftIndex]) = (chars[leftIndex], chars[rightIndex]);
+                    }
+                }
+            }
+
+            return int.Parse(new string(chars));
+        }
+    }
+}
---
+++ src/LeetCode2231/TestCases.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2231
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var result = solution.LargestInteger(1234);
+            result.Should().Be(3412);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var result = solution.LargestInteger(65875);
+            result.Should().Be(87655);
+        }
+    }
+}
