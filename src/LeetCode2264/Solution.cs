7b71bd2a3a782dc870e7bc80e5c7fac18ac03fcc
h82258652 <h82258652@hotmail.com>
2022-05-15 15:45:05 +0800

    submit 2264 solution

---
+++ src/LeetCode2264/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode2264
+{
+    public class Solution
+    {
+        public string LargestGoodInteger(string num)
+        {
+            var length = num.Length;
+            var result = string.Empty;
+            for (var i = 0; i < length - 2; i++)
+            {
+                if (num[i] != num[i + 1] || num[i + 1] != num[i + 2])
+                {
+                    continue;
+                }
+
+                if (result.Length == 0 || num[i] > result[0])
+                {
+                    result = new string(num[i], 3);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode2264/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode2264
+{
+    public class TestCases
+    {
+        [Theory]
+        [InlineData("6777133339", "777")]
+        [InlineData("2300019", "000")]
+        [InlineData("42352338", "")]
+        [InlineData("222", "222")]
+        public void Test(string num, string expected)
+        {
+            var solution = new Solution();
+            var result = solution.LargestGoodInteger(num);
+            result.Should().Be(expected);
+        }
+    }
+}
