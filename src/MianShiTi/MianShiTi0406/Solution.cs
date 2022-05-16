5ce0cb86a6dd472066167f0d6ec7760ec004bc04
h82258652 <842053625@qq.com>
2022-05-16 11:50:26 +0800

    submit mianshiti 0406 solution

---
+++ src/MianShiTi/MianShiTi0406/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace MianShiTi0406
+{
+    public class Solution
+    {
+        public TreeNode InorderSuccessor(TreeNode root, TreeNode p)
+        {
+            var list = new List<TreeNode>();
+            Inorder(root, list);
+
+            for (var i = 0; i < list.Count; i++)
+            {
+                if (list[i] == p && i + 1 < list.Count)
+                {
+                    return list[i + 1];
+                }
+            }
+
+            return null;
+        }
+
+        private void Inorder(TreeNode node, List<TreeNode> list)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            Inorder(node.left, list);
+            list.Add(node);
+            Inorder(node.right, list);
+        }
+    }
+}
---
+++ src/MianShiTi/MianShiTi0406/TestCases.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace MianShiTi0406
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var p = new TreeNode(1);
+            var expected = new TreeNode(2)
+            {
+                left = p,
+                right = new TreeNode(3)
+            };
+            var root = expected;
+            var result = solution.InorderSuccessor(root, p);
+            result.Should().Be(expected);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var p = new TreeNode(6);
+            var root = new TreeNode(5)
+            {
+                left = new TreeNode(3)
+                {
+                    left = new TreeNode(2)
+                    {
+                        left = new TreeNode(1)
+                    },
+                    right = new TreeNode(4)
+                },
+                right = p
+            };
+            var result = solution.InorderSuccessor(root, p);
+            result.Should().BeNull();
+        }
+    }
+}
