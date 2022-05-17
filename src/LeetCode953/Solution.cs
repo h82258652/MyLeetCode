5c44bd94752291fd2f708a0eef7aea84dca2632f
h82258652 <842053625@qq.com>
2022-05-17 10:32:18 +0800

    submit 953 solution

---
+++ src/LeetCode953/Solution.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode953
+{
+    public class Solution
+    {
+        public bool IsAlienSorted(string[] words, string order)
+        {
+            var wordCount = words.Length;
+            if (wordCount == 1)
+            {
+                return true;
+            }
+
+            var orderValue = new Dictionary<char, int>();
+            for (var i = 0; i < order.Length; i++)
+            {
+                orderValue[order[i]] = i;
+            }
+
+            orderValue[' '] = int.MinValue;
+
+            for (var i = 0; i < wordCount - 1; i++)
+            {
+                var previousWord = words[i];
+                var nextWord = words[i + 1];
+                var compareWord = CompareWord(previousWord, nextWord, orderValue);
+                if (!compareWord)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool CompareWord(string previousWord, string nextWord, Dictionary<char, int> orderValue)
+        {
+            var previousWordLength = previousWord.Length;
+            var nextWordLength = nextWord.Length;
+            var totalLength = Math.Max(previousWordLength, nextWordLength);
+            for (var i = 0; i < totalLength; i++)
+            {
+                var previousWordChar = ' ';
+                if (i < previousWordLength)
+                {
+                    previousWordChar = previousWord[i];
+                }
+
+                var nextWordChar = ' ';
+                if (i < nextWordLength)
+                {
+                    nextWordChar = nextWord[i];
+                }
+
+                var previousWordValue = orderValue[previousWordChar];
+                var nextWordValue = orderValue[nextWordChar];
+                if (previousWordValue < nextWordValue)
+                {
+                    return true;
+                }
+
+                if (previousWordValue > nextWordValue)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/LeetCode953/TestCases.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode953
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var words = new[] { "hello", "leetcode" };
+            var order = "hlabcdefgijkmnopqrstuvwxyz";
+            var result = solution.IsAlienSorted(words, order);
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var words = new[] { "word", "world", "row" };
+            var order = "worldabcefghijkmnpqstuvxyz";
+            var result = solution.IsAlienSorted(words, order);
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var solution = new Solution();
+            var words = new[] { "apple", "app" };
+            var order = "abcdefghijklmnopqrstuvwxyz";
+            var result = solution.IsAlienSorted(words, order);
+            result.Should().BeFalse();
+        }
+    }
+}
