b341e92ce28a81f7422434dd9360bc41ef032797
h82258652 <h82258652@hotmail.com>
2022-05-28 14:00:41 +0800

    submit 1021 solution

---
+++ src/LeetCode1021/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode1021
+{
+    public class Solution
+    {
+        public string RemoveOuterParentheses(string s)
+        {
+            var builder = new StringBuilder();
+            var list = new List<char>();
+            var level = 0;
+
+            foreach (var c in s)
+            {
+                if (c == '(')
+                {
+                    list.Add(c);
+                    level++;
+                }
+                else
+                {
+                    list.Add(c);
+                    level--;
+
+                    if (level == 0)
+                    {
+                        for (var i = 1; i < list.Count - 1; i++)
+                        {
+                            builder.Append(list[i]);
+                        }
+                        list.Clear();
+                    }
+                }
+            }
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ src/LeetCode1021/TestCases.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1021
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var result = solution.RemoveOuterParentheses("(()())(())");
+            result.Should().Be("()()()");
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var result = solution.RemoveOuterParentheses("(()())(())(()(()))");
+            result.Should().Be("()()()()(())");
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var solution = new Solution();
+            var result = solution.RemoveOuterParentheses("()()");
+            result.Should().Be("");
+        }
+    }
+}
