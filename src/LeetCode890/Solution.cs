fefad8970f8ecf91d40ee83f9d87b16971fdba82
h82258652 <h82258652@hotmail.com>
2022-06-12 16:27:48 +0800

    submit 890 solution

---
+++ src/LeetCode890/Solution.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode890
+{
+    public class Solution
+    {
+        public IList<string> FindAndReplacePattern(string[] words, string pattern)
+        {
+            var length = pattern.Length;
+            var result = new List<string>();
+            foreach (var word in words)
+            {
+                if (MatchPattern(word, pattern, length))
+                {
+                    result.Add(word);
+                }
+            }
+
+            return result;
+        }
+
+        private bool MatchPattern(string word, string pattern, int length)
+        {
+            var patternToWord = new Dictionary<char, char>();
+            var wordToPattern = new Dictionary<char, char>();
+
+            for (var i = 0; i < length; i++)
+            {
+                var patternCharacter = pattern[i];
+                var wordCharacter = word[i];
+                if (patternToWord.ContainsKey(patternCharacter))
+                {
+                    if (patternToWord[patternCharacter] != wordCharacter)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    patternToWord[patternCharacter] = wordCharacter;
+                }
+
+                if (wordToPattern.ContainsKey(wordCharacter))
+                {
+                    if (wordToPattern[wordCharacter] != patternCharacter)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    wordToPattern[wordCharacter] = patternCharacter;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/LeetCode890/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode890
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var words = new string[] { "abc", "deq", "mee", "aqq", "dkd", "ccc" };
+            var pattern = "abb";
+            var result = solution.FindAndReplacePattern(words, pattern);
+            result.Should().Equal("mee", "aqq");
+        }
+    }
+}
