7edc68100d1aac7153625f71fa9ea2137fd00f3d
h82258652 <842053625@qq.com>
2022-06-13 20:22:56 +0800

    submit 1051 solution

---
+++ src/LeetCode1051/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LeetCode1051
+{
+    public class Solution
+    {
+        public int HeightChecker(int[] heights)
+        {
+            var length = heights.Length;
+            var copy = new int[length];
+            Array.Copy(heights, copy, length);
+            Array.Sort(copy);
+            var result = 0;
+            for (var i = 0; i < length; i++)
+            {
+                if (heights[i] != copy[i])
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
