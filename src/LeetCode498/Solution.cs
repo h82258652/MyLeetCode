9983a1ca08d093b5791cca120f7f630f2b8e77cc
h82258652 <h82258652@hotmail.com>
2022-06-14 19:40:32 +0800

    submit 498 solution

---
+++ src/LeetCode498/Solution.cs
@@ -0,0 +1,74 @@
+﻿namespace LeetCode498
+{
+    public class Solution
+    {
+        public int[] FindDiagonalOrder(int[][] mat)
+        {
+            var m = mat.Length;
+            var n = mat[0].Length;
+
+            var i = 0;
+            var x = 0;
+            var y = 0;
+            var direction = Direction.TopRight;
+            var result = new int[m * n];
+
+            while (true)
+            {
+                if (x == m - 1 && y == n - 1)
+                {
+                    result[i] = mat[x][y];
+                    break;
+                }
+
+                result[i] = mat[x][y];
+                i++;
+
+                if (direction == Direction.TopRight)
+                {
+                    if (y == n - 1)
+                    {
+                        x++;
+                        direction = Direction.BottomLeft;
+                    }
+                    else if (x == 0)
+                    {
+                        y++;
+                        direction = Direction.BottomLeft;
+                    }
+                    else
+                    {
+                        x--;
+                        y++;
+                    }
+                }
+                else
+                {
+                    if (x == m - 1)
+                    {
+                        y++;
+                        direction = Direction.TopRight;
+                    }
+                    else if (y == 0)
+                    {
+                        x++;
+                        direction = Direction.TopRight;
+                    }
+                    else
+                    {
+                        x++;
+                        y--;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public enum Direction
+        {
+            TopRight,
+            BottomLeft
+        }
+    }
+}
---
+++ src/LeetCode498/TestCases.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode498
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var mat = new int[][] { new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 }, new int[] { 7, 8, 9 } };
+            var result = solution.FindDiagonalOrder(mat);
+            var expected = new int[] { 1, 2, 4, 7, 5, 3, 6, 8, 9 };
+            result.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var mat = new int[][] { new int[] { 1, 2 }, new int[] { 3, 4 } };
+            var result = solution.FindDiagonalOrder(mat);
+            var expected = new int[] { 1, 2, 3, 4 };
+            result.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+        }
+    }
+}
