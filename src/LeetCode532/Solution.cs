ab47a578936f5d8afc021ede47abae7b46bd96ef
h82258652 <842053625@qq.com>
2022-06-16 10:43:58 +0800

    submit 532 solution

---
+++ src/LeetCode532/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode532
+{
+    public class Solution
+    {
+        public int FindPairs(int[] nums, int k)
+        {
+            if (k == 0)
+            {
+                return nums.GroupBy(temp => temp).Where(temp => temp.Count() > 1).Count();
+            }
+
+            Array.Sort(nums);
+            var hashSet = new HashSet<int>(nums);
+            var result = 0;
+            foreach (var num in nums.Distinct())
+            {
+                if (hashSet.Contains(num + k))
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode532/TestCases.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode532
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var nums = new int[] { 3, 1, 4, 1, 5 };
+            var k = 2;
+            var result = solution.FindPairs(nums, k);
+            result.Should().Be(2);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var nums = new int[] { 1, 2, 3, 4, 5 };
+            var k = 1;
+            var result = solution.FindPairs(nums, k);
+            result.Should().Be(4);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var solution = new Solution();
+            var nums = new int[] { 1, 3, 1, 5, 4 };
+            var k = 0;
+            var result = solution.FindPairs(nums, k);
+            result.Should().Be(1);
+        }
+    }
+}
