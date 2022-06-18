0bcdbc6e9d40bc68ea5156c6aed464295da858b0
h82258652 <h82258652@hotmail.com>
2022-06-19 01:10:45 +0800

    submit 508 solution

---
+++ src/LeetCode508/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode508
+{
+    public class Solution
+    {
+        public int[] FindFrequentTreeSum(TreeNode root)
+        {
+            var sum = new Dictionary<TreeNode, int>();
+            PostOrder(root, sum);
+            var group = sum.Values.GroupBy(temp => temp).ToList();
+            var maxCount = group.Select(temp => temp.Count()).Max();
+            return group.Where(temp => temp.Count() == maxCount).Select(temp => temp.Key).ToArray();
+        }
+
+        private void PostOrder(TreeNode node, Dictionary<TreeNode, int> sum)
+        {
+            if (node is null)
+            {
+                return;
+            }
+
+            PostOrder(node.left, sum);
+            PostOrder(node.right, sum);
+            var val = node.val;
+            if (node.left != null && sum.ContainsKey(node.left))
+            {
+                val += sum[node.left];
+            }
+            if (node.right != null && sum.ContainsKey(node.right))
+            {
+                val += sum[node.right];
+            }
+            sum[node] = val;
+        }
+    }
+}
---
+++ src/LeetCode508/TestCases.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode508
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var root = new TreeNode(5)
+            {
+                left = new TreeNode(2),
+                right = new TreeNode(-3)
+            };
+            var result = solution.FindFrequentTreeSum(root);
+            result.Should().Equal(2, -3, 4);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var root = new TreeNode(5)
+            {
+                left = new TreeNode(2),
+                right = new TreeNode(-5)
+            };
+            var result = solution.FindFrequentTreeSum(root);
+            result.Should().Equal(2);
+        }
+    }
+}
