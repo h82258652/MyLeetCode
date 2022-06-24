9e16f63f140cd459c430fe95e9be62327150b544
h82258652 <842053625@qq.com>
2022-06-24 13:02:58 +0800

    submit 515 solution

---
+++ src/LeetCode515/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode515
+{
+    public class Solution
+    {
+        public IList<int> LargestValues(TreeNode root)
+        {
+            if (root == null)
+            {
+                return new List<int>();
+            }
+
+            var nodes = new Queue<TreeNode>();
+            nodes.Enqueue(root);
+
+            var result = new List<int>();
+
+            while (nodes.Count > 0)
+            {
+                var count = nodes.Count;
+                var max = int.MinValue;
+                for (var i = 0; i < count; i++)
+                {
+                    var node = nodes.Dequeue();
+                    max = Math.Max(max, node.val);
+
+                    if (node.left != null)
+                    {
+                        nodes.Enqueue(node.left);
+                    }
+
+                    if (node.right != null)
+                    {
+                        nodes.Enqueue(node.right);
+                    }
+                }
+
+                result.Add(max);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode515/TestCases.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode515
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var root = new TreeNode(1)
+            {
+                left = new TreeNode(3)
+                {
+                    left = new TreeNode(5),
+                    right = new TreeNode(3)
+                },
+                right = new TreeNode(2)
+                {
+                    right = new TreeNode(9)
+                }
+            };
+            var result = solution.LargestValues(root);
+            result.Should().Equal(1, 3, 9);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var root = new TreeNode(1)
+            {
+                left = new TreeNode(2),
+                right = new TreeNode(3)
+            };
+            var result = solution.LargestValues(root);
+            result.Should().Equal(1, 3);
+        }
+    }
+}
