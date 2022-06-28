e789bccf7b5a04878a63936843415edbd01989a1
h82258652 <842053625@qq.com>
2022-06-28 08:45:59 +0800

    submit 324 solution

---
+++ src/LeetCode324/Solution.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace LeetCode324
+{
+    public class Solution
+    {
+        public void WiggleSort(int[] nums)
+        {
+            var length = nums.Length;
+            Array.Sort(nums);
+            var result = new int[length];
+
+            if (length % 2 == 0)
+            {
+                var index = 1;
+                for (var i = length - 1; i >= length / 2; i--)
+                {
+                    result[index] = nums[i];
+                    index += 2;
+                }
+
+                index = 0;
+                for (var i = length / 2 - 1; i >= 0; i--)
+                {
+                    result[index] = nums[i];
+                    index += 2;
+                }
+            }
+            else
+            {
+                var index = 1;
+                for (var i = length - 1; i >= length / 2 + 1; i--)
+                {
+                    result[index] = nums[i];
+                    index += 2;
+                }
+
+                index = 0;
+                for (var i = length / 2; i >= 0; i--)
+                {
+                    result[index] = nums[i];
+                    index += 2;
+                }
+            }
+
+            Array.Copy(result, nums, length);
+        }
+    }
+}
---
+++ src/LeetCode324/TestCases.cs
@@ -0,0 +1,67 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode324
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var nums = new int[] { 1, 5, 1, 1, 6, 4 };
+            solution.WiggleSort(nums);
+            AssertResult(nums);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var nums = new int[] { 1, 3, 2, 2, 3, 1 };
+            solution.WiggleSort(nums);
+            AssertResult(nums);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var solution = new Solution();
+            var nums = new int[] { 4, 5, 5, 6 };
+            solution.WiggleSort(nums);
+            AssertResult(nums);
+        }
+
+        private void AssertResult(int[] nums)
+        {
+            var length = nums.Length;
+            for (var i = 0; i < length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    if (i - 1 >= 0)
+                    {
+                        nums[i].Should().BeLessThan(nums[i - 1]);
+                    }
+
+                    if (i + 1 < length)
+                    {
+                        nums[i].Should().BeLessThan(nums[i + 1]);
+                    }
+                }
+                else
+                {
+                    if (i - 1 >= 0)
+                    {
+                        nums[i].Should().BeGreaterThan(nums[i - 1]);
+                    }
+
+                    if (i + 1 < length)
+                    {
+                        nums[i].Should().BeGreaterThan(nums[i + 1]);
+                    }
+                }
+            }
+        }
+    }
+}
