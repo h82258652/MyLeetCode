7e5417c3e071d8c8e1b2e425c0bef9366e1b57cf
h82258652 <842053625@qq.com>
2022-06-29 09:17:14 +0800

    submit 535 solution

---
+++ src/LeetCode535/Codec.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode535
+{
+    public class Codec
+    {
+
+        // Encodes a URL to a shortened URL
+        public string encode(string longUrl)
+        {
+            return longUrl;
+        }
+
+        // Decodes a shortened URL to its original URL.
+        public string decode(string shortUrl)
+        {
+            return shortUrl;
+        }
+    }
+}
---
+++ src/LeetCode535/TestCases.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode535
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var codec = new Codec();
+            var url = "https://leetcode.com/problems/design-tinyurl";
+            var tiny = codec.encode(url);
+            var ans = codec.decode(tiny);
+            ans.Should().Be(url);
+        }
+    }
+}
