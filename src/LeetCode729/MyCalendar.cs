8f1dc56e5684897964775b6909768a2c51017a24
h82258652 <842053625@qq.com>
2022-07-05 10:19:04 +0800

    submit 729 solution

---
+++ src/LeetCode729/MyCalendar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode729
+{
+    public class MyCalendar
+    {
+        private readonly List<MyBook> _myBooks = new List<MyBook>();
+
+        public class MyBook
+        {
+            public MyBook(int start, int end)
+            {
+                Start = start;
+                End = end;
+            }
+
+            public int Start { get; }
+
+            public int End { get; }
+        }
+
+        public MyCalendar()
+        {
+        }
+
+        public bool Book(int start, int end)
+        {
+            foreach (var book in _myBooks)
+            {
+                if (end > book.Start && start < book.End)
+                {
+                    return false;
+                }
+            }
+
+            _myBooks.Add(new MyBook(start, end));
+            return true;
+        }
+    }
+}
---
+++ src/LeetCode729/TestCases.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode729
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var myCalendar = new MyCalendar();
+            myCalendar.Book(10, 20).Should().BeTrue();
+            myCalendar.Book(15, 25).Should().BeFalse();
+            myCalendar.Book(20, 30).Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var myCalendar = new MyCalendar();
+            myCalendar.Book(47, 50).Should().BeTrue();
+            myCalendar.Book(33, 41).Should().BeTrue();
+            myCalendar.Book(39, 45).Should().BeFalse();
+            myCalendar.Book(33, 42).Should().BeFalse();
+            myCalendar.Book(25, 32).Should().BeTrue();
+            myCalendar.Book(26, 35).Should().BeFalse();
+            myCalendar.Book(19, 25).Should().BeTrue();
+            myCalendar.Book(3, 8).Should().BeTrue();
+            myCalendar.Book(8, 13).Should().BeTrue();
+            myCalendar.Book(18, 27).Should().BeFalse();
+        }
+    }
+}
