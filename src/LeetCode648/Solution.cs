ca01cc7674bf64bc40b54c9b529c80565745f72b
h82258652 <842053625@qq.com>
2022-07-07 14:27:19 +0800

    submit 648 solution

---
+++ src/LeetCode648/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode648
+{
+    public class Solution
+    {
+        public string ReplaceWords(IList<string> dictionary, string sentence)
+        {
+            var words = sentence.Split(' ');
+            dictionary = dictionary.OrderBy(temp => temp.Length).ToList();
+            for (var i = 0; i < words.Length; i++)
+            {
+                var word = words[i];
+                foreach (var root in dictionary)
+                {
+                    if (word.StartsWith(root))
+                    {
+                        words[i] = root;
+                        break;
+                    }
+                }
+            }
+
+            return string.Join(' ', words);
+        }
+    }
+}
---
+++ src/LeetCode648/TestCases.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode648
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var dictionary = new List<string>() { "cat", "bat", "rat" };
+            var sentence = "the cattle was rattled by the battery";
+            var result = solution.ReplaceWords(dictionary, sentence);
+            result.Should().Be("the cat was rat by the bat");
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var dictionary = new List<string>() { "a", "b", "c" };
+            var sentence = "aadsfasf absbs bbab cadsfafs";
+            var result = solution.ReplaceWords(dictionary, sentence);
+            result.Should().Be("a a b c");
+        }
+    }
+}
