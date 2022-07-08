f5fc33ed7708217160afb60e36648ab2dbe70629
h82258652 <842053625@qq.com>
2022-07-08 08:53:19 +0800

    submit 1217 solution

---
+++ src/LeetCode1217/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LeetCode1217
+{
+    public class Solution
+    {
+        public int MinCostToMoveChips(int[] position)
+        {
+            var oddCount = 0;// 奇数的个数
+            var evenCount = 0;// 偶数的个数
+            foreach (var num in position)
+            {
+                if (num % 2 == 1)
+                {
+                    oddCount++;
+                }
+                else
+                {
+                    evenCount++;
+                }
+            }
+
+            return Math.Min(oddCount, evenCount);
+        }
+    }
+}
---
+++ src/LeetCode1217/TestCases.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1217
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var position = new int[] { 1, 2, 3 };
+            var result = solution.MinCostToMoveChips(position);
+            result.Should().Be(1);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var position = new int[] { 2, 2, 2, 3, 3 };
+            var result = solution.MinCostToMoveChips(position);
+            result.Should().Be(2);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var solution = new Solution();
+            var position = new int[] { 1, 1000000000 };
+            var result = solution.MinCostToMoveChips(position);
+            result.Should().Be(1);
+        }
+    }
+}
