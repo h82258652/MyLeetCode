b62abd9eca5c7b11d3f6ba07b891fd92793af7c0
h82258652 <842053625@qq.com>
2022-07-11 08:30:45 +0800

    submit 676 solution

---
+++ src/LeetCode676/MagicDictionary.cs
@@ -0,0 +1,60 @@
+﻿namespace LeetCode676
+{
+    public class MagicDictionary
+    {
+        private string[] _dictionary;
+
+        public MagicDictionary()
+        {
+        }
+
+        public void BuildDict(string[] dictionary)
+        {
+            _dictionary = dictionary;
+        }
+
+        public bool Search(string searchWord)
+        {
+            foreach (var word in _dictionary)
+            {
+                if (IsMatch(word, searchWord))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool IsMatch(string word, string searchWord)
+        {
+            if (word == searchWord)
+            {
+                return false;
+            }
+
+            if (word.Length != searchWord.Length)
+            {
+                return false;
+            }
+
+            var missMatch = false;
+            for (var i = 0; i < word.Length; i++)
+            {
+                if (word[i] == searchWord[i])
+                {
+                    continue;
+                }
+
+                if (missMatch)
+                {
+                    return false;
+                }
+
+                missMatch = true;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/LeetCode676/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode676
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var magicDictionary = new MagicDictionary();
+            var dictionary = new string[] { "hello", "leetcode" };
+            magicDictionary.BuildDict(dictionary);
+            magicDictionary.Search("hello").Should().BeFalse();
+            magicDictionary.Search("hhllo").Should().BeTrue();
+            magicDictionary.Search("hell").Should().BeFalse();
+            magicDictionary.Search("leetcoded").Should().BeFalse();
+        }
+    }
+}
