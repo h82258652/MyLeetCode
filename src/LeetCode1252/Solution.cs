e94c13ace115c4972e560148be5d9325e63d7e08
h82258652 <842053625@qq.com>
2022-07-12 08:54:30 +0800

    submit 1252 solution

---
+++ src/LeetCode1252/Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode1252
+{
+    public class Solution
+    {
+        public int OddCells(int m, int n, int[][] indices)
+        {
+            var rowCount = new int[m];
+            var columnCount = new int[n];
+            foreach (var indice in indices)
+            {
+                var ri = indice[0];
+                var ci = indice[1];
+
+                rowCount[ri]++;
+                columnCount[ci]++;
+            }
+
+            var result = 0;
+            for (var r = 0; r < m; r++)
+            {
+                for (var c = 0; c < n; c++)
+                {
+                    var sum = rowCount[r] + columnCount[c];
+                    if (sum % 2 == 1)
+                    {
+                        result++;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode1252/TestCases.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1252
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var result = solution.OddCells(2, 3, new int[][]
+            {
+                new int[] { 0, 1 },
+                new int[] { 1, 1 }
+            });
+            result.Should().Be(6);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var result = solution.OddCells(2, 2, new int[][]
+            {
+                new int[] { 1, 1 },
+                new int[] { 0, 0 }
+            });
+            result.Should().Be(0);
+        }
+    }
+}
