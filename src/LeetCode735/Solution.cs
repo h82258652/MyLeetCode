1ba5e07a632be4db4bcf801bd2c4fce5345b9a4a
h82258652 <842053625@qq.com>
2022-07-13 12:25:19 +0800

    submit 735 solution

---
+++ src/LeetCode735/Solution.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode735
+{
+    public class Solution
+    {
+        public int[] AsteroidCollision(int[] asteroids)
+        {
+            var list = new List<int>(asteroids);
+            for (var i = 0; i < list.Count; i++)
+            {
+                if (list[i] > 0)
+                {
+                    continue;
+                }
+
+                if (i <= 0)
+                {
+                    continue;
+                }
+
+                if (list[i - 1] > 0)
+                {
+                    var abs = Math.Abs(list[i]);
+                    if (list[i - 1] > abs)
+                    {
+                        list.RemoveAt(i);
+                        i--;
+                    }
+                    else if (list[i - 1] == abs)
+                    {
+                        list.RemoveAt(i);
+                        list.RemoveAt(i - 1);
+                        i -= 2;
+                    }
+                    else
+                    {
+                        list.RemoveAt(i - 1);
+                        i -= 2;
+                    }
+                }
+            }
+
+            for (var i = list.Count - 1; i >= 0; i--)
+            {
+                if (list[i] < 0)
+                {
+                    continue;
+                }
+
+                if (i >= list.Count - 1)
+                {
+                    continue;
+                }
+
+                if (list[i + 1] < 0)
+                {
+                    var abs = Math.Abs(list[i + 1]);
+                    if (list[i] > abs)
+                    {
+                        list.RemoveAt(i + 1);
+                    }
+                    else if (list[i] == abs)
+                    {
+                        list.RemoveAt(i + 1);
+                        list.RemoveAt(i);
+                    }
+                    else
+                    {
+                        list.RemoveAt(i);
+                    }
+                }
+            }
+
+            return list.ToArray();
+        }
+    }
+}
---
+++ src/LeetCode735/TestCases.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode735
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var asteroids = new int[] { 5, 10, -5 };
+            var result = solution.AsteroidCollision(asteroids);
+            result.Should().Equal(5, 10);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var asteroids = new int[] { 8, -8 };
+            var result = solution.AsteroidCollision(asteroids);
+            result.Should().Equal();
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var solution = new Solution();
+            var asteroids = new int[] { 10, 2, -5 };
+            var result = solution.AsteroidCollision(asteroids);
+            result.Should().Equal(10);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            var solution = new Solution();
+            var asteroids = new int[] { 7, -1, 2, -3, -6, -6, -6, 4, 10, 2 };
+            var result = solution.AsteroidCollision(asteroids);
+            result.Should().Equal(7, 4, 10, 2);
+        }
+    }
+}
