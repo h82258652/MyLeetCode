5b1443392c6b7770ac47dc2490469f68fd446085
h82258652 <842053625@qq.com>
2022-07-21 16:54:45 +0800

    submit 814 solution

---
+++ src/LeetCode814/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode814
+{
+    public class Solution
+    {
+        public TreeNode PruneTree(TreeNode root)
+        {
+            var dict = new Dictionary<TreeNode, bool>();
+            PostOrder(root, dict);
+
+            return dict[root] ? null : root;
+        }
+
+        private void PostOrder(TreeNode node, Dictionary<TreeNode, bool> canDeleteDict)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            if (node.left != null)
+            {
+                PostOrder(node.left, canDeleteDict);
+                if (canDeleteDict[node.left])
+                {
+                    node.left = null;
+                }
+            }
+
+            if (node.right != null)
+            {
+                PostOrder(node.right, canDeleteDict);
+                if (canDeleteDict[node.right])
+                {
+                    node.right = null;
+                }
+            }
+
+            canDeleteDict[node] = node.val == 0 && node.left == null && node.right == null;
+        }
+    }
+}
---
+++ src/LeetCode814/TestCases.cs
@@ -0,0 +1,83 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode814
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var root = new TreeNode(1)
+            {
+                right = new TreeNode(0)
+                {
+                    left = new TreeNode(0),
+                    right = new TreeNode(1)
+                }
+            };
+            var solution = new Solution();
+            var result = solution.PruneTree(root);
+            result.val.Should().Be(1);
+            result.right.val.Should().Be(0);
+            result.right.left.Should().BeNull();
+            result.right.right.val.Should().Be(1);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var root = new TreeNode(1)
+            {
+                left = new TreeNode(0)
+                {
+                    left = new TreeNode(0),
+                    right = new TreeNode(0)
+                },
+                right = new TreeNode(1)
+                {
+                    left = new TreeNode(0),
+                    right = new TreeNode(1)
+                }
+            };
+            var solution = new Solution();
+            var result = solution.PruneTree(root);
+            result.val.Should().Be(1);
+            result.left.Should().BeNull();
+            result.right.val.Should().Be(1);
+            result.right.left.Should().BeNull();
+            result.right.right.val.Should().Be(1);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var root = new TreeNode(1)
+            {
+                left = new TreeNode(1)
+                {
+                    left = new TreeNode(1)
+                    {
+                        left = new TreeNode(0)
+                    },
+                    right = new TreeNode(1)
+                },
+                right = new TreeNode(0)
+                {
+                    left = new TreeNode(0),
+                    right = new TreeNode(1)
+                }
+            };
+            var solution = new Solution();
+            var result = solution.PruneTree(root);
+            result.val.Should().Be(1);
+            result.left.val.Should().Be(1);
+            result.left.left.val.Should().Be(1);
+            result.left.right.val.Should().Be(1);
+            result.left.left.left.Should().BeNull();
+            result.right.val.Should().Be(0);
+            result.right.left.Should().BeNull();
+            result.right.right.val.Should().Be(1);
+        }
+    }
+}
