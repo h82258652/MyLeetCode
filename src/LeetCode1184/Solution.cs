415a86b41c18e55e5dec534988d69a5085cf3b0f
h82258652 <842053625@qq.com>
2022-07-24 18:52:07 +0800

    submit 1184 solution

---
+++ src/LeetCode1184/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LeetCode1184
+{
+    public class Solution
+    {
+        public int DistanceBetweenBusStops(int[] distance, int start, int destination)
+        {
+            if (start == destination)
+            {
+                return 0;
+            }
+
+            if (destination < start)
+            {
+                var temp = start;
+                start = destination;
+                destination = temp;
+            }
+
+            var sum = 0;
+            var target = 0;
+            var n = distance.Length;
+            for (var i = 0; i < n; i++)
+            {
+                var temp = distance[i];
+                sum += temp;
+                if (i >= start && i < destination)
+                {
+                    target += temp;
+                }
+            }
+
+            return Math.Min(target, sum - target);
+        }
+    }
+}
---
+++ src/LeetCode1184/TestCases.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode1184
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var solution = new Solution();
+            var distance = new int[] { 1, 2, 3, 4 };
+            var start = 0;
+            var destination = 1;
+            var result = solution.DistanceBetweenBusStops(distance, start, destination);
+            result.Should().Be(1);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var solution = new Solution();
+            var distance = new int[] { 1, 2, 3, 4 };
+            var start = 0;
+            var destination = 2;
+            var result = solution.DistanceBetweenBusStops(distance, start, destination);
+            result.Should().Be(3);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var solution = new Solution();
+            var distance = new int[] { 1, 2, 3, 4 };
+            var start = 0;
+            var destination = 3;
+            var result = solution.DistanceBetweenBusStops(distance, start, destination);
+            result.Should().Be(4);
+        }
+    }
+}
