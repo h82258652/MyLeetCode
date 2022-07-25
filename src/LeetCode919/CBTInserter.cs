d9485e46204d415a30287cf6bff12cf12581b1d0
h82258652 <842053625@qq.com>
2022-07-25 08:53:27 +0800

    submit 919 solution

---
+++ src/LeetCode919/CBTInserter.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode919
+{
+    public class CBTInserter
+    {
+        private readonly List<TreeNode> _nodes;
+        private readonly TreeNode _root;
+
+        public CBTInserter(TreeNode root)
+        {
+            _root = root;
+            _nodes = new List<TreeNode>();
+            _nodes.Add(null);
+
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+                _nodes.Add(node);
+
+                if (node.left != null)
+                {
+                    queue.Enqueue(node.left);
+                }
+
+                if (node.right != null)
+                {
+                    queue.Enqueue(node.right);
+                }
+            }
+        }
+
+        public int Insert(int val)
+        {
+            var newNodeIndex = _nodes.Count;
+            var newNode = new TreeNode(val);
+            _nodes.Add(newNode);
+
+            var parentNodeIndex = newNodeIndex / 2;
+            var parentNode = _nodes[parentNodeIndex];
+            if (newNodeIndex % 2 == 0)
+            {
+                parentNode.left = newNode;
+                return parentNode.val;
+            }
+            else
+            {
+                parentNode.right = newNode;
+                return parentNode.val;
+            }
+        }
+
+        public TreeNode Get_root()
+        {
+            return _root;
+        }
+    }
+}
---
+++ src/LeetCode919/TestCases.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode919
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Test1()
+        {
+            var root = new TreeNode(1)
+            {
+                left = new TreeNode(2)
+            };
+            var obj = new CBTInserter(root);
+            obj.Insert(3).Should().Be(1);
+            obj.Insert(4).Should().Be(2);
+            var result = obj.Get_root();
+            result.val.Should().Be(1);
+            result.left.val.Should().Be(2);
+            result.right.val.Should().Be(3);
+            result.left.left.val.Should().Be(4);
+        }
+    }
+}
